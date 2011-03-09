d90358088200e4068bec7ad82a03c106bc61ca89
Quentin THEURET <quentin@tempo-quentin>
2011-03-09 09:05:30 +0100

    [IMP] Improve wizards to go to RfQ or PO generated by the Procurement list

--- procurement_list/wizard/__init__.py
+++ procurement_list/wizard/__init__.py
@@ -20,48 +20,8 @@
 #
 ##############################################################################
 
-
-import wizard
-import pooler
-import time
-
-from osv import osv
-from osv import fields
-
-
-def _launch_wizard(self, cr, uid, data, context={}):
-    """
-    Open the Request for Quotation list of RfQ related to
-    the procurement list
-    """
-    list_obj = pooler.get_pool(cr.dbname).get('procurement.list')
-    l_ids = list_obj.browse(cr, uid, data ['ids'], context=context)
-    rfq_ids = []
-
-    for l in l_ids:
-        rfq_ids.append(l.order_ids)
-
-    return {
-        'type': 'ir.actions.act_window',
-        'res_model': 'purchase.order',
-        'view_mode': 'tree,form',
-        'view_type': 'form',
-        'domain': [('id', 'in', rfq_ids), ('state', '=', 'draft')],
-    }
-
-
-class wizard_to_rfq(wizard.interface):
-
-    states = {
-        'init': {
-            'actions': [],
-            'result': {'type': 'action',
-                       'action': _launch_wizard,
-                       'state': 'end'}
-        }
-    }
-
-wizard_to_rfq('procurement_list_to_rfq')
+import wizard_list_to_order
+import wizard_list_to_rfq
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
