b36c6433416391762213ddcf4d2c7b548a148d3e
mdietrich <>
2011-03-14 11:25:57 +0100

    [ADD] "Expand all" optional button for tree view

--- addons/openerp/widgets/tree_view.py
+++ addons/openerp/widgets/tree_view.py
@@ -34,7 +34,7 @@ import treegrid
 class ViewTree(Form):
 
     template = "/openerp/widgets/templates/viewtree.mako"
-    params = ['model', 'id', 'ids', 'domain', 'context', 'view_id', 'toolbar']
+    params = ['model', 'id', 'ids', 'domain', 'context', 'view_id', 'toolbar', 'expand_button']
     member_widgets = ['tree', 'sidebar']
 
     def __init__(self, view, model, res_id=False, domain=[], context={}, action=None, fields=None):
@@ -66,6 +66,7 @@ class ViewTree(Form):
         attrs = node_attributes(root)
         self.string = attrs.get('string', 'Unknown')
         self.toolbar = attrs.get('toolbar', False)
+        self.expand_button = attrs.get('expand_button', False)
 
         ids = []
         id = res_id
