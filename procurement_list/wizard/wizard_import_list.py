50b2edb86f9db123216e33baa12c77c874ea3d0a
quentin@theuret.net <>
2011-03-15 17:07:07 +0100

    UF-98: [FIX] Fixed non valid line on import file

--- procurement_list/wizard/wizard_import_list.py
+++ procurement_list/wizard/wizard_import_list.py
@@ -59,7 +59,6 @@ class wizard_import_list(osv.osv_memory):
         '''
         Simply close the wizard
         '''
-#        return {'type': 'ir.actions.act_window_close', 'active_id' : context.get('list_id', False),}
         return {'type': 'ir.actions.act_window',
                 'res_model': 'procurement.list',
                 'view_type': 'form',
@@ -119,7 +118,14 @@ class wizard_import_list(osv.osv_memory):
 
         error = ''
 
+        line_num = 0
+
         for line in reader:
+            line_num += 1
+            if len(line) < 6:
+                error += 'Line %s is not valid !' % (line_num)
+                error += '\n'
+                continue
             # Get the product
             product_ids = product_obj.search(cr, uid, [('default_code', '=', line[0])], context=context)
             if not product_ids:
