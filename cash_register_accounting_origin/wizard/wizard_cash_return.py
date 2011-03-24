c687a8b1d6e84b08fe94a28b8b440654a296b422
Olivier DOSSMANN <olivier@tempo-laptop>
2011-03-24 17:23:20 +0100

    UF-148: [IMP] Close invoice when added to the cash return
    Now the invoice is closed after all move lines are written.

--- cash_register_accounting_origin/wizard/wizard_cash_return.py
+++ cash_register_accounting_origin/wizard/wizard_cash_return.py
@@ -326,7 +326,7 @@ class wizard_cash_return(osv.osv_memory):
                 account_id = invoice.account_id.id
                 inv_id = self.create_move_line(cr, uid, ids, curr_date, inv_name, journal, register, partner_id, False, account_id, \
                     debit, credit, move_id, context=context)
-                inv_move_line_ids.append(inv_id)
+                inv_move_line_ids.append((inv_id, invoice.invoice_id.id))
         else:
             # make treatment for advance lines
             # Prepare a list of advances that have a supplier and then demand generating some moves
@@ -364,8 +364,13 @@ class wizard_cash_return(osv.osv_memory):
         # create the statement line for the invoices
         absl_obj = self.pool.get('account.bank.statement.line')
         if wizard.display_invoice:
-            for inv_move_line_id in inv_move_line_ids:
-                inv_st_id = self.create_st_line_from_move_line(cr, uid, ids, register.id, move_id, inv_move_line_id, context=context)
+            for inv_move_line_data in inv_move_line_ids:
+                inv_st_id = self.create_st_line_from_move_line(cr, uid, ids, register.id, move_id, inv_move_line_data[0], context=context)
+                # Confirm the payment for the invoice
+                invoice_id = inv_move_line_data[1]
+                inv_paid = self.pool.get('account.invoice').write(cr, uid, invoice_id, {'state': 'paid'}, context=context)
+                if not inv_paid:
+                    raise osv.except_osv(_('Error'), _('The payment confirmation of an invoice failed.'))
         else:
             for adv_move_line_id in adv_move_line_ids:
                 adv_st_id = self.create_st_line_from_move_line(cr, uid, ids, register.id, move_id, adv_move_line_id, context=context)
