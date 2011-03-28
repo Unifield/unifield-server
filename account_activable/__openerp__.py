64956cabcfdbe8425ea28ba3a8876994eb14420c
Matthieu Dietrich <matthieu.dietrich@geneva.msf.org>
2011-03-28 14:16:41 +0200

    UF-158: [ADD] Chart of Accounts

---
+++ account_activable/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Chart of Accounts for MSF",
+    "version": "1.0",
+    "depends": ["account"],
+    "author" : "MSF: Matthieu Dietrich",
+    "category": "General/Standard",
+    "description": """
+    This module changes the view and adds a searchable "Active" attribute.
+    
+    """,
+    "init_xml": [],
+    'update_xml': [
+        'account_activable_view.xml',
+        'wizard/account_chart_activable_view.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
