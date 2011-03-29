a1dc884c28348ae7c19e22cf9d844185ddb95d2c
Olivier DOSSMANN <olivier@tempo-laptop>
2011-03-29 11:33:19 +0200

    UF-148: [FIX] Some bugs on wizard return advance
    - case where cash return is null
    - case where no currency appears on journal

--- cash_register_accounting_origin/wizard/wizard_cash_return.py
+++ cash_register_accounting_origin/wizard/wizard_cash_return.py
@@ -101,7 +101,9 @@ class wizard_cash_return(osv.osv_memory):
                 raise osv.except_osv(_('Error'), _('A wrong amount was selected. Please select an advance with a positive amount.'))
             else:
                 st_line = self.pool.get('account.bank.statement.line').browse(cr, uid, context.get('active_id'), context=context)
-                currency_id = st_line.statement_id.journal_id.currency.id
+                currency_id = st_line.statement_id.company_id.currency_id.id
+                if st_line.statement_id.journal_id.currency:
+                    currency_id = st_line.statement_id.journal_id.currency.id
                 res.update({'initial_amount': abs(amount), 'advance_st_line_id': context.get('active_id'), 'currency_id': currency_id})
         return res
 
@@ -395,11 +397,12 @@ class wizard_cash_return(osv.osv_memory):
         }
         # create the move
         move_id = move_obj.create(cr, uid, move_vals, context=context)
-        # create a cash return move line
-        return_name = "Cash return"
-        return_acc_id = register.journal_id.default_credit_account_id.id
-        return_id = self.create_move_line(cr, uid, ids, curr_date, return_name, journal, register, False, False, return_acc_id, \
-            wizard.returned_amount, 0.0, move_id, context=context)
+        # create a cash return move line ONLY IF this return is superior to 0
+        if wizard.returned_amount > 0:
+            return_name = "Cash return"
+            return_acc_id = register.journal_id.default_credit_account_id.id
+            return_id = self.create_move_line(cr, uid, ids, curr_date, return_name, journal, register, False, False, return_acc_id, \
+                wizard.returned_amount, 0.0, move_id, context=context)
         if wizard.display_invoice:
             # make treatment for invoice lines
             # create invoice lines
@@ -442,7 +445,8 @@ class wizard_cash_return(osv.osv_memory):
         adv_closing_id = self.create_move_line(cr, uid, ids, curr_date, adv_closing_name, journal, register, False, employee_id, adv_closing_acc_id, \
             0.0, wizard.initial_amount, move_id, context=context)
         # Verify that the balance of the move is null
-        if wizard.advance_st_line_id.statement_id.journal_id.currency.id != wizard.advance_st_line_id.statement_id.company_id.currency_id.id:
+        st_currency = wizard.advance_st_line_id.statement_id.journal_id.currency.id
+        if st_currency and st_currency != wizard.advance_st_line_id.statement_id.company_id.currency_id.id:
             # change the amount_currency of the advance closing line in order to be negative (not done in create_move_line function)
             res_adv_closing = self.pool.get('account.move.line').write(cr, uid, [adv_closing_id], {'amount_currency': -wizard.initial_amount}, context=context)
             if move_obj._compute_balance(cr, uid, move_id, context=context) <> 0:
