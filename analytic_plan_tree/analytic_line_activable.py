673d497499ea306f92703fe0b6aed4f3f793ed08
Matthieu Dietrich <matthieu.dietrich@geneva.msf.org>
2011-04-11 11:19:56 +0200

    UF-133: [FIX] modified test for invalid date

--- analytic_plan_tree/analytic_line_activable.py
+++ analytic_plan_tree/analytic_line_activable.py
@@ -31,7 +31,8 @@ class analytic_line_activable(osv.osv):
             account_obj = self.pool.get('account.analytic.account')
             account = account_obj.browse(cr, uid, vals['account_id'])
             if vals['date'] < account.activation_date \
-            or vals['date'] >= account.inactivation_date:
+            or (account.inactivation_date != False and \
+                vals['date'] >= account.inactivation_date):
                 raise osv.except_osv(_('Error !'), _('You cannot set a date out of the account\'s activity period!'))
 
     def create(self, cr, uid, vals, context=None):
