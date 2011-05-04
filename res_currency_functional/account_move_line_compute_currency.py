989d720c4681c0a53d7fe6eaa81e5b06c765b377
Matthieu Dietrich <matthieu.dietrich@geneva.msf.org>
2011-05-04 18:05:24 +0200

    UF-168: [FIX] check for journal_id in create() + better work-around in write()

--- res_currency_functional/account_move_line_compute_currency.py
+++ res_currency_functional/account_move_line_compute_currency.py
@@ -94,16 +94,23 @@ class account_move_line_compute_currency(osv.osv):
         self.update_amounts(cr, uid, [res_id])
         #@@@override@account.account_move_line.create()
         # The validation is re-done after the amounts have been modified.
-        journal = self.pool.get('account.journal').browse(cr, uid, vals['journal_id'], context=context)
-        if check and ((not context.get('no_store_function')) or journal.entry_posted):
-            tmp = self.pool.get('account.move').validate(cr, uid, [vals['move_id']], context)
-            if journal.entry_posted and tmp:
-                self.pool.get('account.move').button_validate(cr,uid, [vals['move_id']], context)
+        if 'journal_id' in vals:
+            journal = self.pool.get('account.journal').browse(cr, uid, vals['journal_id'], context=context)
+            if check and ((not context.get('no_store_function')) or journal.entry_posted):
+                tmp = self.pool.get('account.move').validate(cr, uid, [vals['move_id']], context)
+                if journal.entry_posted and tmp:
+                    self.pool.get('account.move').button_validate(cr,uid, [vals['move_id']], context)
         return res_id
     
     def write(self, cr, uid, ids, vals, context=None, check=True, update_check=True):
         self.check_date(cr, uid, vals)
-        res = super(account_move_line_compute_currency, self).write(cr, uid, ids, vals, context, check, update_check)
+        #@@@override@account.account_move_line.write()
+        # This is done to retrieve the todo_date from the super method.
+        todo_date = None
+        if vals.get('date', False):
+            todo_date = vals['date']
+        # @override
+        res = super(account_move_line_compute_currency, self).write(cr, uid, ids, vals, context, check=False, update_check=update_check)
         self.update_amounts(cr, uid, ids)
         #@@@override@account.account_move_line.write()
         # The validation is re-done after the amounts have been modified.
@@ -113,6 +120,9 @@ class account_move_line_compute_currency(osv.osv):
                 if line.move_id.id not in done:
                     done.append(line.move_id.id)
                     self.pool.get('account.move').validate(cr, uid, [line.move_id.id], context)
+                    if todo_date:
+                        self.pool.get('account.move').write(cr, uid, [line.move_id.id], {'date': todo_date}, context=context)
+        # @override
         return res
     
     _columns = {
