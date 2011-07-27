2ca118df435f3c4339088c7788f6e991bd51a36f
Olivier DOSSMANN <olivier@tempo-laptop>
2011-07-27 11:19:16 +0200

    UF-340 [ADD] Supplier invoice ref. field

--- register_accounting/account_move_line.py
+++ register_accounting/account_move_line.py
@@ -42,6 +42,7 @@ class account_move_line(osv.osv):
         'third_parties': fields.function(_get_third_parties, type='reference', method=True, 
             string="Third Parties", selection=[('res.partner', 'Partner'), ('hr.employee', 'Employee'), ('account.bank.statement', 'Register')], 
             help="To use for python code when registering", multi="third_parties_key"),
+        'supplier_invoice_ref': fields.related('invoice', 'name', type='char', size=64, string="Supplier inv.ref.", store=False),
     }
 
     def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
