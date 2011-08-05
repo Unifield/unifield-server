35bdfc0a43b38742dc2324e2f41824fc77355c75
Olivier DOSSMANN <olivier@tempo-laptop>
2011-08-05 09:40:04 +0200

    UF-340 [FIX] Error on date
    - correct the date entered by the user to be written in the register

--- register_accounting/wizard/import_cheque_on_bank_registers.py
+++ register_accounting/wizard/import_cheque_on_bank_registers.py
@@ -139,7 +139,7 @@ class wizard_import_cheque(osv.osv_memory):
             total = line.amount_currency
             vals = {
                 'name': 'Imported Cheque: ' + (line.name or line.ref or line.cheque_number or ''),
-                'date': _get_date_in_period(self, cr, uid, line.date or curr_date, wizard.period_id.id, context=context),
+                'date': _get_date_in_period(self, cr, uid, imported_line.date or curr_date, wizard.period_id.id, context=context),
                 'statement_id': wizard.statement_id.id,
                 'account_id': line.account_id.id,
                 'partner_id': line.partner_id.id,
