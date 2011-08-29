5c4dff95f5261458eec53d307130fb3894ec0105
Olivier DOSSMANN <olivier@tempo-laptop>
2011-08-29 15:27:46 +0200

    UF-358 [ADD] Add reverse lines for invoices that have been changed to open state

--- account_override/invoice.py
+++ account_override/invoice.py
@@ -23,11 +23,47 @@
 
 
 from osv import osv
+from time import strftime
 from tools.translate import _
 
 class account_invoice(osv.osv):
     _name = 'account.invoice'
     _inherit = 'account.invoice'
+
+    def action_reverse_engagement_lines(self, cr, uid, ids, context, *args):
+        """
+        Reverse an engagement lines with an opposite amount
+        """
+        if not context:
+            context = {}
+        eng_obj = self.pool.get('account.analytic.line')
+        # Browse invoice
+        for inv in self.browse(cr, uid, ids, context=context):
+            # Search engagement journal line ids
+            invl_ids = [x.id for x in inv.invoice_line]
+            eng_ids = eng_obj.search(cr, uid, [('invoice_line_id', 'in', invl_ids)])
+            # Browse engagement journal line ids
+            for eng in eng_obj.browse(cr, uid, eng_ids, context=context):
+                # Create new line and change some fields:
+                # - name with REV
+                # - amount * -1
+                # - source_date with same date as first one
+                # - date with current date
+                # Update old engagement line with invoice date
+                eng_obj.write(cr, uid, [eng.id], {'date': inv.date_invoice, 'source_date': inv.date_invoice,})
+                # Copy this line for reverse
+                new_line_id = eng_obj.copy(cr, uid, eng.id, context=context)
+                # Prepare reverse values
+                vals = {
+                    'name': 'REV-' + eng.name,
+                    'amount': eng.amount * -1,
+                    'date': strftime('%Y-%m-%d'),
+                    'source_date': inv.date_invoice,
+                    'reversal_origin': eng.id,
+                }
+                # Write changes
+                eng_obj.write(cr, uid, [new_line_id], vals, context=context)
+        return True
 
     def action_open_invoice(self, cr, uid, ids, context={}, *args):
         """
@@ -37,8 +73,8 @@ class account_invoice(osv.osv):
             return False
         if not self.action_move_create(cr, uid, ids, context, args):
             return False
-#        if not self.action_reverse_engagement_lines():
-#            return False
+        if not self.action_reverse_engagement_lines(cr, uid, ids, context, args):
+            return False
         if not self.action_number(cr, uid, ids, context):
             return False
         return self.write(cr, uid, ids, {'state': 'open'}, context=context)
