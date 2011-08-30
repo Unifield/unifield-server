42367192ad563dea8961575902b7d59ac0e216f4
Olivier DOSSMANN <olivier@tempo-laptop>
2011-08-30 13:38:34 +0200

    UF-358 [FIX] Recalculation of amount regarding fx rate changes

--- res_currency_functional/res_currency_rate_functional.py
+++ res_currency_functional/res_currency_rate_functional.py
@@ -42,12 +42,36 @@ class res_currency_rate_functional(osv.osv):
         
         move_line_ids = move_line_obj.search(cr, uid, move_line_search_params)
         move_line_obj.update_amounts(cr, uid, move_line_ids)
-    
+
+    def refresh_analytic_lines(self, cr, uid, ids, date=None, currency=None, context={}):
+        """
+        Refresh analytic lines that don't come from a move
+        """
+        if not context:
+            context={}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        # Engagement lines object
+        eng_obj = self.pool.get('account.analytic.line')
+        # Search all engagement journal lines that don't come from a move and which date is superior to the rate
+        search_params = [('move_id', '=', '')]
+        if date:
+            search_params.append(('source_date', '>=', date))
+        if currency:
+            search_params.append(('currency_id', '=', currency))
+        eng_ids = eng_obj.search(cr, uid, [('move_id', '=', ''), ('source_date', '>=', date)])
+        if eng_ids:
+            eng_obj.update_amounts(cr, uid, eng_ids, context=context)
+        return True
+
     def create(self, cr, uid, vals, context=None):
         # This method is used to re-compute all account move lines
         # when a currency is modified
         res_id = super(res_currency_rate_functional, self).create(cr, uid, vals, context)
         self.refresh_move_lines(cr, uid, [res_id], date=vals['name'])
+        # Also update analytic move line that don't come from a move (engagement journal lines)
+        currency_id = vals.get('currency_id', False)
+        self.refresh_analytic_lines(cr, uid, [res_id], date=vals['name'], currency=currency_id, context=context)
         return res_id
     
     def write(self, cr, uid, ids, vals, context=None):
@@ -55,6 +79,10 @@ class res_currency_rate_functional(osv.osv):
         # when a currency is modified
         res = super(res_currency_rate_functional, self).write(cr, uid, ids, vals, context)
         self.refresh_move_lines(cr, uid, ids, date=vals['name'])
+        # Also update analytic move line that don't come from a move (engagement journal lines)
+        for rate in self.browse(cr, uid, ids, context=context):
+            currency_id = rate.currency_id and rate.currency_id.id or False
+            self.refresh_analytic_lines(cr, uid, ids, date=vals['name'], currency=currency_id, context=context)
         return res
     
     def unlink(self, cr, uid, ids, context=None):
@@ -66,7 +94,6 @@ class res_currency_rate_functional(osv.osv):
             res = res & super(res_currency_rate_functional, self).unlink(cr, uid, ids, context)
             self.refresh_move_lines(cr, uid, ids, currency=currency_id)
         return res
-    
 
 res_currency_rate_functional()
 
