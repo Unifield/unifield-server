7c46eea15af6c486422aeefb0216594287a1a29f
Olivier DOSSMANN <olivier@tempo-laptop>
2011-09-02 15:30:46 +0200

    UF-338 [ADD] Two function to reverse lines:
    - One to permit to reverse all lines from a move that contains the given lines. This doesn't works if ONE line of the move is corrected
    - Second that permits to reverse a line if this one doesn't be corrected

--- account_corrections/account_move_line.py
+++ account_corrections/account_move_line.py
@@ -24,6 +24,7 @@
 from osv import osv
 from osv import fields
 from tools.translate import _
+from time import strftime
 
 class account_move_line(osv.osv):
     _name = 'account.move.line'
@@ -81,6 +82,133 @@ class account_move_line(osv.osv):
         Open all corrections linked to the given one
         """
         return True
+
+    def reverse(self, cr, uid, ids, date=None, context={}):
+        """
+        Reverse given lines by creating a new Journal Entry (account_move) and write in the reversal line.
+        Reversal lines have some information:
+         - name begin with REV
+         - debit and credit are reversed
+         - amount_currency is the opposite
+         - date is those from given date or current date by default
+         - period is those that correspond to the given date
+         - line is written in the first 'correction' type journal found
+        NB : return the succeeded move lines
+        """
+        # Verifications
+        if not context:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if not date:
+            date = strftime('%Y-%m-%d')
+        # Prepare some values
+        success_move_line_ids = []
+        move_obj = self.pool.get('account.move')
+        j_obj = self.pool.get('account.journal')
+        j_ids = j_obj.search(cr, uid, [('type', '=', 'correction')], context=context)
+        # Search correction journal
+        j_corr_ids = j_obj.search(cr, uid, [('type', '=', 'correction')], context=context)
+        if not j_corr_ids:
+            raise osv.except_osv(_('Error'), ('No correction journal found!'))
+        # Search attached period
+        period_ids = self.pool.get('account.period').search(cr, uid, [('date_start', '<=', date), ('date_stop', '>=', date)], context=context, 
+            limit=1, order='date_start, name')
+        # Browse move line
+        for ml in self.browse(cr, uid, ids, context=context):
+            # Abort process if this move line was corrected before
+            if ml.corrected:
+                continue
+            
+            # FIXME: Verify that this line doesn't come from a register (statement_id) => update register line
+            
+            # Create a new move
+            move_id = move_obj.create(cr, uid,{'journal_id': j_corr_ids[0], 'period_id': period_ids[0], 'date': date}, context=context)
+            # Prepare default value for new line
+            vals = {
+                'move_id': move_id,
+                'date': date,
+                'journal_id': j_corr_ids[0],
+                'period_id': period_ids[0],
+            }
+            # Copy the line
+            rev_line_id = self.copy(cr, uid, ml.id, vals, context=context)
+            # Do the reverse
+            name = 'REV' + ' ' + ml.name
+            amt = -1 * ml.amount_currency
+            vals.update({
+                'debit': ml.credit,
+                'credit': ml.debit,
+                'amount_currency': amt,
+                'journal_id': j_corr_ids[0],
+                'name': name,
+                'corrected_line_id': ml.id,
+                'account_id': ml.account_id.id,
+            })
+            self.write(cr, uid, [rev_line_id], vals, context=context)
+            # Inform old line that it have been corrected
+            self.write(cr, uid, [ml.id], {'corrected': True}, context=context)
+            # Add this line to succeded lines
+            success_move_line_ids.append(ml.id)
+        return success_move_line_ids
+
+    def reverse_move(self, cr, uid, ids, date=None, context={}):
+        """
+        Reverse the move from lines
+        Return succeeded move lines (not complementary move lines)
+        """
+        # Verifications
+        if not context:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if not date:
+            date = strftime('%Y-%m-%d')
+        # Prepare some values
+        success_move_line_ids = []
+        move_obj = self.pool.get('account.move')
+        j_obj = self.pool.get('account.journal')
+        j_ids = j_obj.search(cr, uid, [('type', '=', 'correction')], context=context)
+        # Search correction journal
+        j_corr_ids = j_obj.search(cr, uid, [('type', '=', 'correction')], context=context)
+        if not j_corr_ids:
+            raise osv.except_osv(_('Error'), ('No correction journal found!'))
+        # Search attached period
+        period_ids = self.pool.get('account.period').search(cr, uid, [('date_start', '<=', date), ('date_stop', '>=', date)], context=context, 
+            limit=1, order='date_start, name')
+        # Sort ids
+        move_lines = self.browse(cr, uid, ids, context=context)
+        tmp_move_ids = [x.move_id.id for x in move_lines if x.move_id]
+        # Delete double and sort it
+        move_ids = sorted(list(set(tmp_move_ids)))
+        # Browse moves
+        for m in move_obj.browse(cr, uid, move_ids, context=context):
+            # Verify this move could be reversed
+            corrigible = True
+            for ml in m.line_id:
+                if ml.corrected:
+                    corrigible = False
+            if not corrigible:
+                continue
+            
+            # FIXME: verify that no lines come from a statement_id => should be corrected if necessary
+            
+            # Create a new move
+            new_move_id = move_obj.create(cr, uid,{'journal_id': j_corr_ids[0], 'period_id': period_ids[0], 'date': date}, context=context)
+            # Browse all move lines and change information
+            for ml in self.browse(cr, uid, [x.id for x in m.line_id], context=context):
+                amt = -1 * ml.amount_currency
+                name = 'REV' + ' ' + ml.name
+                new_line_id = self.copy(cr, uid, ml.id, {'move_id': new_move_id}, context=context)
+                self.write(cr, uid, [new_line_id], {'name': name, 'debit': ml.credit, 'credit': ml.debit, 'amount_currency': amt, 
+                    'corrected_line_id': ml.id}, context=context)
+                # Flag this line as corrected
+                self.write(cr, uid, [ml.id], {'corrected': True}, context=context)
+                if ml.id in ids:
+                    success_move_line_ids.append(ml.id)
+            # Hard post the move
+            move_obj.post(cr, uid, [new_move_id], context=context)
+        return success_move_line_ids
 
     def correct_account(self, cr, uid, ids, date=None, new_account_id=None, context={}):
         """
