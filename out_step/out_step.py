db080b78df5b95de588243f7c4fe725bbe5424be
chloups208 <chloups208@chloups208-laptop>
2011-11-08 17:37:31 +0100

    out_step added

---
+++ out_step/out_step.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 MSF, TeMPO Consulting
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv, fields
+from tools.translate import _
+import netsvc
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import decimal_precision as dp
+import netsvc
+import logging
+import tools
+from os import path
+
+class stock_picking(osv.osv):
+    '''
+    add a check boolean for confirmation of delivery
+    '''
+    _inherit = 'stock.picking'
+
+    _columns = {'delivered': fields.boolean(string='Delivered'),
+                }
+    _defaults = {'delivered': False,
+                 }
+     
+stock_picking()
