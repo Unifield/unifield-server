d90d7c663de6593266a7e7f650e59c7a6d3cb8bb
Olivier DOSSMANN <olivier@tempo-laptop>
2011-11-16 17:46:49 +0100

    UF-673 [ADD] VAT management module
    - add new module VAT management
    - Create some VAT accounts in Chart of account
    - Create a VAT

--- msf_profile/__openerp__.py
+++ msf_profile/__openerp__.py
@@ -75,6 +75,7 @@
         "threshold_value",
         "analytic_distribution_invoice",
 #        "analytic_distribution_purchase",
+        "vat_management",
     ],
     "update_xml": [
         "report.xml",
