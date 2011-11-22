1a791a124f0535b4a7d4b6bd7c41b3e688f3956a
Olivier DOSSMANN <olivier@tempo-laptop>
2011-11-22 13:43:09 +0100

    UF-665 [ADD] View to select some filters then new tab to display multi-critera data browser

---
+++ account_mcdb/account_mcdb.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 TeMPO Consulting, MSF. All Rights Reserved
+#    Developer: Olivier DOSSMANN
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+class account_mcdb(osv.osv_memory):
+    _name = 'account.mcdb'
+
+    _columns = {
+        'journal_id': fields.many2one('account.journal', string="Journal Code"),
+        'abs_id': fields.many2one('account.bank.statement', string="Register Code"), # Change into many2many ?
+        'company_id': fields.many2one('res.company', string="Proprietary instance"),
+        'posting_date': fields.date('Posting date'),
+        'document_date': fields.date('Document date'),
+        'period_id': fields.many2one('account.period', string="Accounting Period"),
+        'account_ids': fields.many2many(obj='account.account', rel='account_account_mcdb', id1='mcdb_id', id2='account_id', string="Account Code"),
+        'partner_id': fields.many2one('res.partner', string="Partner"),
+        'employee_id': fields.many2one('hr.employee', string="Employee"),
+        'register_id': fields.many2one('account.bank.statement', string="Register"),
+        'reconciled': fields.selection([('reconciled', 'Reconciled'), ('unreconciled', 'NOT reconciled')], string='Reconciliation'),
+        'booking_currency_id': fields.many2one('res.currency', string="Booking currency"),
+        'amount_func_from': fields.float('Amount'),
+        'amount_func_to': fields.float('to'),
+        'amount_book_from': fields.float('Amount'),
+        'amount_book_to': fields.float('to'),
+        'account_type_ids': fields.many2many(obj='account.account.type', rel='account_account_type_mcdb', id1='mcdb_id', id2='account_type_id', string="Account type"),
+        'reconcile_id': fields.many2one('account.move.reconcile', string="Reconcile Reference"),
+        'ref': fields.char(string='Reference', size=255),
+        'name': fields.char(string='Description', size=255),
+        'type': fields.selection([('account.move.line', 'Journal Items'), ('account.analytic.line', 'Analytic Journal Items')], string="Type")
+    }
+
+    _defaults = {
+        'type': lambda *a: 'account.move.line'
+    }
+
+    def button_validate(self, cr, uid, ids, context={}):
+        """
+        Validate current forms and give result
+        """
+        # Some verifications
+        if not context:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        # Prepare some values
+        domain = []
+        wiz = self.browse(cr, uid, [ids[0]], context=context)[0]
+        res_model = wiz and wiz.type or False
+        if res_model:
+            # Prepare domain values
+            if wiz.account_ids:
+                domain.append(('account_id', 'in', tuple([x.id for x in wiz.account_ids])),)
+            return {
+                'name': _('Multi-criteria data browser result'),
+                'type': 'ir.actions.act_window',
+                'res_model': res_model,
+                'view_type': 'form',
+                'view_mode': 'tree',
+                'view_id': False,
+                'domain': domain,
+                'context': context,
+                'target': 'current',
+            }
+        return False
+
+account_mcdb()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- msf_profile/__openerp__.py
+++ msf_profile/__openerp__.py
@@ -74,6 +74,7 @@
         "consumption_calculation",
         "threshold_value",
         "analytic_distribution_invoice",
+        "account_mcdb",
 #        "analytic_distribution_purchase",
     ],
     "update_xml": [
