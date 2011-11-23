4b685b04e13222fe8c64162e3ac2cfe79481522a
Quentin THEURET <qt@tempo-consulting.fr>
2011-11-23 16:09:52 +0100

    [ADD] Added msf_audittrail module [FIX] Fixed a bug on object_query module [FIX] Fixed the bug which hides the Links on left toolbar

--- msf_profile/__openerp__.py
+++ msf_profile/__openerp__.py
@@ -74,6 +74,7 @@
         "consumption_calculation",
         "threshold_value",
         "analytic_distribution_invoice",
+        "msf_audittrail",
 #        "analytic_distribution_purchase",
     ],
     "update_xml": [
