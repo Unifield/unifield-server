bf1c249b8a916e2b9005ae9142136410da729b38
chloups208 <chloups208@chloups208-laptop>
2011-11-29 09:52:59 +0100

    uf-717 both qty values are set to False with product change

--- specific_rules/specific_rules.py
+++ specific_rules/specific_rules.py
@@ -482,7 +482,11 @@ class stock_move(osv.osv):
                 result.setdefault('value', {})['hidden_perishable_mandatory'] = True
                 result['warning'] = {'title': _('Info'),
                                      'message': _('The selected product is Perishable.')}
-            
+        
+        # quantities are set to False
+        result.setdefault('value', {}).update({'product_qty': False,
+                                               'product_uos_qty': False,
+                                               })
         return result
     
     def _get_checks_all(self, cr, uid, ids, name, arg, context=None):
