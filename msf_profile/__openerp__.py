f60d5963dbd4ecbf48276ce5b92b14f8f1fe6bc3
qt-tempo-consulting <>
2011-12-15 09:11:34 +0100

    UF-697 [ADD] Added the msf_config_locations which allows user to configure the optionnal locations of his instance.

--- msf_profile/__openerp__.py
+++ msf_profile/__openerp__.py
@@ -74,6 +74,7 @@
         "consumption_calculation",
         "threshold_value",
         "analytic_distribution_invoice",
+        "msf_config_locations",
 #        "analytic_distribution_purchase",
     ],
     "update_xml": [
