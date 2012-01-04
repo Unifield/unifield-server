bfcf65220c544922b0163a8358c273da37f186f5
jf <jf@tempo4>
2012-01-04 09:52:46 +0100

    [FIX] Destruction Report link

--- stock_override/__openerp__.py
+++ stock_override/__openerp__.py
@@ -35,6 +35,7 @@
         "stock_workflow.xml",
         "report/report_stock_move_view.xml",
         "procurement_view.xml",
+        "destruction_report.xml",
         "report/report_destruction_location_view.xml",
     ],
     'test': [],
