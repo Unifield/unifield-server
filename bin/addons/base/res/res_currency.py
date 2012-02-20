823257249ec6dbaba11d2304423a73aeabfe7f38
duy.vo@geneva.msf.org <>
2012-02-20 11:04:49 +0100

    [FIX] OEB-105: Fix the type check of input value

--- bin/addons/base/res/res_currency.py
+++ bin/addons/base/res/res_currency.py
@@ -68,6 +68,10 @@ class res_currency(osv.osv):
     _order = "name"
 
     def read(self, cr, user, ids, fields=None, context=None, load='_classic_read'):
+        is_integer = False
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+            is_integer = True
         res=super(osv.osv, self).read(cr, user, ids, fields, context, load)
         for r in res:
             if r.__contains__('rate_ids'):
@@ -76,6 +80,8 @@ class res_currency(osv.osv):
                     currency_rate_obj=  self.pool.get('res.currency.rate')
                     currency_date = currency_rate_obj.read(cr,user,rates[0],['name'])['name']
                     r['date'] = currency_date
+        if is_integer:
+            return res[0]
         return res
 
     def round(self, cr, uid, currency, amount):
