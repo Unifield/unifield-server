144fa6eb0647809077bb6de9ec119dbd1d2bcf4a
Olivier DOSSMANN <olivier@tempo-laptop>
2012-03-16 15:45:43 +0100

    UF-691 [ADD] Delete some useless elements
      * a whitespace
      * a line used for development

--- register_accounting/account_bank_statement.py
+++ register_accounting/account_bank_statement.py
@@ -1264,7 +1264,6 @@ class account_bank_statement_line(osv.osv):
                 if self.analytic_distribution_is_mandatory(cr, uid, absl.id, context=context) and not context.get('from_yml'):
                     raise osv.except_osv(_('Error'), _('No analytic distribution found!'))
                 if absl.is_transfer_with_change and not absl.transfer_amount and not absl.transfer_currency:
-                    print absl.is_transfer_with_change, absl.account_id.type_for_register, absl.account_id.code, absl.transfer_amount, absl.transfer_currency
                     raise osv.except_osv(_('Error'), _('Transfer amount and transfer currency is missing!'))
                 seq = self.pool.get('ir.sequence').get(cr, uid, 'all.registers')
                 self.write(cr, uid, [absl.id], {'sequence_for_reference': seq}, context=context)
