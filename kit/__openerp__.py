942e69fa872a7675dd2a2a37759d24a8ec6f5c0c
chloups208 <chloups208@chloups208-laptop>
2012-03-26 17:42:07 +0200

    uf-844 yaml test for kitting

--- kit/__openerp__.py
+++ kit/__openerp__.py
@@ -43,7 +43,7 @@
     ],
     "demo_xml": [],
     'test': ['test/kit_data.yml',
-#             'test/kit.yml',
+             'test/kit.yml',
              'test/kitting.yml',
              ],
     'installable': True,
