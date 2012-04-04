de8d383c54623ddfe4a76d29c4bf38aca5df1c44
Olivier DOSSMANN <olivier@tempo-laptop>
2012-04-04 13:46:01 +0200

    UF-689 [ADD] Some data for tests

--- register_accounting/__openerp__.py
+++ register_accounting/__openerp__.py
@@ -81,7 +81,7 @@
         'test/wizard_register_creation.yml',
         'test/bug_closing_balance_on_cashbox.yml',
         'test/import_invoice.yml',
-        'test/import_invoice_rate_before_import.yml'
+        'test/import_invoice_rate_before_import.yml',
     ],
     "installable": True,
     "active": False
