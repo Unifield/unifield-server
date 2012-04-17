dfad9c519cf3322fc05462185d03811bbebfb652
jf <jf@ubuntu>
2012-04-17 19:30:52 +0200

    CoA Installer: import_invoice_default_account

--- msf_chart_of_account/msf_chart_of_account_installer.py
+++ msf_chart_of_account/msf_chart_of_account_installer.py
@@ -9,18 +9,29 @@ class msf_chart_of_account_installer(osv.osv_memory):
     _inherit = 'res.config'
     _columns = {
         'create': fields.boolean('Create Journals'),
+        'import_invoice_default_account': fields.many2one('account.account', string="Re-billing Inter-section account"),
     }
 
+    def get_inter(self, cr, uid, *a, **b):
+        try:
+            return self.pool.get('ir.model.data').get_object_reference(cr, uid, 'msf_chart_of_account', '1404')[1]
+        except ValueError:
+            return False
+
     _defaults = {
-        'create': True
+        'create': True,
+        'import_invoice_default_account': get_inter
     }
 
     def execute(self, cr, uid, ids, context=None):
         res = self.read(cr, uid, ids)
-        if res and res[0] and res[0]['create']:
-            fp = tools.file_open(opj('msf_chart_of_account', 'journal_data.xml'))
-            tools.convert_xml_import(cr, 'msf_chart_of_account', fp, {}, 'init', True, None)
-            fp.close()
+        if res and res[0]:
+            if res[0]['create']:
+                fp = tools.file_open(opj('msf_chart_of_account', 'journal_data.xml'))
+                tools.convert_xml_import(cr, 'msf_chart_of_account', fp, {}, 'init', True, None)
+                fp.close()
+            if res[0]['import_invoice_default_account']:
+                self.pool.get('res.users').browse(cr, uid, uid).company_id.write({'import_invoice_default_account': res[0]['import_invoice_default_account']})
         return {}
 
 msf_chart_of_account_installer()
