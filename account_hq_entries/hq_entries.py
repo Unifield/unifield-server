8e102ba4ae0355ee6323505a5fb53795a6d3cf39
Olivier DOSSMANN <olivier@tempo-laptop>
2012-05-02 09:59:50 +0200

    UF-679 [ADD] HQ Entries validation
      * Create a move
      * Create lines
      * Do not create expat salaries lines with others

--- account_hq_entries/hq_entries.py
+++ account_hq_entries/hq_entries.py
@@ -23,9 +23,84 @@
 
 from osv import osv
 from osv import fields
+from time import strftime
 
 class hq_entries_validation_wizard(osv.osv_memory):
     _name = 'hq.entries.validation.wizard'
+
+    def create_move(self, cr, uid, ids):
+        """
+        Create a move with given hq entries lines
+        """
+        # Some verifications
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        # Prepare some values
+        res = False
+        if ids:
+            # prepare some values
+            current_date = strftime('%Y-%m-%d')
+            journal_ids = self.pool.get('account.journal').search(cr, uid, [('type', '=', 'hq')])
+            if not journal_ids:
+                raise osv.except_osv(_('Warning'), _('No HQ journal found!'))
+            journal_id = journal_ids[0]
+            period_ids = self.pool.get('account.period').get_period_from_date(cr, uid, current_date)
+            if not period_ids:
+                raise osv.except_osv(_('Warning'), _('No open period found for given date: %s') % (current_date,))
+            if len(period_ids) > 1:
+                raise osv.except_osv(_('Warning'), _('More than one period found for given date: %s') % (current_date,))
+            period_id = period_ids[0]
+            # create move
+            move_id = self.pool.get('account.move').create(cr, uid, {
+                'date': current_date,
+                'journal_id': journal_id,
+                'period_id': period_id,
+            })
+            total_amount = 0
+            for line in self.pool.get('hq.entries').read(cr, uid, ids, ['account_id', 'period_id', 'analytic_id', 'cost_center_id', 'date', 
+                'free_1_id', 'free_2_id']):
+                # create new distribution (only for expense accounts)
+                distrib_id = False
+                cc_id = line.get('cost_center_id', False) and line.get('cost_center_id')[0] or False
+                fp_id = line.get('analytic_id', False) and line.get('analytic_id')[0] or False
+                f1_id = line.get('free1_id', False) and line.get('free1_id')[0] or False
+                f2_id = line.get('free2_id', False) and line.get('free2_id')[0] or False
+                distrib_id = self.pool.get('analytic.distribution').create(cr, uid, {})
+                if distrib_id:
+                    common_vals = {
+                        'distribution_id': distrib_id,
+#                        'currency_id': line.get('currency_id', False) and line.get('currency_id')[0] or False,
+                        'percentage': 100.0,
+                        'date': line.get('date', False) or current_date,
+                        'source_date': line.get('date', False) or current_date,
+                    }
+                    common_vals.update({'analytic_id': cc_id,})
+                    cc_res = self.pool.get('cost.center.distribution.line').create(cr, uid, common_vals)
+                    common_vals.update({'analytic_id': fp_id, 'cost_center_id': cc_id,})
+                    fp_res = self.pool.get('funding.pool.distribution.line').create(cr, uid, common_vals)
+                    del common_vals['cost_center_id']
+                    if f1_id:
+                        common_vals.update({'analytic_id': f1_id,})
+                        self.pool.get('free.1.distribution.line').create(cr, uid, common_vals)
+                    if f2_id:
+                        common_vals.update({'analytic_id': f2_id})
+                        self.pool.get('free.2.distribution.line').create(cr, uid, common_vals)
+                vals = {
+                    'account_id': line.get('account_id', False),
+                    'period_id': line.get('period_id', False),
+                    'journal_id': journal_id,
+                    'date': line.get('date'),
+                    'move_id': move_id,
+                    'analytic_distribution_id': distrib_id,
+                }
+                self.pool.get('account.move.line').create(cr, uid, vals, context={}, check=False)
+                # total_amount += line.get('amount')
+            # FIXME: write an account move line with total_amount
+            # Post move
+            post = self.pool.get('account.move').post(cr, uid, [move_id])
+            if post:
+                res = True
+        return res
 
     def validate(self, cr, uid, ids, context={}):
         """
@@ -38,9 +113,20 @@ class hq_entries_validation_wizard(osv.osv_memory):
         if isinstance(active_ids, (int, long)):
             active_ids = [active_ids]
         # Tag active_ids as user validated
-        for line in self.pool.get('account.move.line').browse(cr, uid, active_ids, context=context):
+        to_write = []
+        for line in self.pool.get('hq.entries').browse(cr, uid, active_ids, context=context):
             if not line.user_validated:
-                self.pool.get('account.move.line').write(cr, uid, [line.id], {'user_validated': True}, context=context)
+                # FIXME: Determine an account as Expat Salaries in Company
+                if line.account_id.code == '61200':
+                    self.create_move(cr, uid, line.id)
+                    self.pool.get('hq.entries').write(cr, uid, [line.id], {'user_validated': True}, context=context)
+                    continue
+                to_write.append(line.id)
+        # Write lines and validate them
+        write = self.create_move(cr, uid, to_write)
+        if write:
+            self.pool.get('hq.entries').write(cr, uid, to_write, {'user_validated': True}, context=context)
+        # Return HQ Entries Tree View in current view
         action_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'account_hq_entries', 'action_hq_entries_tree')
         res = self.pool.get('ir.actions.act_window').read(cr, uid, action_id[1], [], context=context)
         res['target'] = 'crush'
