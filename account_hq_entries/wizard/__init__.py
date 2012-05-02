6e19ec1648be32ce850b1902287417c3434231da
Olivier DOSSMANN <olivier@tempo-laptop>
2012-05-02 11:36:08 +0200

    UF-679 [ADD] Add analytic reallocation wizard

--- account_hq_entries/wizard/__init__.py
+++ account_hq_entries/wizard/__init__.py
@@ -22,5 +22,6 @@
 ##############################################################################
 
 import hq_entries_import
+import hq_reallocation
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
