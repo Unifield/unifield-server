eeb992a2297eb1f456d7a9c321386ed064c06241
Olivier DOSSMANN <olivier@tempo-laptop>
2012-05-14 16:44:28 +0200

    UF-679 [FIX] Model access rules

--- account_hq_entries/__openerp__.py
+++ account_hq_entries/__openerp__.py
@@ -30,6 +30,7 @@
     "depends" : ["base", "account", "analytic", "account_journal", "account_corrections", "analytic_distribution"],
     "init_xml" : [],
     "update_xml" : [
+        'security/ir.model.access.csv',
         'res_company_view.xml',
         'account_view.xml',
     ],
