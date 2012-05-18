94a7c3c62c5b39f0328a6b98ef06170d8de64bdd
Olivier DOSSMANN <olivier@tempo-laptop>
2012-05-18 16:56:12 +0200

    UF-689 [ADD] Reference field for down payment after invoice validation

--- register_accounting/invoice.py
+++ register_accounting/invoice.py
@@ -130,6 +130,7 @@ class account_invoice(osv.osv):
                     'period_id': inv.period_id.id,
                     'date': inv.date_invoice,
                     'partner_id': inv.partner_id.id,
+                    'ref': ':'.join(['%s' % (x.name or '') for x in inv.purchase_ids]),
                 }
                 move_id = self.pool.get('account.move').create(cr, uid, vals)
                 # then 2 lines for this move
