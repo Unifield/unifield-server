3c131719e10f2fb9a606042ec4d1ca8c28e7430e
jf <jf@tempo4>
2012-05-21 14:32:42 +0200

    UF-998: reset ana. disti. on po/fo/po lines/so lines duplication

--- analytic_distribution_supply/purchase.py
+++ analytic_distribution_supply/purchase.py
@@ -122,16 +122,10 @@ class purchase_order(osv.osv):
             default = {}
         # Update default
         default.update({'commitment_ids': False,})
+        if 'analytic_distribution_id' not in default:
+            default['analytic_distribution_id'] = False
         # Default method
-        res = super(purchase_order, self).copy_data(cr, uid, id, default=default, context=context)
-        # Update analytic distribution
-        if res:
-            po = self.browse(cr, uid, res, context=context)
-        if res and po.analytic_distribution_id:
-            new_distrib_id = self.pool.get('analytic.distribution').copy(cr, uid, po.analytic_distribution_id.id, {}, context=context)
-            if new_distrib_id:
-                self.write(cr, uid, [res], {'analytic_distribution_id': new_distrib_id}, context=context)
-        return res
+        return super(purchase_order, self).copy_data(cr, uid, id, default=default, context=context)
 
     def action_create_commitment(self, cr, uid, ids, type=False, context=None):
         """
@@ -411,12 +405,8 @@ class purchase_order_line(osv.osv):
             default = {}
         # Update default
         default.update({'commitment_line_ids': [(6, 0, [])],})
-        # Copy analytic distribution
-        pol = self.browse(cr, uid, [id], context=context)[0]
-        if pol.analytic_distribution_id:
-            new_distrib_id = self.pool.get('analytic.distribution').copy(cr, uid, pol.analytic_distribution_id.id, {}, context=context)
-            if new_distrib_id:
-                default.update({'analytic_distribution_id': new_distrib_id})
+        if 'analytic_distribution_id' not in default:
+            default['analytic_distribution_id'] = False
         return super(purchase_order_line, self).copy_data(cr, uid, id, default, context)
 
 purchase_order_line()
