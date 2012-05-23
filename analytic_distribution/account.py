ebce8b31b4946c467fba5204c083c6483219ff11
Olivier DOSSMANN <olivier@tempo-laptop>
2012-05-23 17:55:06 +0200

    UF-1106 [ADD] Add default destination account to the list of compatible destination account on G/L Account configuration

--- analytic_distribution/account.py
+++ analytic_distribution/account.py
@@ -39,24 +39,26 @@ class destination_m2m(fields.many2many):
                 # search current destination
                 destination_sql = """
                 SELECT destination_id FROM %s
-                WHERE account_id = '%s'
-                """
-                cr.execute(destination_sql % (self._rel, id))
+                WHERE account_id = %%s
+                """ % self._rel
+                cr.execute(destination_sql, (id,))
                 destination_ids = cr.fetchall()
                 old_list = [x[0] for x in destination_ids if x and x[0]]
                 # delete useless destination
                 if act[2]:
                     delete_sql = """
                     DELETE FROM %s
-                    WHERE account_id = '%s'
-                    AND destination_id NOT IN %s
-                    """ % (self._rel, id, tuple(act[2]))
+                    WHERE account_id = %%s
+                    AND destination_id NOT IN %%s
+                    """ % self._rel
+                    query_sql = (id, tuple(act[2]))
                 else:
                     delete_sql = """
                     DELETE FROM %s
-                    WHERE account_id = '%s'
-                    """ % (self._rel, id)
-                cr.execute(delete_sql)
+                    WHERE account_id = %%s
+                    """ % self._rel
+                    query_sql = (id,)
+                cr.execute(delete_sql, query_sql)
                 # insert new destination
                 for new_id in list(set(act[2]) - set(old_list)):
                     cr.execute('insert into '+self._rel+' ('+self._id1+','+self._id2+') values (%s, %s)', (id, new_id))
@@ -83,9 +85,29 @@ class account_account(osv.osv):
         'user_type_code': fields.related('user_type', 'code', type="char", string="User Type Code", store=False),
         'funding_pool_line_ids': fields.many2many('account.analytic.account', 'funding_pool_associated_accounts', 'account_id', 'funding_pool_id', 
             string='Funding Pools'),
-        'default_destination_id': fields.many2one('account.analytic.account', 'Default Destination'),
+        'default_destination_id': fields.many2one('account.analytic.account', 'Default Destination', domain="[('type', '!=', 'view'), ('category', '=', 'DEST')]"),
         'destination_ids': destination_m2m('account.analytic.account', 'account_destination_link', 'account_id', 'destination_id', 'Destinations'),
     }
+
+    def write(self, cr, uid, ids, vals, context=None):
+        """
+        Add default destination to the list of destination_ids
+        """
+        # Prepare some values
+        if not context:
+            context = {}
+        # Check default destination presence
+        if 'default_destination_id' in vals:
+            # Fetch it
+            dd_id = vals.get('default_destination_id')
+            res = super(account_account, self).write(cr, uid, ids, vals, context=context)
+            for a in self.browse(cr, uid, ids):
+                if dd_id not in a.destination_ids:
+                    all_ids = [x.id for x in a.destination_ids] or []
+                    all_ids.append(dd_id)
+                    super(account_account, self).write(cr, uid, [a.id], {'destination_ids': [(6, 0, all_ids)]})
+            return res
+        return super(account_account, self).write(cr, uid, ids, vals, context=context)
 
 account_account()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
