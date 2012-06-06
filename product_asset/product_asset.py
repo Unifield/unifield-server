a33b96c76e103b3d0daaf6f5537cb634a6ac5ba7
pierre-marie <pierre-marie@pierre-marie-laptop>
2012-06-06 15:02:57 +0200

    UF-1041 - Service product type removal - correction

--- product_asset/product_asset.py
+++ product_asset/product_asset.py
@@ -372,7 +372,7 @@ class product_product(osv.osv):
     
     def create(self, cr, uid, vals, context=None):
         if context.get('res_log_read',False) and vals.get('type',False) == 'service':
-            vals.update({'type': 'service_recep'})
+            vals.update({'type': 'service_recep','procure_method':'make_to_order'})
         '''
         if a product is not of type product, it is set to single subtype
         '''
