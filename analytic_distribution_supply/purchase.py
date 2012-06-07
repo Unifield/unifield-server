6ab2ecda97e078d89d9efe1a4ad8fe320234673b
jf <jf@tempo4>
2012-06-07 17:01:03 +0200

    [FIX] Validate PO, cancel it, set it to draft and validate again

--- analytic_distribution_supply/purchase.py
+++ analytic_distribution_supply/purchase.py
@@ -310,9 +310,10 @@ class purchase_order_line(osv.osv):
         if isinstance(ids, (int, long)):
             ids = [ids]
 
-        line = self.browse(cr, uid, ids, context=context)[0]
-        if 'price_unit' in vals and vals['price_unit'] == 0.00 and self.pool.get('purchase.order').browse(cr, uid, vals.get('order_id', line.order_id.id), context=context).from_yml_test:
-            vals['price_unit'] = 1.00
+        if ids:
+            line = self.browse(cr, uid, ids, context=context)[0]
+            if 'price_unit' in vals and vals['price_unit'] == 0.00 and self.pool.get('purchase.order').browse(cr, uid, vals.get('order_id', line.order_id.id), context=context).from_yml_test:
+                vals['price_unit'] = 1.00
 
         return super(purchase_order_line, self).write(cr, uid, ids, vals, context=context)
 
