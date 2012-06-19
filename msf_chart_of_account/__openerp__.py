b0a718fb116c98ab816342f40e51756c97d48aff
duy.vo@geneva.msf.org <>
2012-06-19 15:08:57 +0200

    [FIX] UF-1099: Some integration with synch modules

--- msf_chart_of_account/__openerp__.py
+++ msf_chart_of_account/__openerp__.py
@@ -29,8 +29,8 @@
     "category" : "Localisation/Account Charts",
     "depends" : ["base", "account_activable", "register_accounting", "account_journal", "msf_instance"],
     "init_xml" : [
-        "data/account.xml",
-        "data/account_data.xml",
+#        "data/account.xml",
+#        "data/account_data.xml",
     ],
     "update_xml" : [
         "msf_chart_of_account_installer.xml",
