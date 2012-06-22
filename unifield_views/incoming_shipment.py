c4e9f4491f77ef5b817f23925086ef74856b6028
Quentin THEURET <qt@tempo-consulting.fr>
2012-06-22 16:33:07 +0200

    UF-1152 [IMP] Delivery orders : Rework search and tree view

--- unifield_views/incoming_shipment.py
+++ unifield_views/incoming_shipment.py
@@ -18,7 +18,19 @@ class ir_values(osv.osv):
                                     'tree_but_open': []}
         
         internal_accepted_values = {'client_action_multi': [],
-                                    'client_print_multi': [],
+                                    'client_print_multi': ['Labels'],
+                                    'client_action_relate': [],
+                                    'tree_but_action': [],
+                                    'tree_but_open': []}
+        
+        delivery_accepted_values = {'client_action_multi': [],
+                                    'client_print_multi': ['Labels'],
+                                    'client_action_relate': [''],
+                                    'tree_but_action': [],
+                                    'tree_but_open': []}
+        
+        picking_accepted_values = {'client_action_multi': [],
+                                    'client_print_multi': ['Picking Ticket', 'Pre-Packing List', 'Labels'],
                                     'client_action_relate': [''],
                                     'tree_but_action': [],
                                     'tree_but_open': []}
@@ -32,6 +44,16 @@ class ir_values(osv.osv):
             new_values = []
             for v in values:
                 if key == 'action' and v[1] in internal_accepted_values[key2]:
+                    new_values.append(v)
+        elif context.get('picking_type', False) == 'delivery_order' and 'stock.picking' in [x[0] for x in models]:
+            new_values = []
+            for v in values:
+                if key == 'action' and v[1] in delivery_accepted_values[key2]:
+                    new_values.append(v)
+        elif context.get('picking_type', False) == 'delivery_order' and 'stock.picking' in [x[0] for x in models]:
+            new_values = []
+            for v in values:
+                if key == 'action' and v[1] in picking_accepted_values[key2]:
                     new_values.append(v)
  
         return new_values
