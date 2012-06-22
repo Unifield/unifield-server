2ce2c1f4cc35a58a5b6a9ba36b31e03a1d62c43b
matthieu.choplin@geneva.msf.org <>
2012-06-22 14:19:01 +0200

    New module for useability requirements about menus and dashboard. Slide 4 of the ppt Menus and Dashboard.ppt done

---
+++ useability_dashboard_and_menu/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 TeMPO Consulting, MSF 
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Transport management",
+    "version": "1.0",
+    "depends": [
+                "sale", 
+                "purchase", 
+    ],
+    "author": "TeMPO Consulting, MSF",
+    "website": "",
+    "category": "Useability",
+    "description": """
+    This module aims at redefining the dashboards and menus for more useability.
+    """,
+    "init_xml": [
+    ],
+    'update_xml': [
+                   "menu/main_menu.xml",
+    ],
+    'demo_xml': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'active': False,
+}
