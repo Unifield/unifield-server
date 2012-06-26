61ce5edebda40947e59281ea4971380a61c3aeec
Olivier DOSSMANN <olivier@tempo-laptop>
2012-06-26 16:24:43 +0200

    UF-1106 [ADD] User-friendly improvement
      * Add onchange on destination for HQ entries
      * Permit to search on FP compatibility regarding account and cost center for HQ Entries

--- account_hq_entries/hq_entries.py
+++ account_hq_entries/hq_entries.py
@@ -372,10 +372,37 @@ class hq_entries(osv.osv):
                 fp_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'analytic_distribution', 'analytic_account_msf_private_funds')[1]
             except ValueError:
                 fp_id = 0
-            fields[0].set('domain', "[('type', '!=', 'view'), ('category', '=', 'FUNDING'), '|', '&', ('cost_center_ids', '=', cost_center_id), ('account_ids', '=', account_id), ('id', '=', %s)]"%(fp_id, ))
+            fields[0].set('domain', "[('type', '!=', 'view'), ('state', '=', 'open'), ('category', '=', 'FUNDING'), '|', '&', ('cost_center_ids', '=', cost_center_id), '&', ('tuple_destination_account_ids.account_id', '=', account_id), ('tuple_destination_account_ids.destination_id', '=', destination_id), ('id', '=', %s)]" % fp_id)
             view['arch'] = etree.tostring(arch)
         return view
 
+    def onchange_destination(self, cr, uid, ids, destination_id=False, funding_pool_id=False, account_id=False):
+        """
+        Check given funding pool with destination
+        """
+        # Prepare some values
+        res = {}
+        # If all elements given, then search FP compatibility
+        if destination_id and funding_pool_id and account_id:
+            fp_line = self.pool.get('account.analytic.account').browse(cr, uid, funding_pool_id)
+            # Search MSF Private Fund element, because it's valid with all accounts
+            try:
+                fp_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'analytic_distribution', 
+                'analytic_account_msf_private_funds')[1]
+            except ValueError:
+                fp_id = 0
+            # Delete funding_pool_id if not valid with tuple "account_id/destination_id".
+            # but do an exception for MSF Private FUND analytic account
+            if (account_id, destination_id) not in [x.account_id and x.destination_id and (x.account_id.id, x.destination_id.id) for x in fp_line.tuple_destination_account_ids] and funding_pool_id != fp_id:
+                res = {'value': {'analytic_id': False}}
+        # If no destination, do nothing
+        elif not destination_id:
+            res = {}
+        # Otherway: delete FP
+        else:
+            res = {'value': {'analytic_id': False}}
+        # If destination given, search if given 
+        return res
 
     def write(self, cr, uid, ids, vals, context=None):
         """
