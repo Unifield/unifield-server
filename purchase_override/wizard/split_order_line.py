6eaa033af8c168d089b5aef286c530a6bb67cec6
chloups208 <chloups208@chloups208-laptop>
2012-07-03 14:42:01 +0200

    uf-1050 the po split wizard detects and displays the corresponding sale order, and a checkbox to choose wheter or not the so is impacted is created

--- purchase_override/wizard/split_order_line.py
+++ purchase_override/wizard/split_order_line.py
@@ -28,16 +28,55 @@ from tools.translate import _
 class split_purchase_order_line_wizard(osv.osv_memory):
     _name = 'split.purchase.order.line.wizard'
     _description = 'Split purchase order lines'
+    
+    def _vals_get(self, cr, uid, ids, fields, arg, context=None):
+        '''
+        multi fields function method
+        '''
+        # Some verifications
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+            
+        # objects
+        sol_obj = self.pool.get('sale.order.line')
+        
+        result = {}
+        for obj in self.browse(cr, uid, ids, context=context):
+            result[obj.id] = {}
+            # has_corresponding_so_line_split_po_line_wizard
+            sol_ids = []
+            if obj.purchase_line_id:
+                if obj.purchase_line_id.procurement_id:
+                    # look for corresponding sale order line
+                    sol_ids = sol_obj.search(cr, uid, [('procurement_id', '=', obj.purchase_line_id.procurement_id.id)], context=context)
+            # true if we get some sale order lines
+            result[obj.id].update({'has_corresponding_so_line_split_po_line_wizard': sol_ids and True or False})
+            # corresponding_so_id_split_po_line_wizard
+            so_id = False
+            if sol_ids:
+                datas = sol_obj.read(cr, uid, sol_ids, ['order_id'], context=context)
+                for data in datas:
+                    if data['order_id']:
+                        so_id = data['order_id'][0]
+            # write the value
+            result[obj.id].update({'corresponding_so_id_split_po_line_wizard': so_id})
+        return result
 
     _columns = {
         'purchase_line_id': fields.many2one('purchase.order.line', string='Line Id', readonly=True),
         'original_qty': fields.float(digits=(16,2), string='Original Quantity', readonly=True),
         'old_line_qty': fields.float(digits=(16,2), string='Old line quantity', readonly=True),
         'new_line_qty': fields.float(digits=(16,2), string='New line quantity', required=True),
+        'impact_so_split_po_line_wizard': fields.boolean('Impact Field Order', help='Impact corresponding Field Order by creating a corresponding Field Order line.'),
+        'has_corresponding_so_line_split_po_line_wizard': fields.function(_vals_get, method=True, type='boolean', string='Has Corresponding So', multi='get_vals_split_po_line', store=False, readonly=True),
+        'corresponding_so_id_split_po_line_wizard': fields.function(_vals_get, method=True, type='many2one', relation='sale.order', string='Corresponding Fo', multi='get_vals_split_po_line', store=False, readonly=True),
     }
 
     _defaults = {
         'new_line_qty': lambda *a: 0.00,
+        'impact_so_split_po_line_wizard': True,
     }
 
     def split_line(self, cr, uid, ids, context=None):
@@ -69,6 +108,14 @@ class split_purchase_order_line_wizard(osv.osv_memory):
                 # Change the qty of the old line
                 line_obj.write(cr, uid, [split.purchase_line_id.id], {'product_qty': split.original_qty - split.new_line_qty,
                                                                       'price_unit': split.purchase_line_id.price_unit,}, context=context)
+                # we treat two different cases
+                # 1) the check box impact corresponding Fo is checked
+                #    we create a Fo line by copying related Fo line. we then execute procurement creation function, and process the procurement
+                #    the merge into the actual Po is forced
+                bool = split.has_corresponding_so_line_split_po_line_wizard
+                
+                # 2) the check box impatc corresponding Fo is not check or does not apply (po from scratch or from replenishment),
+                #    a new line is simply created
                 # Create the new line
                 new_line_id = line_obj.copy(cr, uid, split.purchase_line_id.id, {'parent_line_id': split.purchase_line_id.id,
                                                                                  'change_price_manually': split.purchase_line_id.change_price_manually,
