958b92c9ac7ded8ae2e08cc2f9f4da2c6e036e4b
Matthieu Dietrich <mdietrich@chloups211>
2012-07-10 11:38:46 +0200

    UF-1087: [FIX] backported msf_instance fix

--- msf_instance/add_instance.py
+++ msf_instance/add_instance.py
@@ -101,13 +101,13 @@ class account_move_line(osv.osv):
         'instance_id': fields.many2one('msf.instance', 'Proprietary Instance'),
     }
     
-    def create(self, cr, uid, vals, context=None):
+    def create(self, cr, uid, vals, context=None, check=True):
         if 'journal_id' in vals:
             journal = self.pool.get('account.journal').browse(cr, uid, vals['journal_id'], context=context)
             vals['instance_id'] = journal.instance_id.id
         return super(account_move_line, self).create(cr, uid, vals, context=context)
     
-    def write(self, cr, uid, ids, vals, context=None):
+    def write(self, cr, uid, ids, vals, context=None, check=True, update_check=True):
         if 'journal_id' in vals:
             journal = self.pool.get('account.journal').browse(cr, uid, vals['journal_id'], context=context)
             vals['instance_id'] = journal.instance_id.id
