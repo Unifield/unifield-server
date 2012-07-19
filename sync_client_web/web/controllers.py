4c3567576d531cbaf3a006c97010ed963a16d6f1
tfr@openerp.com <>
2012-07-19 11:00:31 +0200

    [ADD] web client adons

---
+++ sync_client_web/web/controllers.py
@@ -0,0 +1,15 @@
+
+from openobject.tools import expose
+
+import openerp.controllers
+from openerp.utils import rpc, TinyDict
+
+class Client_Sync(openerp.controllers.SecuredController):
+    _cp_path = "/sync_client_web/synchro_client"
+
+    
+    @expose('json')
+    def get_data(self):
+        status = rpc.RPCProxy('sync.client.entity').get_status()
+        upgrade_status = rpc.RPCProxy('sync.client.entity').get_upgrade_status()
+        return dict(status=status,upgrade_status=upgrade_status)
