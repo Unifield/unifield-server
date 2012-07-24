116e8c5b2b384d42768ad66b3263b82b12080317
Cecile Tonglet <cto@openerp.com>
2012-07-24 17:52:21 +0200

    [IMP] Reboot & Update Feature

--- bin/openerp-server.py
+++ bin/openerp-server.py
@@ -30,13 +30,127 @@ GNU Public Licence.
 (c) 2003-TODAY, Fabien Pinckaers - OpenERP s.a.
 """
 
+import os
+import sys
+import psycopg2
+from datetime import datetime
+
+## Unix-like find
+def find(path):
+    files = os.listdir(path)
+    for name in iter(files):
+        abspath = path+os.path.sep+name
+        if os.path.isdir( abspath ) and not os.path.islink( abspath ):
+            files.extend( map(lambda x:name+os.path.sep+x, os.listdir(abspath)) )
+    return files
+
+## We expect to be in the bin/ directory to proceed
+if os.path.exists('update.lock'):
+    rev_file = os.path.join('.update','revisions.txt')
+    hist_file = "revision_history.txt"
+    infos = None
+    revisions = None
+    cur = None
+    conn = None
+    update_revisions = None
+    files = None
+    args = list(sys.argv)
+    for i, x in enumerate(args):
+        if x in ('-d', '-u'):
+            args[i] = None
+            args[i+1] = None
+    args = filter(lambda x:x is not None, args)
+    try:
+        ## Read DB name
+        f = open('update.lock')
+        infos = eval(f.read())
+        f.close()
+        revisions = ",".join( map(lambda x:"'"+str(x)+"'", infos['revisions']) )
+        ## Connect to the DB
+        conn = psycopg2.connect(database=infos['dbname'], user=infos['db_user'], password=infos['db_password'], host=infos['db_host'], port=infos['db_port'])
+        conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        ## Explore .update directory
+        files = find('.update')
+        ## Prepare backup directory
+        if not os.path.exists('backup'):
+            os.mkdir('backup')
+        else:
+            for f in reversed(find('backup')):
+                target = os.path.join('backup', f)
+                if os.path.isfile(target) or os.path.islink(target):
+                    print "rm", target
+                    os.unlink( target )
+                elif os.path.isdir(target):
+                    print "rmdir", target
+                    os.rmdir( target )
+        ## Update Files
+        print "Updating..."
+        for f in files:
+            target = os.path.join('.update', f)
+            bak = os.path.join('backup', f)
+            if os.path.isdir(target):
+                if os.path.isfile(f) or os.path.islink(f):
+                    os.unlink(f)
+                if not os.path.exists(f):
+                    os.mkdir(f)
+                os.mkdir(bak)
+            else:
+                if os.path.exists(f):
+                    print "`%s' -> `%s'" % (f, bak)
+                    os.rename(f, bak)
+                print "`%s' -> `%s'" % (target, f)
+                os.rename(target, f)
+        ## Update installed revisions in DB
+        cur.execute("""UPDATE sync_client_version SET state = 'installed', applied = '%s' WHERE name in (%s)"""
+            % ( datetime.today().strftime("%Y-%m-%d %H:%M:%S"), revisions ))
+        print "Update successful."
+        print "Revisions added: ", ", ".join( infos['revisions'] )
+        args.extend(['-d', infos['dbname'], '-u', 'all'])
+    except:
+        print 'failure!'
+        ## Update DB to mark revisions as not-installed
+        if cur and infos:
+            cur.execute("""UPDATE sync_client_version SET state = 'not-installed' WHERE name in (%s)"""
+                % ( revisions ))
+        ## Restore backup and purge .update
+        if files:
+            print "Restoring..."
+            for f in reversed(files):
+                target = os.path.join('backup', f)
+                if os.path.isfile(target) or os.path.islink(target):
+                    print "`%s' -> `%s'" % (target, f)
+                elif os.path.isdir(target):
+                    print "rmdir", target
+                    os.rmdir( target )
+            print "Purging..."
+            for f in reversed(files):
+                target = os.path.join('.update', f)
+                if os.path.isfile(target) or os.path.islink(target):
+                    print "rm", target
+                    os.unlink( target )
+                elif os.path.isdir(target):
+                    print "rmdir", target
+                    os.rmdir( target )
+            print "rmdir", '.update'
+            os.rmdir( '.update' )
+    finally:
+        if cur: cur.close()
+        if conn: conn.close()
+    ## Remove lock file
+    print "rm", 'update.lock'
+    os.unlink( 'update.lock' )
+    print "Restart OpenERP in", infos['exec_path'], "with: ", list(sys.argv) + args
+    if infos: os.chdir(infos['exec_path'])
+    os.execv(sys.argv[0], args)
+
+print "OpenERP Started with: ", sys.argv
+
 #----------------------------------------------------------
 # python imports
 #----------------------------------------------------------
 import logging
-import os
 import signal
-import sys
 import threading
 import traceback
 
@@ -70,9 +184,9 @@ for name, value in [('addons_path', tools.config['addons_path']),
     logger.info("%s - %s", name, value)
 
 # Don't allow if the connection to PostgreSQL done by postgres user
-if tools.config['db_user'] == 'postgres':
-    logger.error("Connecting to the database as 'postgres' user is forbidden, as it present major security issues. Shutting down.")
-    sys.exit(1)
+#if tools.config['db_user'] == 'postgres':
+#    logger.error("Connecting to the database as 'postgres' user is forbidden, as it present major security issues. Shutting down.")
+#    sys.exit(1)
 
 import time
 
@@ -215,7 +329,7 @@ for signum in SIGNALS:
 if os.name == 'posix':
     signal.signal(signal.SIGQUIT, dumpstacks)
 
-def quit():
+def quit(restart=False):
     netsvc.Agent.quit()
     netsvc.Server.quitAll()
     if tools.config['pidfile']:
@@ -235,7 +349,10 @@ def quit():
                 # and would present the forced shutdown
                 thread.join(0.05)
                 time.sleep(0.05)
-    sys.exit(0)
+    if not restart:
+        sys.exit(0)
+    else:
+        os.execv(sys.argv[0], sys.argv)
 
 if tools.config['pidfile']:
     fd = open(tools.config['pidfile'], 'w')
@@ -247,9 +364,11 @@ netsvc.Server.startAll()
 
 logger.info('OpenERP server is running, waiting for connections...')
 
-while netsvc.quit_signals_received == 0:
-    time.sleep(60)
+tools.restart_required = False
 
-quit()
+while netsvc.quit_signals_received == 0 and not tools.restart_required:
+    time.sleep(5)
+
+quit(restart=tools.restart_required)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
