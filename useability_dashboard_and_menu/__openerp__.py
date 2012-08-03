ac5971a0566f833a7a42c69c01b022b927c98505
matthieu.choplin@geneva.msf.org <>
2012-08-03 13:36:51 +0200

    redisplaying the menu directly (it needed to delete the initial one and to create an other one with a new id)

--- useability_dashboard_and_menu/__openerp__.py
+++ useability_dashboard_and_menu/__openerp__.py
@@ -46,10 +46,11 @@
     "init_xml": [
     ],
     'update_xml': [
+                   "menu/delete_menu.xml",
                    "menu/main_menu.xml",
+                   "menu/supply_config_menu.xml",
                    "menu/product_menu.xml",
                    "menu/purchase_menu.xml",
-                   "menu/supply_config_menu.xml",
                    "menu/warehouse_menu.xml",
                    "menu/partner_menu.xml",
                    "menu/sale_menu.xml",
