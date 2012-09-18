49df54f58c632d26f4d39e1a430b1abcef15689e
chloups208 <chloups208@chloups208-laptop>
2012-09-18 16:56:26 +0200

    uf-1334 import function from file and data structure

--- msf_profile/user_access_configurator.py
+++ msf_profile/user_access_configurator.py
@@ -27,23 +27,43 @@ import tools
 
 from spreadsheet_xml.spreadsheet_xml import SpreadsheetXML
 from msf_supply_doc_import.check_line import *
-from msf_supply_doc_import import MAX_LINES_NB
+
+MAX_LINES_NB = 5000
 
 
 class user_access_configurator(osv.osv_memory):
     _name = 'user.access.configurator'
-    _columns = {'file_to_import_uac': fields.binary(string='File to import', filters='*.xml', help='You can use the template of the export for the format that you need to use. \n The file should be in XML Spreadsheet 2003 format. \n The columns should be in this order : Product Code*, Product Description*, Initial Average Cost, Location*, Batch, Expiry Date, Quantity')}
+    _columns = {'file_to_import_uac': fields.binary(string='File to import', filters='*.xml', help='You can use the template of the export for the format that you need to use. \n The file should be in XML Spreadsheet 2003 format. \n The columns should be in this order : Product Code*, Product Description*, Initial Average Cost, Location*, Batch, Expiry Date, Quantity'),
+                'number_of_non_group_columns_uac': fields.integer(string='Number of columns not containing group name')}
+    _defaults = {#'file_to_import_uac': '/home/chloups208/Dropbox/patrick/unifield/wm/uf1334/merged.xml',
+                 'number_of_non_group_columns_uac': 4}
     
+    def _row_is_empty(self, cr, uid, ids, context=None, *args, **kwargs):
+        """
+        return True if row is empty
+        """
+        row = kwargs['row']
+        return all([not cell.data for cell in row.cells])
+    
+    def _cell_is_true(self, cr, uid, ids, context=None, *args, **kwargs):
+        """
+        return True if row is empty
+        """
+        cell = kwargs['cell']
+        if cell.data and cell.data.upper() == 'YES':
+            return True
+        return False
     
     def import_data_uac(self, cr, uid, ids, context=None):
         '''
         import data and generate data structure
         
         {id: {
-              'group_list': [group_names],
-              'menus_groups': {'menu_name': [group_names]} - we only take the group_name into account if True
+              'group_name_list': [group_names],
+              'menus_groups': {'menu_id': [group_names]}, - we only take the group_name into account if True
+              'errors': ['list of errors'],
+              'warnings': ['list of warnings'],
               }
-        
         '''
         # Some verifications
         if context is None:
@@ -54,10 +74,52 @@ class user_access_configurator(osv.osv_memory):
         # data structure returned with processed data from file
         data_structure = {}
         
-        for id in ids:
+        for obj in self.browse(cr, uid, ids, context=context):
             # data structure returned with processed data from file
-            data_structure.update({id: {'group_list': [], 'menus_groups': {}}})
-            
+            data_structure.update({obj.id: {'group_name_list': [],
+                                            'menus_groups': {},
+                                            'errors': [],
+                                            'warnings': []}})
+            # file to process
+            file = obj.file_to_import_uac
+            # file is mandatory for import process
+            if not file:
+                raise osv.except_osv(_('Warning'), _('No File Selected.'))
+            # load the selected file according to XML OUTPUT
+            fileobj = SpreadsheetXML(xmlstring=base64.decodestring(file))
+            # iterator on rows
+            rows = fileobj.getRows()
+            # first row flag
+            first_row = True
+            # loop the rows for groups-menus relation
+            for row in rows:
+                # skip empty lines
+                if self._row_is_empty(cr, uid, ids, context=context, row=row):
+                    continue
+                
+                # first row, gather group names
+                if first_row:
+                    first_row = False
+                    # skip information rows
+                    for i in range(obj.number_of_non_group_columns_uac, len(row)):
+                        data_structure[obj.id]['group_name_list'].append(row.cells[i].data)
+                else:
+                    # information rows
+                    try:
+                        menu_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, row.cells[0], row.cells[1])[1]
+                    except ValueError:
+                        # menu is in the file but not in the database
+                        data_structure[obj.id]['errors'].append('The menu %s (%s.%s) is missing in the database.'%(row.cells[3], row.cells[0], row.cells[1]))
+                        continue
+                        
+                    # skip information rows - find related groups
+                    for i in range(obj.number_of_non_group_columns_uac, len(row)):
+                        # group is true for this menu
+                        if self._cell_is_true(cr, uid, ids, context=context, cell=row.cells[i]):
+                            # name of group
+                            group_name = data_structure[obj.id]['group_name_list'][i - obj.number_of_non_group_columns_uac]
+                            data_structure[obj.id]['menus_groups'].setdefault(menu_id, []).append(group_name)
+                    
         return data_structure
     
     def process_groups_uac(self, cr, uid, ids, context=None):
@@ -69,6 +131,12 @@ class user_access_configurator(osv.osv_memory):
             context = {}
         if isinstance(ids, (int, long)):
             ids = [ids]
+        
+        # data structure
+        data_structure = context['data_structure']
+        # load the groups from file if not present
+        for group in data_structure[obj.id]['group_name_list']:
+            
         
         return False
     
@@ -129,8 +197,24 @@ class user_access_configurator(osv.osv_memory):
             context = {}
         if isinstance(ids, (int, long)):
             ids = [ids]
-            
+        
+        # gather data structure corresponding to selected file
+        data_structure = self.import_data_uac(cr, uid, ids, context=context)
+        # process the groups
+        self.process_groups_uac(cr, uid, ids, context=dict(context, data_structure=data_structure)
+        
         return {'type': 'ir.actions.act_window_close'}
 
 user_access_configurator()
+
+
+class res_groups(osv.osv):
+    '''
+    add an active column
+    '''
+    _inherit = 'res.groups'
+    _columns = {'active': fields.boolean('Active', readonly=True)}
+    _defaults = {'active': True}
+
+res_groups()
 
