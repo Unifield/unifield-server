06b76a18e39e252aee5bccf4852750135618d67a
Olivier DOSSMANN <olivier@tempo-laptop>
2012-09-20 18:24:31 +0200

    UF-1437 [ADD] HR journals into import invoice
      * Changer filter on Import invoice wizard in order to display HR lines

--- register_accounting/account_move_line.py
+++ register_accounting/account_move_line.py
@@ -69,7 +69,7 @@ class account_move_line(osv.osv):
             ('account_id.type','in',['receivable','payable']),
             ('reconcile_id','=',False), 
             ('state', '=', 'valid'), 
-            ('journal_id.type', 'in', ['purchase', 'sale','purchase_refund','sale_refund']), 
+            ('journal_id.type', 'in', ['purchase', 'sale','purchase_refund','sale_refund', 'hr']), 
             ('account_id.type_for_register', 'not in', ['down_payment']) 
         ]
         return dom1+[('amount_residual_import_inv', '>', 0)]
