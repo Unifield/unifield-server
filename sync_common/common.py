59dbae298accb52bc0e534725b344ba0b1c0f5d2
Samus CTO <cto@openerp.com>
2012-10-01 18:20:32 +0200

    [FIX] Bad use of super()

--- sync_common/common.py
+++ sync_common/common.py
@@ -95,7 +95,7 @@ def sync_log(obj, message=None, level='debug', ids=None, data=None, traceback=Fa
     return output
 
 def __init_logger__(obj, *a, **kw):
-    super(obj.__class__, obj).__init__(obj, *a, **kw)
+    super(obj.__class__, obj).__init__(*a, **kw)
     if not hasattr(obj, '_logger') and (obj._module.startswith('sync') or obj._module.startswith('update_')):
         obj._logger = logging.getLogger(obj._module)
 
