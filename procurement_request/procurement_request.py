5807bbbc658b84df9a6bada195aeeea7d942c6b4
matthieu.choplin@geneva.msf.org <>
2012-10-01 13:59:14 +0200

    [uf-1447] FIX the write: add a more explicit message when no product neither comment have been defined

--- procurement_request/procurement_request.py
+++ procurement_request/procurement_request.py
@@ -371,8 +371,11 @@ class procurement_request_line(osv.osv):
         """
         if context is None:
             context = {}
-        if context.get('procurement_request') and not vals.get('product_id', False) and not vals.get('comment', False) or not vals.get('name', False):
-            raise osv.except_osv(_('Warning !'), _('You should enter either a comment or a Product.'))
+        sol_obj = self.pool.get('sale.order.line')
+        if context.get('procurement_request'):
+            for line in sol_obj.browse(cr, uid, ids, context):
+                if not line.product_id and not line.comment or not line.name:
+                    raise osv.except_osv(_('Warning !'), _('You should enter either a Comment or a Product on the line %s.') % line.line_num)
         return super(procurement_request_line, self).write(cr, uid, ids, vals, context=context)
     
     def _get_fake_state(self, cr, uid, ids, field_name, args, context=None):
