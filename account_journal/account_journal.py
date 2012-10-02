433c9fdba6e060389ec6189e38d4f1c616584e2d
Matthieu Dietrich <matthieu.dietrich@geneva.msf.org>
2012-10-02 12:09:15 +0200

    UTP-193: [FIX] unit test now fixed, as some journals were created without a proprietary instance

--- account_journal/account_journal.py
+++ account_journal/account_journal.py
@@ -45,8 +45,7 @@ class account_journal(osv.osv):
 
         if demo:
             # Search if an engagement journal exists
-            eng_ids = self.pool.get('account.analytic.journal').search(cr, 1, [('type', '=', 'engagement'),
-                                                                               ('instance_id', '=', self.pool.get('res.users').browse(cr, uid, uid).company_id.instance_id.id)])
+            eng_ids = self.pool.get('account.analytic.journal').search(cr, 1, [('type', '=', 'engagement')])
             if not len(eng_ids):
                 logging.getLogger('init').info('HOOK: module account_journal: loading account_journal_demo.xml')
                 pathname = path.join('account_journal', 'account_journal_demo.xml')
