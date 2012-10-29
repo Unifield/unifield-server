a3b18fa9bb5b359538ce3617d2de15130a2dc384
matthieu.choplin@geneva.msf.org <>
2012-10-29 15:47:13 +0100

    [uf-1544] set the default_code required in the view but not in the python file

--- product_list/product_list.py
+++ product_list/product_list.py
@@ -230,7 +230,8 @@ class product_product(osv.osv):
     _columns = {
         'list_ids': fields.function(_get_list_sublist, fnct_search=_search_list_sublist, 
                                     type='many2many', relation='product.list', method=True, string='Lists'),
-        'default_code' : fields.char('CODE', size=14, required=True, select=True),
+        # we can't write the default_code required because it is used in the product addons
+        'default_code' : fields.char('CODE', size=14, select=True),
     }
 
     _sql_constraints = [
