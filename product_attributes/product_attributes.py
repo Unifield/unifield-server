1705911d6f19438700d631cd46491c1ef8157e1f
matthieu.choplin@geneva.msf.org <>
2012-10-29 17:49:00 +0100

    [uf-1544] set the required fields in python file with adaptation to pass loading of yml + data

--- product_attributes/product_attributes.py
+++ product_attributes/product_attributes.py
@@ -258,8 +258,33 @@ class product_attributes(osv.osv):
             if product.gmdn_code and not int_pattern.match(product.gmdn_code):
                 return False
         return True
-    
+
+    def _get_default_req(self, cr, uid, context=None):
+        # Some verifications
+        if context is None:
+            context = {}
+        res = {}
+        res= {'default_code': datetime.now().strftime('%m%d%H%M%S'),
+              'international_status': 'itc'}
+        return res
+
     def create(self, cr, uid, vals, context=None):
+        '''
+        Set default values for datas.xml and tests.yml
+        '''
+        if context is None:
+            context = {}
+        if context.get('update_mode') in ['init', 'update']:
+            required = ['default_code', 'international_status']
+            has_required = False
+            for req in required:
+                if  req in vals:
+                    has_required = True
+                    break
+            if not has_required:
+                logging.getLogger('init').info('Loading default values for product.product')
+                vals.update(self._get_default_req(cr, uid, context))
+        logging.getLogger('init').info('Value of %s' % vals)
         if 'batch_management' in vals:
             vals['track_production'] = vals['batch_management']
             vals['track_incoming'] = vals['batch_management']
@@ -329,36 +354,4 @@ class product_attributes(osv.osv):
 
 product_attributes()
 
-class product_template(osv.osv):
-    _inherit = "product.template"
-    
-    def _get_default_req(self, cr, uid, context=None):
-        # Some verifications
-        if context is None:
-            context = {}
-        res = {}
-        res= {'default_code': datetime.now().strftime('%m%d%H%M%S'),
-              'international_status': 'itc'}
-        return res
-
-    def create(self, cr, uid, vals, context=None):
-        '''
-        Set default values for datas.xml and tests.yml
-        '''
-        if context is None:
-            context = {}
-        if context.get('update_mode') in ['init', 'update']:
-            required = ['default_code', 'international_status']
-            has_required = False
-            for req in required:
-                if  req in vals:
-                    has_required = True
-                    break
-            if not has_required:
-                logging.getLogger('init').info('Loading default values for product.product')
-                vals.update(self._get_default_req(cr, uid, context))
-        logging.getLogger('init').info('Value of %s' % vals)
-        return super(product_template, self).create(cr, uid, vals, context)
-
-product_template()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
