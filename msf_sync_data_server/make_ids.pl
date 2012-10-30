e7b4d776459b2e9c4a354565fe9cfe52a83d0b4f
Samus CTO <cto@openerp.com>
2012-10-30 14:11:01 +0100

    [IMP] make_ids.pl now documented and can re-calculate ids (in order to see any change)

--- msf_sync_data_server/make_ids.pl
+++ msf_sync_data_server/make_ids.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/env perl
+#
+#  This small script add column 'id' to CSV files based on column 'name'
+#
+#  Usage: ./make_ids.pl <csv_file1> <csv_file2> ...
+#
 
 use strict;
 use warnings;
-use Data::Dumper;
 
-sub csv_split { #split /,/, @_ ? shift() : $_ }
+## Small helpful function to split lines into rows
+## Accept only these formats:
+##   - value1,value2,value3
+##   - "value1","value2","value3"
+sub csv_split {
     local $_ = @_ ? shift() : $_;
     chomp;
     $_ .= ',';
@@ -19,46 +27,41 @@ sub csv_split { #split /,/, @_ ? shift() : $_ }
 
 my @files;
 
+## Parse CSV file
 while( $_ = shift @ARGV ) {
     open IN,$_ or die "Cannot open $_: $!\n";
     my %f = (datas => [], file => $_);
     $_ = <IN>;
     $f{header} = [csv_split($_)];
     if( grep {$_ eq 'id'} @{$f{header}} ) {
-        warn "Skipping file `$f{file}'\n";
-        next;
+        warn "The file `$f{file}' already have a column id, I will re-calculate it.\n";
     }
     my $cols = $#{$f{header}};
     while( <IN> ) {
         my @line = csv_split;
         push @{$f{datas}}, {map {$f{header}->[$_], $line[$_]} 0..$cols};
-        #warn join('###', @line)."\n";
     }
     push @files, \%f;
-    #warn Dumper($f{header});
 }
 
+## Create column 'id' and write back to the file
 for my $f (@files) {
     my %known;
     for my $data (@{$f->{datas}}) {
-        #die Dumper($data);
         $_ = lc($data->{name} or $data->{sequence});
         tr/ ./__/;
-        s/[^a-z0-9-_]//g;
+        s/[^a-z0-9_-]//g;
         s/__+/_/g;
         $data->{id} = $_;
         die "This tag already exists: $_\n" if exists $known{$_};
         $known{$_} = 1;
     }
-    unshift @{$f->{header}}, 'id';
+    unshift @{$f->{header}}, 'id' unless grep {$_ eq 'id'} @{$f->{header}};
     open OUT,'>',$f->{file} or die "Cannot write to `$f->{file}': $!\n";
     print OUT join(',', map {s/"/""/;"\"$_\""} @{$f->{header}})."\n";
     for my $data (@{$f->{datas}}) {
         $_ = join(',', map {s/"/""/;"\"$data->{$_}\""} @{$f->{header}})."\n";
         print OUT;
     }
-    #warn Dumper(\%known);
 }
-
-#warn Dumper(@files);
 
