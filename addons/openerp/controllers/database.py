eff548ea23f3d271b91f4b64a82ea9ed712dd38e
jf <jfb@tempo-consulting.fr>
2012-11-15 09:31:02 +0100

    AIO-28 [IMP?] Database restore lp:~unifield-team/unifield-web/web_aio_28

--- addons/openerp/controllers/database.py
+++ addons/openerp/controllers/database.py
@@ -100,7 +100,7 @@ class FormRestore(DBForm):
     submit_text = _('Restore')
     fields = [openobject.widgets.FileField(name="filename", label=_('File:')),
               openobject.widgets.PasswordField(name='password', label=_('Password:'), validator=formencode.validators.NotEmpty()),
-              openobject.widgets.TextField(name='dbname', label=_('New database name:'), validator=formencode.validators.NotEmpty())]
+              openobject.widgets.TextField(name='dbname', label=_('New database name:'), validator=formencode.validators.NotEmpty(), readonly=1, attrs={'disabled': 'disabled'})]
 
 class FormPassword(DBForm):
     name = "password"
@@ -254,7 +254,7 @@ class Database(BaseController):
             return self.backup()
         raise redirect('/openerp/login')
 
-    @expose(template="/openerp/controllers/templates/database.mako")
+    @expose(template="/openerp/controllers/templates/database_restore.mako")
     def restore(self, tg_errors=None, **kw):
         form = _FORMS['restore']
         error = self.msg
@@ -264,8 +264,17 @@ class Database(BaseController):
     @expose()
     @validate(form=_FORMS['restore'])
     @error_handler(restore)
-    def do_restore(self, filename, password, dbname, **kw):
+    def do_restore(self, filename, password, dbname=None, **kw):
         self.msg = {}
+        if getattr(filename, 'filename', ''):
+            submitted_filename = filename.filename
+            matches = re.search('^(.*)-[0-9]{8}-[0-9]{6}.dump$', submitted_filename)
+            if matches:
+                dbname = matches.group(1)
+            else:
+                self.msg = {'message': _('The choosen file in not a valid database file'),
+                            'title': _('Error')}
+                return self.restore()
         try:
             data = base64.encodestring(filename.file.read())
             rpc.session.execute_db('restore', password, dbname, data)
