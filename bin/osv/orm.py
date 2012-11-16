aba576dc57cae752fb67c10fa8347b8451786441
Olivier DOSSMANN <od@tempo-consulting.fr>
2012-11-16 11:31:41 +0100

    UTP-168 [FIX] Split method not found on dict object for field.reference when attempting to export a field.reference
      * Bug find on "partner_type" field on register lines. It blocks export of register lines.

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -265,7 +265,13 @@ class browse_record(object):
                             if isinstance(result_line[field_name], browse_record):
                                 new_data[field_name] = result_line[field_name]
                             else:
-                                ref_obj, ref_id = result_line[field_name].split(',')
+                                if isinstance(result_line[field_name], dict):
+                                    ref_obj, ref_id = result_line[field_name].get('selection', ',').split(',')
+                                else:
+                                    ref_obj, ref_id = result_line[field_name].split(',')
+                                if not ref_id:
+                                    new_data[field_name] = browse_null()
+                                    continue
                                 ref_id = long(ref_id)
                                 if ref_id:
                                     obj = self._table.pool.get(ref_obj)
