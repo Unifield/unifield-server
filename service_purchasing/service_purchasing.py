b21fa1b21b55b371e906532428508a1cbfea3428
Quentin THEURET <qt@tempo-consulting.fr>
2012-11-19 10:02:58 +0100

    UF-1515 [FIX] Fix unit tests

--- service_purchasing/service_purchasing.py
+++ service_purchasing/service_purchasing.py
@@ -238,7 +238,7 @@ class stock_move(osv.osv):
                 elif obj.picking_id and obj.picking_id.type == 'out' and obj.picking_id.subtype in ('standard', 'picking'):
                     if not obj.location_id.cross_docking_location_ok:
                         raise osv.except_osv(_('Error'), _('Service Products must have Cross Docking Location as Source Location.'))
-            elif obj.location_dest_id.service_location:
+            elif obj.location_dest_id.service_location or obj.location_id.service_location:
                 raise osv.except_osv(_('Error'), _('Service Location cannot be used for non Service Products.'))
         return True
     
