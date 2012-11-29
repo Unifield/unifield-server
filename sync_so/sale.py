9570db68a589c41d17c4bc820426fab6eac43720
matthieu.choplin@geneva.msf.org <>
2012-11-29 17:20:38 +0100

    [utp-360] replace the write of {'confirmed_delivery_date': False} to place it directly in the create

--- sync_so/sale.py
+++ sync_so/sale.py
@@ -47,7 +47,11 @@ class sale_order_sync(osv.osv):
         so_po_common.retrieve_so_header_data(cr, uid, source, header_result, po_dict, context)
         header_result['order_line'] = so_po_common.get_lines(cr, uid, po_info, False, False, False, True, context)
         # [utp-360] we set the confirmed_delivery_date to False directly in creation and not in modification
-        header_result['order_line'] = [line[2].update({'confirmed_delivery_date': False}) for line in header_result['order_line']]
+        order_line = []
+        for line in header_result['order_line']:
+            line[2].update({'confirmed_delivery_date': False})
+            order_line.append((0, 0, line[2]))
+        header_result['order_line'] = order_line
         
         default = {}
         default.update(header_result)
