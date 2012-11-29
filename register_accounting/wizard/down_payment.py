4dc5dea8825e77d85bf6149119c21f444e163ab3
Olivier DOSSMANN <od@tempo-consulting.fr>
2012-11-29 15:20:08 +0100

    UTP-34 [FIX] Error message changed
      * For down payment error @register line write/hard post

--- register_accounting/wizard/down_payment.py
+++ register_accounting/wizard/down_payment.py
@@ -80,7 +80,7 @@ class wizard_down_payment(osv.osv_memory):
             total -= inv.get('amount_total', 0.0)
         if (-1 * absl.amount) > total:
             raise osv.except_osv(_('Warning'), 
-                _('Maximum amount should be: %s. Register line amount is higher than (PO - DPs - open/paid INV).') % (total))
+                _('Maximum amount should be: %s. Register line amount is higher than (PO - unexpended DPs - open/paid INV).') % (total))
         return True
 
     def button_validate(self, cr, uid, ids, context=None):
