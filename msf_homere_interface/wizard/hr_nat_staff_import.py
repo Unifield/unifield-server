9e9c7f037746a64c695d83578df62ca3279923a1
Olivier DOSSMANN <od@tempo-consulting.fr>
2012-12-04 19:05:25 +0100

    [WIP] work in progress - Employee national staff update via XML Spreadsheet import

--- msf_homere_interface/wizard/hr_nat_staff_import.py
+++ msf_homere_interface/wizard/hr_nat_staff_import.py
@@ -37,6 +37,49 @@ class hr_nat_staff_import_wizard(osv.osv_memory):
         'filename': fields.char(string="Imported filename", size=256),
     }
 
+    def update_or_create_employee(self, cr, uid, vals=None, context=None):
+        """
+        Update employee if exists. Otherwise create it.
+        Also check values regarding National Staff employees:
+         - name: mandatory field
+         - identification_id: mandatory field for identification_id
+         - job: search job if exists, else create it.
+         - destination: search destination code or name
+         - cost center: like destination
+         - funding pool: like destination
+         - free1: like destination
+         - free2: like destination
+        """
+        # Some checks
+        if not context:
+            context = {}
+        if not vals:
+            return {}
+        # Prepare some values
+        employee_id = False
+        # Check mandatory fields
+        if not vals.get('name', False):
+            raise osv.except_osv(_('Error'), _('Name is a mandatory field!'))
+        if not vals.get('identification_id', False):
+            raise osv.except_osv(_('Error'), _('Code is a mandatory field!'))
+        # Search employee and check double identification_id
+        employee_ids = self.pool.get('hr.employee').search(cr, uid, [('employee_type', '=', 'local'), ('identification_id', '=', vals.get('identification_id'))])
+        if employee_ids and len(employee_ids) > 1:
+            employees = ','.join([x.get('name', False) and x.get('name') for x in self.pool.get('hr.employee').read(cr, uid, employee_ids, ['name'])])
+            raise osv.except_osv(_('Warning'), _('More than one employee have the same code: %s') % (employees or '',))
+        elif employee_ids:
+            employee_id = employee_ids[0]
+        # Search job
+        if vals.get('job', False):
+            job_ids = self.pool.get('hr.job').search(cr, uid, [('name', '=', vals.get('job'))])
+            if job_ids:
+                vals.update({'job_id': job_ids[0]})
+            else:
+                job_id = self.pool.get('hr.job').create(cr, uid, {'name': vals.get('job')})
+                vals.update({'job_id': job_id})
+        del(vals['job'])
+        return vals
+
     def button_validate(self, cr, uid, ids, context=None):
         """
         Import XLS file
@@ -51,6 +94,7 @@ class hr_nat_staff_import_wizard(osv.osv_memory):
             created = 0
             updated = 0
             processed = 0
+            errors = []
             # Check that a file is given
             if not wiz.file:
                 raise osv.except_osv(_('Error'), _('No file given'))
@@ -61,14 +105,29 @@ class hr_nat_staff_import_wizard(osv.osv_memory):
             fileobj = SpreadsheetXML(xmlstring=decodestring(wiz.file))
             reader = fileobj.getRows()
             reader.next()
-            for line in reader:
+            start = 1
+            column_list = ['name', 'identification_id', 'job', 'dest', 'cc', 'fp', 'f1', 'f2']
+            for num, line in enumerate(reader):
                 processed += 1
-                name = line.cells and line.cells[0] and line.cells[0].data or False
-                if not name:
-                    continue
-                code = line.cells and line.cells[1] and line.cells[1].data or False
-                # Create Expat employee
-                self.pool.get('hr.employee').create(cr, uid, {'name': line.cells[0].data, 'active': True, 'type': 'local', 'identification_id': code})
+                # Fetch values
+                vals = {}
+                if line.cells:
+                    for i, el in enumerate(column_list):
+                        if len(line.cells) > i:
+                            vals[el] = str(line.cells[i])
+                        else:
+                            vals[el] = False
+                # Check values
+                try:
+                    vals = self.update_or_create_employee(cr, uid, vals, context)
+                except osv.except_osv, e:
+                    errors.append('Line %s, %s' % (start+num, e.value))
+#                name = line.cells and line.cells[0] and line.cells[0].data or False
+#                if not name:
+#                    continue
+#                code = line.cells and line.cells[1] and line.cells[1].data or False
+#                # Create Expat employee
+#                self.pool.get('hr.employee').create(cr, uid, {'name': line.cells[0].data, 'active': True, 'type': 'local', 'identification_id': code})
                 created += 1
             
             context.update({'message': ' '})
