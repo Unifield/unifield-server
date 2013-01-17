70a95d997df3f33f5561b9343d16239288557b1f
Olivier DOSSMANN <od@tempo-consulting.fr>
2013-01-17 15:43:57 +0100

    UF-1579 [FIX] Do not allow to add more than 1 currency rate per date for a given currency

--- account_override/res_currency.py
+++ account_override/res_currency.py
@@ -4,6 +4,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011 TeMPO Consulting, MSF. All Rights Reserved
+#    All Rigts Reserved
 #    Developer: Olivier DOSSMANN
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,14 +21,28 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from osv import osv
+from osv import fields
 
-import res_currency
-import account
-import invoice
-import account_voucher
-import account_move_line
-import account_analytic_line
-import account_bank_statement
-import report
+class res_currency_rate(osv.osv):
+  _name = 'res.currency.rate'
+  _inherit = 'res.currency.rate'
 
+  def _check_rate_unicity(self, cr, uid, ids, context=None):
+      """
+      Check that no rate exists on the same currency with the same date
+      """
+      if not context:
+        context = {}
+      for rate in self.browse(cr, uid, ids):
+          rate_ids = self.search(cr, uid, [('name', '=', rate.name), ('currency_id', '=', rate.currency_id.id)])
+          if rate_ids and len(rate_ids) > 1:
+              return False
+      return True
+
+  _constraints = [
+      (_check_rate_unicity, "Only one rate per date is accorded.", ['currency_id', 'name']),
+  ]
+
+res_currency_rate()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
