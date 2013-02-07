013131f7fc827dd84f348e8f1904220dd0851b59
matthieu.choplin@geneva.msf.org <>
2013-02-07 18:13:29 +0100

    [IMP] utp-129: update the default_get for taking the uom of the product_supplierinfo when creating a pricelist_partnerinfo

--- supplier_catalogue/product.py
+++ supplier_catalogue/product.py
@@ -150,7 +150,7 @@ class pricelist_partnerinfo(osv.osv):
     def default_get(self, cr, uid, fields, context=None):
         '''
         Set automatically the currency of the line with the default
-        purchase currency of the supplier
+        purchase currency of the supplier, and the uom with the uom of the product.supplierinfo.
         '''
         if not context:
             context = {}
@@ -161,6 +161,9 @@ class pricelist_partnerinfo(osv.osv):
             partner = self.pool.get('res.partner').browse(cr, uid, context.get('partner_id'), context=context)
             res['currency_id'] = partner.property_product_pricelist_purchase.currency_id.id
             res['partner_id'] = partner.id
+        if context.get('active_id', False) and isinstance(context['active_id'], (int, long)):
+            read_partnerinfo = self.pool.get('product.supplierinfo').read(cr, uid, context['active_id'])
+            res['uom_id'] = read_partnerinfo['product_uom'][0]
         
         return res
     
