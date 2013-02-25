71a6339ad1e5220f125ced52d137e40dedc7a449
Quentin THEURET <qt@tempo-consulting.fr>
2013-02-25 09:20:32 +0100

    UF-1825 [FIX] Don't change subflow if there is ont main_ids

--- bin/workflow/wkf_service.py
+++ bin/workflow/wkf_service.py
@@ -87,7 +87,7 @@ class workflow_service(netsvc.Service):
     def trg_change_subflow(self, uid, main_type, main_ids, res_type, res_ids, new_rid, cr):
         # get ids of wkf instances for the old resource (res_id)
 #CHECKME: shouldn't we get only active instances?
-        if not res_ids:
+        if not res_ids or not main_ids:
             return
 
         cr.execute('select id, wkf_id from wkf_instance where res_id in %s and res_type=%s', (tuple(res_ids), res_type))
