803a6260c7ec278bd5a4a972c6e69c15e175ecf7
matthieu.choplin@geneva.msf.org <>
2013-03-01 15:33:25 +0100

    [YML test] uf-1834: add assertion for the po and fo import in French

--- msf_supply_doc_import/__openerp__.py
+++ msf_supply_doc_import/__openerp__.py
@@ -53,12 +53,12 @@
 # test for the import in French
 #        'test/fr_import_ir.yml',
 #        'test/fr_import_po.yml',
-#        'test/fr_import_so.yml',
+        'test/fr_import_so.yml',
 #        'test/fr_import_tender.yml',
 # test for the import in English
 #        'test/import_ir.yml',
-        'test/import_po.yml',
-        'test/import_so.yml',
+#        'test/import_po.yml',
+#        'test/import_so.yml',
 #        'test/import_composition_kit.yml',
 #        'test/import_tender.yml',
     ],
