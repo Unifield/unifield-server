f55f7584108463df1e010cf8b25772437be1fee2
matthieu.choplin@geneva.msf.org <>
2013-03-04 16:35:41 +0100

    [IMP] utp-518: add the comment on the PO coming from a FO

--- purchase_msf/purchase_msf.py
+++ purchase_msf/purchase_msf.py
@@ -39,12 +39,19 @@ class purchase_order_line(osv.osv):
         '''
         update the name attribute if a product is selected
         '''
+        sol_obj = self.pool.get('sale.order')
         prod_obj = self.pool.get('product.product')
         if vals.get('product_id'):
             vals.update(name=prod_obj.browse(cr, uid, vals.get('product_id'), context=context).name,)
         elif vals.get('comment'):
             vals.update(name=vals.get('comment'),)
-            
+        # utp-518:we write the comment from the sale.order.line on the PO line through the procurement
+        po_procurement_id = vals.get('procurement_id', False)
+        if po_procurement_id:
+            sale_id = sol_obj.search(cr, uid, [('procurement_id', '=', po_procurement_id)], context=context)
+            if sale_id:
+                comment_so = sol_obj.read(cr, uid, sale_id, ['comment'], context=context)[0]['comment']
+                vals.update(comment=comment_so)
         return super(purchase_order_line, self).create(cr, uid, vals, context=context)
     
     def write(self, cr, uid, ids, vals, context=None):
