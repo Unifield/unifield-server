0a3ad7c791da19d3fa6799021b4dbc40c19fdf37
Quentin THEURET <qt@tempo-consulting.fr>
2013-03-06 13:57:31 +0100

    UF-1847 [FIX] Add context on _get_frequence_name on procurement_cycle to use French

--- procurement_cycle/procurement.py
+++ procurement_cycle/procurement.py
@@ -82,7 +82,7 @@ class stock_warehouse_order_cycle(osv.osv):
         res = {}
         for proc in self.browse(cr, uid, ids):
             if proc.frequence_id:
-                res[proc.id] = self.pool.get('stock.frequence').name_get(cr, uid, [proc.frequence_id.id])[0][1]
+                res[proc.id] = self.pool.get('stock.frequence').name_get(cr, uid, [proc.frequence_id.id], context=context)[0][1]
             
         return res
     
