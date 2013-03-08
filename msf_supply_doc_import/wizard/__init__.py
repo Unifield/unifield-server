bbd026e639a26858cfdc79d4b9498ddf54465291
matthieu.choplin@geneva.msf.org <>
2013-03-08 12:25:41 +0100

    [IMP] uf-1615: adapt the number of columns for export and precise the type of date for the date columnsmsf_supply_doc_import/purchase_order.py

--- msf_supply_doc_import/wizard/__init__.py
+++ msf_supply_doc_import/wizard/__init__.py
@@ -21,10 +21,10 @@
 
 # if you update a file in PO_COLUMNS_HEADER_FOR_INTEGRATION, you also need to modify the method export_po_integration, get_po_row_values and get_po_header_row_values.
 PO_COLUMNS_HEADER_FOR_INTEGRATION=[
-('Line*', 'number'), ('Product Code*', 'string'), ('Quantity*', 'number'), ('UoM*', 'string'), ('Price*', 'number'), 
-('Delivery Confirmed Date*', 'string'), ('Order Reference*', 'string'), ('Delivery Confirmed Date (PO)*', 'string'),
+('Line*', 'number'), ('Product Code*', 'string'), ('Product Description', 'string'), ('Quantity*', 'number'), ('UoM*', 'string'), ('Price*', 'number'), ('Delivery Request Date', 'date'),
+('Delivery Confirmed Date*', 'date'), ('Order Reference*', 'string'), ('Delivery Confirmed Date (PO)*', 'date'),
 ('Comment', 'string'), ('Supplier Reference', 'string'), ('Origin', 'string'), ('Notes', 'string'), ('Est. Transport Lead Time', 'number'), ('Transport Mode', 'string'), 
-('Destination Partner', 'string'), ('Destination Address', 'string'), ('Invoicing Address', 'string'), ('Arrival Date in the country', 'string'),
+('Destination Partner', 'string'), ('Destination Address', 'string'), ('Invoicing Address', 'string'), ('Arrival Date in the country', 'date'),
 ('Incoterm', 'string'), ('Notes (PO)', 'string')]
 
 PO_COLUMNS_FOR_INTEGRATION = [x for (x, y) in PO_COLUMNS_HEADER_FOR_INTEGRATION]
