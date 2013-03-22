48dccaec1a431da6d0763368a6f8bbe805f49485
matthieu.choplin@geneva.msf.org <>
2013-03-22 15:10:35 +0100

    [Refactore] utp-129: fix argument in write of po that call the check_uom

--- purchase_msf/purchase_msf.py
+++ purchase_msf/purchase_msf.py
@@ -80,8 +80,8 @@ class purchase_order_line(osv.osv):
         res = super(purchase_order_line, self).write(cr, uid, ids, vals, context=context)
         if not context.get('import_in_progress', False):
             for pol_read in self.read(cr, uid, ids, ['product_id', 'product_uom']):
-                product_id = pol_read['product_id']
-                uom_id = pol_read['product_uom']
+                product_id = pol_read['product_id'][0]
+                uom_id = pol_read['product_uom'][0]
                 if not self.pool.get('uom.tools').check_uom(cr, uid, product_id, uom_id, context):
                     raise osv.except_osv(_('Error'), _('You have to select a product UOM in the same category than the purchase UOM of the product !'))
 
