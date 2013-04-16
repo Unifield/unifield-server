f1149b6bec57ef0b9665b2772ba3bf340d05abcd
mmu-openerp <mmu@openerp.com>
2013-04-16 15:13:47 +0200

    [IMP] rename *horizontal* columns to usb

--- sync_remote_warehouse_server/sync_rule.py
+++ sync_remote_warehouse_server/sync_rule.py
@@ -5,14 +5,14 @@ class SyncRule(osv.osv):
     _name = 'sync_server.sync_rule'
     _columns = {
         # Specifies that this rule is a rule for USB synchronisations
-        'horizontal': fields.boolean('Remote Warehouse Rule', help='Should this rule be used when using the USB Synchronization engine?', required=True),
+        'usb': fields.boolean('Remote Warehouse Rule', help='Should this rule be used when using the USB Synchronization engine?', required=True),
         
         # specifies the direction of the USB synchronisation - like the 'direction' field
         'direction_remote_warehouse': fields.selection((('rw_to_cp', 'Remote Warehouse to Central Platform'), ('cp_to_rw', 'Central Platform to Remote Warehouse'), ('bidirectional','Bidirectional')), 'Direction', help='The direction of the synchronization', required=True),
     }
     
     _defaults = {
-        'horizontal': False,
+        'usb': False,
         'direction_remote_warehouse': 'bidirectional',
     }
     
