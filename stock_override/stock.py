a3d25982910182cc8f05ecd4bd572f1e1acd9df6
Quentin THEURET <qt@tempo-consulting.fr>
2013-05-03 16:15:11 +0200

    UTP-289 [IMP] IN Reception : If the UoM is changed, change the Unit price on generated invoice according to the ration of the ordered and the received UoMs

--- stock_override/stock.py
+++ stock_override/stock.py
@@ -638,6 +638,19 @@ class stock_picking(osv.osv):
                     # Create invoice
                     self.action_invoice_create(cr, uid, [sp.id], journal_ids[0], False, inv_type, {})
         return res
+    
+    def _get_price_unit_invoice(self, cr, uid, move_line, type):
+        '''
+        Update the Unit price according to the UoM received and the UoM ordered
+        '''
+        res = super(stock_picking, self)._get_price_unit_invoice(cr, uid, move_line, type)
+        if move_line.purchase_line_id:
+            po_uom_id = move_line.purchase_line_id.product_uom.id
+            move_uom_id = move_line.product_uom.id
+            uom_ratio = self.pool.get('product.uom')._compute_qty(cr, uid, po_uom_id, 1, move_uom_id)
+            return res/uom_ratio
+        
+        return res
 
     def action_invoice_create(self, cr, uid, ids, journal_id=False, group=False, type='out_invoice', context=None):
         """
