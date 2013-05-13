3f5d1d085a6fbf187ead94e71e205f4a8730492e
Quentin THEURET <qt@tempo-consulting.fr>
2013-05-13 14:00:44 +0200

    UF-1763 [FIX] Scheduler : Search the unit price on the product supplier price list with the good qty and the good UoM instead of the PO UoM  at POs creation

--- sourcing/sourcing.py
+++ sourcing/sourcing.py
@@ -1020,6 +1020,10 @@ class procurement_order(osv.osv):
         if line.get('price_unit', False) == False:
             st_price = self.pool.get('product.product').browse(cr, uid, line['product_id']).standard_price
             if 'pricelist' in kwargs:
+                if 'procurement' in kwargs and 'partner_id' in context:
+                    procurement = kwargs['procurement']
+                    pricelist = kwargs['pricelist']
+                    st_price = self.pool.get('product.pricelist').price_get(cr, uid, [pricelist.id], procurement.product_id.id, procurement.product_qty, context['partner_id'], {'uom': line.get('product_uom', procurement.product_id.uom_po_id.id)})[pricelist.id]
                 cur_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.currency_id.id
                 st_price = self.pool.get('res.currency').compute(cr, uid, cur_id, kwargs['pricelist'].currency_id.id, st_price, round=False, context=context)
             line.update({'price_unit': st_price})
