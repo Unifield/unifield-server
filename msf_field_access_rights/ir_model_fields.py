3f1d0df77ecca941b8fece458c2594b6d9c5c254
mmu-openerp <mmu@openerp.com>
2013-05-14 14:26:38 +0200

    [FIX] nonetype object is not iterable on import

--- msf_field_access_rights/ir_model_fields.py
+++ msf_field_access_rights/ir_model_fields.py
@@ -4,9 +4,10 @@ class ir_model_field(osv.osv):
     _inherit = 'ir.model.fields'
     
     def _modify_search_args(self, args):
-        for index, arg in enumerate(args):
-            if isinstance(arg, (list, tuple)) and arg[0] == 'model_id' and arg[1] == 'in' and isinstance(arg[2], (list, tuple)) and isinstance(arg[2][0], tuple) and len(arg[2][0]) == 3 and arg[2][0][0] ==6:
-                args[index] = ('model_id', 'in', arg[2][0][2])
+        if hasattr(args, '__iter__'):
+            for index, arg in enumerate(args):
+                if isinstance(arg, (list, tuple)) and arg[0] == 'model_id' and arg[1] == 'in' and isinstance(arg[2], (list, tuple)) and isinstance(arg[2][0], tuple) and len(arg[2][0]) == 3 and arg[2][0][0] == 6:
+                    args[index] = ('model_id', 'in', arg[2][0][2])
         return args
    
     def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
