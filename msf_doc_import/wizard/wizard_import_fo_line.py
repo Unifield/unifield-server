72f1d1ae6bf39508665c78deb79b00463f809646
Quentin THEURET <qt@tempo-consulting.fr>
2013-05-23 16:20:16 +0200

    UTP-685 [FIX] Fix error when importing lines on FO/IR with a quantity = 0.00

--- msf_doc_import/wizard/wizard_import_fo_line.py
+++ msf_doc_import/wizard/wizard_import_fo_line.py
@@ -185,6 +185,16 @@ class wizard_import_fo_line(osv.osv_memory):
                     })
                     # we check consistency on the model of on_change functions to call for updating values
                     sale_line_obj.check_data_for_uom(cr, uid, ids, to_write=to_write, context=context)
+
+                    if to_write.get('product_uom_qty', 0.00) <= 0.00:
+                        error_log += _("Line %s in the Excel file was ignored. Details: %s") % (line_num, _('Product Quantity must be greater than zero.'))
+                        line_with_error.append(wiz_common_import.get_line_values(cr, uid, ids, row, cell_nb=False, error_list=error_list, line_num=line_num, context=context))
+                        ignore_lines += 1
+                        line_ignored_num.append(line_num)
+                        percent_completed = float(line_num)/float(total_line_num-1)*100.0
+                        cr.rollback()
+                        continue
+
                     # write order line on FO
                     vals['order_line'].append((0, 0, to_write))
                     if sale_obj._check_service(cr, uid, fo_id, vals, context=context):
