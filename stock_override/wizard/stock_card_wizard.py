b8681c90504ac36c5f317d26248c89d70fe71690
Quentin THEURET <qt@tempo-consulting.fr>
2013-07-04 15:45:05 +0200

    UF-2071 [ADD] Stock card lines : Add the document 'stock.card.line'

--- stock_override/wizard/stock_card_wizard.py
+++ stock_override/wizard/stock_card_wizard.py
@@ -26,7 +26,7 @@ import time
 
 class stock_card_wizard(osv.osv_memory):
     _name = 'stock.card.wizard'
-    _description = 'Print stock card'
+    _description = 'Stock card'
 
     _columns = {
         'location_id': fields.many2one('stock.location', string='Location', 
@@ -98,3 +98,21 @@ class stock_card_wizard(osv.osv_memory):
         raise NotImplementedError
 
 stock_card_wizard()
+
+
+class stock_card_wizard_line(osv.osv_memory):
+    _name = 'stock.card.wizard.line'
+    _description = 'Stock card line'
+
+    _columns = {
+        'date_done': fields.datetime(string='Date'),
+        'picking_id': fields.many2one('stock.picking', string='Doc. Ref.'),
+        'origin': fields.char(size=64, string='Origin'),
+        'qty_in': fields.float(digits=(16,2), string='Qty IN'),
+        'qty_out': fields.float(digits=(16,2), string='Qty OUT'),
+        'balance': fields.float(digits=(16,2), string='Balance'),
+        'location_id': fields.many2one('stock.location', string='Source/Destination'),
+        'notes': fields.text(string='Notes'),
+    }
+
+stock_card_wizard_line()
