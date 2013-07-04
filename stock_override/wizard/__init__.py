c0a2caad5cc9f25fc83e5bd32a1e46957d6178aa
Quentin THEURET <qt@tempo-consulting.fr>
2013-07-04 14:13:55 +0200

    UF-2071 [ADD] Add a first skeleton of Stock card wizard

--- stock_override/wizard/__init__.py
+++ stock_override/wizard/__init__.py
@@ -23,6 +23,7 @@ import stock_partial_move
 import stock_partial_picking
 import stock_analyze_view
 import stock_invoice_onshipping
+import stock_card_wizard
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
---
+++ stock_override/wizard/stock_card_wizard.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 TeMPO Consulting, MSF
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+from tools.translate import _
+import time
+
+
+class stock_card_wizard(osv.osv_memory):
+    _name = 'stock.card.wizard'
+    _description = 'Print stock card'
+
+    _columns = {
+        'location_id': fields.many2one('stock.location', string='Location', 
+                                       required=True),
+        'product_id': fields.many2one('product.product', string='Product', 
+                                      required=True),
+        'perishable': fields.boolean(string='Perishable'),
+        'prodlot_id': fields.many2one('stock.production.lot', 
+                                      string='Batch number'),
+        'from_date': fields.date(string='From date'),
+        'to_date': fields.date(string='To date'),
+    }
+
+    _defaults = {
+        'to_date': lambda *a: time.strftime('%Y-%m-%d'),
+    }
+
+
+    def onchange_product_id(self, cr, uid, ids, product_id, context=None):
+        '''
+        Set the 'perishable' field if the selected product is perishable.
+        '''
+        prod_obj = self.pool.get('product.product')
+
+        if not context:
+            context = {}
+
+        if not product_id:
+            return {'value': {'perishable': False}}
+
+        product = prod_obj.browse(cr, uid, product_id, context=context)
+
+        return {'value': {'perishable': product.perishable}}
+
+    def print_pdf(self, cr, uid, ids, context=None):
+        '''
+        Print the PDF report according to parameters
+        '''
+        if not context:
+            context = {}
+
+        if isistance(ids, (int, long)):
+            ids = [ids]
+
+        raise NotImplementedError
+
+    def print_excel(self, cr, uid, ids, context=None):
+        '''
+        Print the Excel (XML) report according to parameters
+        '''
+        if not context:
+            context = {}
+
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+
+        raise NotImplementedError
+
+stock_card_wizard()
