4a3d8bdda0f65310cca0fabb9f209ae2cfafb757
Olivier DOSSMANN <od@tempo-consulting.fr>
2013-09-30 16:58:13 +0200

    UTP-766 [ADD] Check on each move line at move validation:
      * Check that move posting date is similar to move line posting date

--- account_override/account.py
+++ account_override/account.py
@@ -190,6 +190,19 @@ class account_move(osv.osv):
                 raise osv.except_osv(_('Error'), _('Posting date should be include in defined Period%s.') % (m.period_id and ': ' + m.period_id.name or '',))
         return True
 
+    def _hook_check_move_line(self, cr, uid, move_line, context=None):
+        """
+        Check date on move line. Should be the same as Journal Entry (account.move)
+        """
+        if not context:
+            context = {}
+        res = super(account_move, self)._hook_check_move_line(cr, uid, move_line, context=context)
+        if not move_line:
+            return res
+        if move_line.date != move_line.move_id.date:
+            raise osv.except_osv(_('Error'), _("Journal item does not have same posting date (%s) as journal entry (%s).") % (move_line.date, move_line.move_id.date))
+        return res
+
     def create(self, cr, uid, vals, context=None):
         """
         Change move line's sequence (name) by using instance move prefix.
