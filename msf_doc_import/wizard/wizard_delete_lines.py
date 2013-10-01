230670c88fe4a3d913783310a9d8a587590f0bf1
Quentin THEURET <qt@tempo-consulting.fr>
2013-10-01 14:58:47 +0200

    UTP-101 [IMP] Made inheritance on some documents where need to remove lines

--- msf_doc_import/wizard/wizard_delete_lines.py
+++ msf_doc_import/wizard/wizard_delete_lines.py
@@ -44,6 +44,13 @@ DOCUMENT_DATA = {'product.list': ('product.list.line', 'list_id', 'product_ids')
 
 
 class document_remove_line(osv.osv):
+    """
+    This object is a dummy object used to have only one method to remove
+    lines from multiple objects.
+    Some other objects inherit from this object to avoid multiple overriding of
+    the button_remove_lines method.
+    """
+
     _name = 'document.remove.line'
 
     def button_remove_lines(self, cr, uid, ids, context=None):
@@ -55,10 +62,12 @@ class document_remove_line(osv.osv):
         if isinstance(ids, (int, long)):
             ids = [ids]
 
+        # If there is no line to remove.
         for obj in self.browse(cr, uid, ids, context=context):
             if not obj[DOCUMENT_DATA.get(self._name)[2]]:
                 raise osv.except_osv(_('Error'), _('No line to remove'))
 
+        # Return the wizard to display lines to remove
         return {'type': 'ir.actions.act_window',
                 'res_model': 'wizard.delete.lines',
                 'view_type': 'form',
@@ -69,43 +78,136 @@ class document_remove_line(osv.osv):
 document_remove_line()
 
 
+#### DOCUMENT INHERITANCE ####
+"""
+All the following documents will inherit from document.remove.line to have an
+unique button_remove_lines method to remove some or all lines on documents.
+
+Documents which inherit from document.remove.line:
+    * Product List
+    * Theoretical Kit Composition
+    * Purchase Order / Request for Quotation
+    * Tender
+    * Field Order / Internal request
+    * Supplier catalogue
+    * Stock Picking (IN / INT / OUT / PICK)
+    * Order Cycle Replenishment Rule
+    * Automatic Supply Replenishment Rule
+    * Threshold value Replenishment Rule
+    * Physical Inventory
+    * Initial stock inventory
+    * Real consumption report
+    * Monthly consumption report
+"""
+
 class product_list(osv.osv):
     _name = 'product.list'
     _inherit = ['product.list', 'document.remove.line']
-
-product_list()
 
 
 class purchase_order(osv.osv):
     _name = 'purchase.order'
     _inherit = ['purchase.order', 'document.remove.line']
 
+
+class composition_kit(osv.osv):
+    _name = 'composition.kit'
+    _inherit = ['composition.kit', 'document.remove.line']
+
+
+class tender(osv.osv):
+    _name = 'tender'
+    _inherit = ['tender', 'document.remove.line']
+
+
+class sale_order(osv.osv):
+    _name = 'sale.order'
+    _inherit = ['sale.order', 'document.remove.line']
+
+
+class supplier_catalogue(osv.osv):
+    _name = 'supplier.catalogue'
+    _inherit = ['supplier.catalogue', 'document.remove.line']
+
+
+class stock_picking(osv.osv):
+    _name = 'stock.picking'
+    _inherit = ['stock.picking', 'document.remove.line']
+
+
+class stock_warehouse_automatic_supply(osv.osv):
+    _name = 'stock.warehouse.automatic.supply'
+    _inherit = ['stock.warehouse.automatic.supply', 'document.remove.line']
+
+
+class stock_warehouse_order_cycle(osv.osv):
+    _name = 'stock.warehouse.order.cycle'
+    _inherit = ['stock.warehouse.order.cycle', 'document.remove.line']
+
+
+class threshold_value(osv.osv):
+    _name = 'threshold.value'
+    _inherit = ['threshold.value', 'document.remove.line']
+
+
+class stock_inventory(osv.osv):
+    _name = 'stock.inventory'
+    _inherit = ['stock.inventory', 'document.remove.line']
+
+
+class initial_stock_inventory(osv.osv):
+    _name = 'initial.stock.inventory'
+    _inherit = ['initial.stock.inventory', 'document.remove.line']
+
+
+class real_average_consumption(osv.osv):
+    _name = 'real.average.consumption'
+    _inherit = ['real.average.consumption', 'document.remove.line']
+
+
+class monthly_review_consumption(osv.osv):
+    _name = 'monthly.review.consumption'
+    _inherit = ['monthly.review.consumption', 'document.remove.line']
+
+
+## Object initializations ##
+
+product_list()
 purchase_order()
+composition_kit()
+tender()
+sale_order()
+supplier_catalogue()
+stock_picking()
+stock_warehouse_automatic_supply()
+stock_warehouse_order_cycle()
+threshold_value()
+stock_inventory()
+initial_stock_inventory()
+real_average_consumption()
+monthly_review_consumption()
+
+#### END OF INHERITANCE OF DOCUMENTS ####
 
 
 class wizard_delete_lines(osv.osv_memory):
     """
-    Wizard to remove lines of document
+    Wizard to remove lines of document.
+
+    The displaying of lines to remove is dynamically build according to
+    the initial_doc_type given (see fields_get method).
     """
     _name = 'wizard.delete.lines'
-
-    def _get_doc_name(self, cr, uid, ids, field_name, args, context=None):
-        '''
-        Return the name of the initial document
-        '''
-        res = {}
-
-        for wiz_data in self.read(cr, uid, ids, ['initial_doc_id', 'initial_doc_type'], context=context):
-            res[wiz_data['id']] = self.pool.get(wiz_data['initial_doc_type']).name_get(cr, uid, wiz_data['initial_doc_id'], context=context)[0][1]
-
-        return res
 
     _columns = {
         'initial_doc_id': fields.integer(string='ID of the initial document', required=True),
         'initial_doc_type': fields.char(size=128, string='Model of the initial document', required=True),
         'to_remove_type': fields.char(size=128, string='Model of the lines', required=True),
         'linked_field_name': fields.char(size=128, string='Field name of the link between lines and original doc', required=True),
-        'initial_doc_name': fields.function(_get_doc_name, method=True, string='Initial doc. name', type='char', readonly=True),
+        # The line_ids field is a text field, but the content of this field is
+        # the result of the many2many field creation (like [(6,0,[IDS])]).
+        # On the remove_selected_lines method, we parse this content to get
+        # id of the lines to remove.
         'line_ids': fields.text(string='Line ids'),
     }
 
@@ -115,11 +217,9 @@ class wizard_delete_lines(osv.osv_memory):
         '''
         context = context or {}
 
-#        if not 'line_ids' in self._columns:
-#            raise osv.except_osv(_('Error'), _('You has to override the object wizard.delete.lines before using it.'))
-
         res = super(wizard_delete_lines, self).default_get(cr, uid, fields, context=context)
 
+        # Set the different data which are coming from the context
         if 'active_id' in context:
             res['initial_doc_id'] = context.get('active_id')
 
@@ -141,6 +241,8 @@ class wizard_delete_lines(osv.osv_memory):
         for wiz in self.browse(cr, uid, ids, context=context):
             line_obj = self.pool.get(wiz.to_remove_type)
             line_ids = []
+            # Parse the content of 'line_ids' field (text field) to retrieve
+            # the id of lines to remove.
             for line in wiz.line_ids:
                 for l in line[2]:
                     line_ids.append(l)
@@ -164,18 +266,14 @@ class wizard_delete_lines(osv.osv_memory):
 
         return {'type': 'ir.actions.act_window_close'}
 
-    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
-        '''
-        '''
-        if self._name != 'wizard.delete.lines':
-            res = self.pool.get('wizard.delete.lines').fields_view_get(cr, uid, view_id, view_type, context=dict(context, object_model=self._name), toolbar=toolbar, submenu=submenu)
-        else:
-            res = super(wizard_delete_lines, self).fields_view_get(cr, uid, view_id, view_type, context=context, toolbar=toolbar, submenu=submenu)
-
-        return res
-
     def fields_get(self, cr, uid, fields=None, context=None):
         '''
+        On this fields_get method, we build the line_ids field.
+        The line_ids field is defined as a text field but, for users, this 
+        field should be displayed as a many2many that allows us to select
+        lines of document to remove.
+        The line_ids field is changed to a many2many field according to the
+        data in DOCUMENT_DATA (see top of this file).
         '''
         context = context or {}
 
@@ -197,56 +295,3 @@ class wizard_delete_lines(osv.osv_memory):
     
 wizard_delete_lines()
 
-'''
-class wizard_delete_product_list_line(osv.osv_memory):
-    _name = 'wizard.delete.product.list.line'
-    _inherit = 'wizard.delete.lines'
-
-    _columns = {
-        'line_ids': fields.many2many('product.list.line', 'product_list_line_to_remove', 'wiz_id', 'line_id',
-                                     string='Lines to remove', domain="[('list_id', '=', initial_doc_id)]"),
-    }
-
-    _defaults = {
-        'initial_doc_type': lambda *a: 'product.list',
-        'to_remove_type': lambda *a: 'product.list.line',
-        'linked_field_name': lambda *a: 'list_id',
-    }
-
-wizard_delete_product_list_line()
-
-
-class wizard_delete_kit_line(osv.osv_memory):
-    _name = 'wizard.delete.kit.line'
-    _inherit = 'wizard.delete.lines'
-
-    _columns = {
-        'line_ids': fields.many2many('composition.item', 'composition_item_to_remove', 'wiz_id', 'item_id',
-                                     string='Lines to remove', domain="[('item_kit_id', '=', initial_doc_id)]"),
-    }
-
-    _defaults = {
-        'initial_doc_type': lambda *a: 'composition.kit',
-        'to_remove_type': lambda *a: 'composition.item',
-        'linked_field_name': lambda *a: 'item_kit_id',
-    }
-
-wizard_delete_kit_line()
-
-class wizard_delete_purchase_order_line(osv.osv_memory):
-    _name = 'wizard.delete.purchase.order.line'
-    _inherit = 'wizard.delete.lines'
-
-    _columns = {
-        'line_ids': fields.many2many('purchase.order.line', 'purchase_order_line_to_remove', 'wiz_id', 'line_id',
-                                     string='Lines to remove', domain="[('order_id', '=', initial_doc_id)]"),
-    }
-
-    _defaults = {
-        'initial_doc_type': lambda *a: 'purchase.order',
-        'to_remove_type': lambda *a: 'purchase.order.line',
-        'linked_field_name': lambda *a: 'order_id',
-    }
-
-wizard_delete_purchase_order_line()
-'''
