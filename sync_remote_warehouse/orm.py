780e2cc020ec11f2c612c97e36c6423365f732d1
Max Mumford <mmu@openerp.com>
2013-11-07 15:23:47 +0100

    [FIX] workflow.instance res_id received from sync not being converted from sd ref to id

--- sync_remote_warehouse/orm.py
+++ sync_remote_warehouse/orm.py
@@ -76,7 +76,9 @@ from workflow import workitem, instance, wkf_expr, wkf_logs
 import pooler
 import netsvc
 
+# trigger creation of SD ref automatically
 old_wkf_workitem_create = workitem.create
+
 def wkf_workitem_create(cr, act_datas, inst_id, ident, stack):
     new_ids = old_wkf_workitem_create(cr, act_datas, inst_id, ident, stack)
     pooler.get_pool(cr.dbname).get('workflow.workitem').get_sd_ref(cr, 1, new_ids)
@@ -84,6 +86,7 @@ def wkf_workitem_create(cr, act_datas, inst_id, ident, stack):
  
 workitem.create = wkf_workitem_create   
 
+# trigger creation of SD ref automatically
 old_wkf_instance_create = instance.create
 
 def wkf_instance_create(cr, ident, wkf_id):
@@ -96,7 +99,7 @@ instance.create = wkf_instance_create
 
 class wkf_instance(osv.osv):
     """
-    replace res_id with sdref for synchronization
+    If synchronising, convert res_id from int to SD ref on export, and sd ref to int on import 
     """
     
     _inherit = 'workflow.instance'
@@ -137,10 +140,31 @@ class wkf_instance(osv.osv):
         
         return is_list and vals or vals[0]
     
+    def is_int(self, obj):
+        try:
+            int(obj)
+            return True
+        except ValueError:
+            return False
+    
     def read(self, cr, uid, ids, fields=None, context=None, load='_classic_read'):
+        """ Convert int to SD ref for res_id field """
         vals = super(wkf_instance, self).read(cr, uid, ids, fields=fields, context=context, load=load)
         return self.replace_res_id_by_xml_id(cr, uid, ids, fields, vals, context)
     
+    def import_data(self, cr, uid, fields, datas, mode='init', current_module='', noupdate=False, context=None, filename=None):
+        """ Convert SD ref to id for res_id field """
+        if not isinstance(context, dict) or not context.get('sync_update_execution'): #May replace by offline_synchronization
+            return super(wkf_instance, self).import_data(cr, uid, fields, datas, mode=mode, current_module=current_module, noupdate=noupdate, context=context, filename=filename)
+        
+        index = fields.index('res_id')
+        for data in datas:
+            if not self.is_int(data[index]):
+                sd_ref = '.' in data[index] and data[index].split('.', 1)[1] or data[index]
+                cr.execute("select res_id from ir_model_data where name = '%s' and module = 'sd'" % sd_ref)
+                res_id = cr.fetchone()
+                data[index] = res_id and res_id[0] or 0
+        return super(wkf_instance, self).import_data(cr, uid, fields, datas, mode=mode, current_module=current_module, noupdate=noupdate, context=context, filename=filename)
     
 wkf_instance()
 
