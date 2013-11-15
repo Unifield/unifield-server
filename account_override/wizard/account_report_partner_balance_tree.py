61cb0ca96b8c80d7a724fb048adf07332ee7a71b
 VG <vg@tempo-consulting.fr>
2013-11-15 15:58:03 +0100

    [uf-1715] wizard entry point

---
+++ account_override/wizard/account_report_partner_balance_tree.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class account_partner_balance_tree(osv.osv_memory):
+    """
+        This wizard will provide the partner balance report by periods, between any two dates.
+    """
+    _inherit = 'account.common.partner.report'
+    _name = 'account.partner.balance.tree'
+    _description = 'Print Account Partner Balance Tree'
+    _columns = {
+        'display_partner': fields.selection([('non-zero_balance',
+                                             'With balance is not equal to 0'),
+                                             ('all', 'All Partners')]
+                                            ,'Display Partners'),
+        'output_currency': fields.many2one('res.currency', 'Output Currency', required=True),
+    }
+
+    _defaults = {
+        'display_partner': 'non-zero_balance',
+    }
+    
+    def show(self, cr, uid, ids, data, context=None):
+        if context is None:
+            context = {}
+   
+    def export(self, cr, uid, ids, data, context=None):
+        if context is None:
+            context = {}
+
+    #~ def _print_report(self, cr, uid, ids, data, context=None):
+        #~ if context is None:
+            #~ context = {}
+        #~ data = self.pre_print_report(cr, uid, ids, data, context=context)
+        #~ data['form'].update(self.read(cr, uid, ids, ['display_partner'])[0])
+        #~ return {
+            #~ 'type': 'ir.actions.report.xml',
+            #~ 'report_name': 'account.partner.balance',
+            #~ 'datas': data,
+    #~ }
+
+account_partner_balance_tree()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
