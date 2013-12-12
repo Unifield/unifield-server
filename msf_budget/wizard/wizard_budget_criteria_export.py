36ef106c9d5faddcc671a059f05a565869e7511b
jf <jfb@tempo-consulting.fr>
2013-12-12 18:29:01 +0100

    UTP-820: Budget vs. Actuals

--- msf_budget/wizard/wizard_budget_criteria_export.py
+++ msf_budget/wizard/wizard_budget_criteria_export.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 from osv import osv, fields
-import datetime
+import time
 
 class wizard_budget_criteria_export(osv.osv_memory):
     _name = "wizard.budget.criteria.export"
@@ -54,6 +54,8 @@ class wizard_budget_criteria_export(osv.osv_memory):
             data['form'].update({'commitment': wizard.commitment})
             data['form'].update({'breakdown': wizard.breakdown})
             data['form'].update({'granularity': wizard.granularity})
+            budget_code = self.pool.get('msf.budget').read(cr, uid, context['active_id'], ['code'])
+            data['target_filename'] = 'Budget vs. Actual_%s_%s' % (budget_code['code'] or '', time.strftime('%Y%M%d'))
             if wizard.currency_table_id:
                 data['form'].update({'currency_table_id': wizard.currency_table_id.id})
             if wizard.period_id:
