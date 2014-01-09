4025a38e47132fe6e108d081792a239d287b0b4e
duy.vo@geneva.msf.org <>
2014-01-09 17:29:44 +0100

    UTP-972: monir change: reset value when copy lines

--- sale_override/sale.py
+++ sale_override/sale.py
@@ -1489,7 +1489,8 @@ class sale_order_line(osv.osv):
         if 'so_back_update_dest_po_id_sale_order_line' not in default:
             default.update({'so_back_update_dest_po_id_sale_order_line': False,
                             'so_back_update_dest_pol_id_sale_order_line': False,})
-        default.update({'sync_order_line_db_id': False, 'manually_corrected': False})
+        default.update({'sync_order_line_db_id': False, 'manually_corrected': False, 'is_line_split': False})
+        
         return super(sale_order_line, self).copy_data(cr, uid, id, default, context=context)
 
     def open_order_line_to_correct(self, cr, uid, ids, context=None):
@@ -1632,7 +1633,8 @@ class sale_order_line(osv.osv):
         if not default:
             default = {}
             
-        default.update({'sync_order_line_db_id': False, 'manually_corrected': False})
+        default.update({'sync_order_line_db_id': False, 'manually_corrected': False,'is_line_split': False})
+        
         return super(sale_order_line, self).copy(cr, uid, id, default, context)
 
     def create(self, cr, uid, vals, context=None):
