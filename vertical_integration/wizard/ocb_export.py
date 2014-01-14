888e401ed7e03f31e2175a6a1a3bc9d2c14c8f71
Olivier DOSSMANN <od@tempo-consulting.fr>
2014-01-14 16:53:58 +0100

    UF-2236 [IMP] Point 20 - Change name of export

--- vertical_integration/wizard/ocb_export.py
+++ vertical_integration/wizard/ocb_export.py
@@ -67,7 +67,7 @@ class ocb_export_wizard(osv.osv_memory):
             data['form'].update({'reset': True,})
         ############################
 
-        data['target_filename'] = '%s_%s_formatted data UF to Epicor' % (wizard.instance_id and wizard.instance_id.code[0:3] or '', period_name)
+        data['target_filename'] = '%s_%s_formatted data UF to OCB HQ system' % (wizard.instance_id and wizard.instance_id.code[0:3] or '', period_name)
         return {'type': 'ir.actions.report.xml', 'report_name': 'hq.ocb', 'datas': data}
 
 ocb_export_wizard()
