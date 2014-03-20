f1eca216cf471d6b450b6e4275a529ec0933354b
 VG <vg@tempo-consulting.fr>
2014-03-20 15:02:50 +0100

    UFTP-69 [FIX] do not allow validate or confirm of a purchase order with a total amount of 0

--- purchase_override/purchase.py
+++ purchase_override/purchase.py
@@ -943,6 +943,9 @@ stock moves which are already processed : '''
                 
             if not po.split_po and not po.order_line:
                 raise osv.except_osv(_('Error !'), _('You can not validate a purchase order without Purchase Order Lines.'))
+            
+            if po.amount_total == 0:  # UFTP-69
+                raise osv.except_osv(_('Error'), _('You can not validate a purchase order with a total amount of 0.'))
                 
             for line in po.order_line:
                 if line.state=='draft':
@@ -1373,6 +1376,12 @@ stock moves which are already processed : '''
         setup = uf_config.get_config(cr, uid)
 
         for order in self.browse(cr, uid, ids):
+            if order.amount_total == 0:  # UFTP-69
+                # total amount could be set to 0 after it was Validated
+                # or no lines
+                # (after wkf_confirm_order total amount check)
+                raise osv.except_osv(_('Error'), _('You can not confirm a purchase order with a total amount of 0.'))
+            
             # Create commitments for each PO only if po is "from picking"
             # UTP-114: No Commitment Voucher on PO that are 'purchase_list'!
             if (order.invoice_method in ['picking', 'order'] and not order.from_yml_test and order.order_type not in ['in_kind', 'purchase_list'] and order.partner_id.partner_type != 'intermission') or (order.invoice_method == 'manual' and order.order_type == 'direct' and order.partner_id.partner_type == 'esc'):
