5b5e2a4c660a71d321ae597062ae28a2a673f6c9
Sean Carroll <sean@sean-msf>
2014-03-25 17:28:26 +0100

    UTP-978:[IMP] changed seqnum to OD; added original as a ref

--- account_corrections/wizard/analytic_distribution_wizard.py
+++ account_corrections/wizard/analytic_distribution_wizard.py
@@ -220,31 +220,31 @@ class analytic_distribution_wizard(osv.osv_memory):
             # reverse the line
             to_reverse_ids = self.pool.get('account.analytic.line').search(cr, uid, [('distrib_line_id', '=', 'funding.pool.distribution.line,%d'%line.distribution_line_id.id), ('is_reversal', '=', False), ('is_reallocated', '=', False)])
             
-            # get the original sequence & and split it. Use the wizard date as the posting date
+            # get the original sequence
             orig_line = self.pool.get('account.analytic.line').browse(cr, uid, to_reverse_ids)[0]
-            seq_split = orig_line.entry_sequence.split("-",)
-            
-            # create the new entry sequence
-            seq_vals = {}
-            seq_vals['name'] = seq_split[0]
-            seq_vals['code'] = seq_split[1]
             period_ids = self.pool.get('account.period').get_period_from_date(cr, uid, wizard.date)
             if not period_ids:
                 raise osv.except_osv(_('Warning'), _('No period found for creating sequence on the given date: %s') % (wizard.date or ''))
             period = self.pool.get('account.period').browse(cr, uid, period_ids)[0]
                         
             # note that account_analytic_line.move_id is actually account_analytic_line.move_line_id, but the journal_id is
-            # stored on both account_move and account_move_line. Complete madness.
-            sequence_number = self.pool.get('ir.sequence').get_id(cr, uid, orig_line.move_id.journal_id.sequence_id.id, context={'fiscalyear_id': period.fiscalyear_id.id})
-            entry_seq = "%s-%s-%s" % (seq_vals['name'], 'OD', sequence_number)
+            # Get a new OD sequence for the REV / COR rows
+            cr.execute("select id, code from account_journal where type = 'correction' and is_current_instance = true")
+            for row in cr.dictfetchall():
+                journal_id = row['id']
+                code = row['code']
+            journal = self.pool.get('account.journal').browse(cr, uid, journal_id, context=context)
+            move_prefix = self.pool.get('res.users').browse(cr, uid, uid, context).company_id.instance_id.move_prefix
+            seqnum = self.pool.get('ir.sequence').get_id(cr, uid, journal.sequence_id.id, context={'fiscalyear_id': period.fiscalyear_id.id}) 
+            entry_seq = "%s-%s-%s" % (move_prefix, code, seqnum)
               
             # UTP-943: Set wizard date as date for REVERSAL AND CORRECTION lines
             reversed_id = self.pool.get('account.analytic.line').reverse(cr, uid, to_reverse_ids[0], posting_date=wizard.date, context=context)[0]
             # Add reversal origin link (to not loose it). last_corrected_id is to prevent case where you do a reverse a line that have been already corrected
-            # UTP-943: Add correction journal on it
-            self.pool.get('account.analytic.line').write(cr, uid, [reversed_id], {'reversal_origin': to_reverse_ids[0], 'last_corrected_id': False, 'journal_id': correction_journal_id})
+    
+            self.pool.get('account.analytic.line').write(cr, uid, [reversed_id], {'reversal_origin': to_reverse_ids[0], 'last_corrected_id': False, 'journal_id': correction_journal_id, 'ref': orig_line.entry_sequence})
             # Mark old lines as non reallocatable (ana_ids): why reverse() don't set this flag ?
-            self.pool.get('account.analytic.line').write(cr, uid, [to_reverse_ids[0]], {'is_reallocated': True,})
+            self.pool.get('account.analytic.line').write(cr, uid, [to_reverse_ids[0]], {'is_reallocated': True})
             cr.execute('update account_analytic_line set entry_sequence = %s where id = %s', (entry_seq, reversed_id) )
 
             # update the distrib line
