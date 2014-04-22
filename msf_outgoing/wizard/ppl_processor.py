0cccf627d89d8b1ab4f41dc2e675cf7c3060dbad
 VG <vg@tempo-consulting.fr>
2014-04-22 15:19:59 +0200

    UFTP-159 [FIX] ppl processor: ppl_familly_processor update intigrity at write to refresh properly tree colors when weight changes

--- msf_outgoing/wizard/ppl_processor.py
+++ msf_outgoing/wizard/ppl_processor.py
@@ -294,6 +294,11 @@ class ppl_family_processor(osv.osv):
     _defaults = {
         'integrity_status': 'empty',
     }
+    
+    def write(self, cr, uid, ids, vals, context=None):
+        if 'weight' in vals:
+            vals['integrity_status'] = 'empty' if (vals['weight'] and vals['weight'] > 0) else 'missing_weight'
+        super(ppl_family_processor, self).write(cr, uid, ids, vals, context=context)
 
     """
     Controller methods
