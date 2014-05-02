1ba8b532fd85937ea239e31564543698b1b4d5a3
Quentin THEURET <qt@tempo-consulting.fr>
2014-05-02 14:51:58 +0200

    UFTP-224 [FIX] Sourcing on RfQ: If the supplier has no address, put the procurement order in Exception state with the possibility to retry the procurement

--- sourcing/sale_order_line.py
+++ sourcing/sale_order_line.py
@@ -1360,6 +1360,14 @@ the supplier must be either in 'Internal', 'Inter-section' or 'Intermission type
             return result
 
         partner = partner_obj.browse(cr, uid, supplier, context)
+
+        # Check if the partner has addresses
+        if not partner.address:
+            result['warning'] = {
+                'title': _('Warning'),
+                'message': _('The choosen partner has no address defined. Please defined an address before continue.'),
+            }
+
         # If the selected partner belongs to product->suppliers, we take that delay (from supplierinfo)
         line = self.browse(cr, uid, line_id, context=context)
         delay = self.check_supplierinfo(line, partner, context=context)
