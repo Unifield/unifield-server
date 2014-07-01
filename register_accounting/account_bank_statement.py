bcd299eebbc025cf4eb66c4d9b89f58deead59b8
Sean Carroll <sean@sean-msf>
2014-07-01 11:14:37 +0200

    UFTP-253:[FIX] bypass cheque number validation for direct invoice wizard

--- register_accounting/account_bank_statement.py
+++ register_accounting/account_bank_statement.py
@@ -1918,8 +1918,9 @@ class account_bank_statement_line(osv.osv):
         acc_move_obj = self.pool.get("account.move")
         # browse all statement lines for creating move lines
         for absl in self.browse(cr, uid, ids, context=context):
-            if absl.statement_id and absl.statement_id.journal_id and absl.statement_id.journal_id.type in ['cheque'] and not absl.cheque_number:
-                raise osv.except_osv(_('Warning'), _('Cheque Number is missing!'))
+            if context.get('_terp_view_name') != 'Supplier Direct Invoice':
+                if absl.statement_id and absl.statement_id.journal_id and absl.statement_id.journal_id.type in ['cheque'] and not absl.cheque_number:
+                    raise osv.except_osv(_('Warning'), _('Cheque Number is missing!'))
             previous_state = ''.join(absl.state)
             if absl.state == "hard":
                 raise osv.except_osv(_('Warning'), _('You can\'t re-post a hard posted entry !'))
