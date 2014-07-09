a0ecef8a0a6dce7c8d523b8a64002fc173a5b18b
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-07-09 16:11:38 +0200

    UTP-1044 [IMP] Add a link between cheque journal and bank journal so that each cheque journal should have a bank journal linked.

--- account_journal/account_journal.py
+++ account_journal/account_journal.py
@@ -82,6 +82,7 @@ class account_journal(osv.osv):
     _columns = {
         'type': fields.selection(get_journal_type, 'Type', size=32, required=True),
         'code': fields.char('Code', size=10, required=True, help="The code will be used to generate the numbers of the journal entries of this journal."),
+        'bank_journal_id': fields.many2one('account.journal', _("Linked bank"), domain="[('type', '=', 'bank')]"),
     }
 
     _defaults = {
