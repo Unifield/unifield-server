246be9addb6f3d2159c38ebcb1542e895c87b4f8
duy.vo@geneva.msf.org <>
2014-07-15 14:52:03 +0200

    UTP-1100: Removed the context fields after importing the data

--- sync_client/update.py
+++ sync_client/update.py
@@ -388,6 +388,7 @@ class update_received(osv.osv):
                 context['fields'] = fields # UTP-1100: Added this list for checking later if some values need to be re-taken
                 cr.execute("SAVEPOINT import_data")
                 res = obj.import_data(cr, uid, fields, values, mode='update', current_module='sd', noupdate=True, context=context)
+                del context['fields']
             except BaseException, e:
                 cr.execute("ROLLBACK TO SAVEPOINT import_data")
                 self._logger.exception("import failure")
