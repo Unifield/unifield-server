4617e3fa2c69cfe4fa764b2a65a22392b4f6da10
Quentin THEURET <qt@tempo-consulting.fr>
2014-09-25 11:53:33 +0200

    UTP-1051 [IMP] When a res.log is created, check if the user can read the document or not

--- msf_button_access_rights/res_log.py
+++ msf_button_access_rights/res_log.py
@@ -16,6 +16,86 @@ class res_log(osv.osv):
         'read_ok': False,
     }
 
+    def _check_parent_menu_access(self, cr, uid, menu_id, context=None):
+        """
+        Check if the use can access to parent menus
+        """
+        menu_obj = self.pool.get('ir.ui.menu')
+
+        menu_ids = []
+        while menu_id:
+            menu_ids.append(menu_id)
+            cr.execute('''
+                SELECT
+                    m.parent_id, m.id
+                FROM ir_ui_menu m
+                WHERE m.id = %(menu_id)s
+            ''', {'menu_id': menu_id})
+            menus = cr.dictfetchall()
+            for menu in menus:
+                if menu.get('parent_id'):
+                    menu_id = menu.get('parent_id')
+                else:
+                    menu_id = False
+
+        # Check the visibility
+        menu_acl = menu_obj._filter_visible_menus(cr, uid, menu_ids, context=context)
+        menu_ids.reverse()
+        for menu_id in menu_ids:
+            if menu_id not in menu_acl:
+                return False
+
+        return True
+
+
+    def _check_menu_access(self, cr, uid, model, context=None):
+        """
+        Check if the user can access to this object by checking the access to
+        menus.
+        """
+        menu_obj = self.pool.get('ir.ui.menu')
+
+        menus = False
+
+        cr.execute('''
+            SELECT
+                m.parent_id, m.id
+            FROM ir_ui_menu m
+                LEFT JOIN
+                    ir_values v ON v.res_id = m.id
+            WHERE
+                v.model = 'ir.ui.menu'
+              AND
+                v.key = 'action'
+              AND
+                v.key2 = 'tree_but_open'
+              AND
+                v.value IN (SELECT
+                                'ir.actions.act_window,' || w.id
+                            FROM
+                                ir_act_window w
+                            WHERE 
+                                w.res_model = %(model)s
+                              AND
+                                w.type = 'ir.actions.act_window') 
+        ''', {'model': model})
+                
+        menus = cr.dictfetchall()
+
+        res = False
+        for m in menus:
+            m_access = True
+            if m.get('parent_id'):
+                m_access = self._check_parent_menu_access(cr, uid, m.get('parent_id'), context=context)
+                if m_access:
+                    res = True
+
+            if m.get('id') and m_access:
+                if m['id'] in menu_obj._filter_visible_menus(cr, uid, [m['id']], context=context):
+                    res = True
+
+        return res
+
     def create(self, cr, uid, vals, context=None):
         if vals is None:
             vals = {}
@@ -24,6 +104,8 @@ class res_log(osv.osv):
         read_ok = False
         if user_id and vals.get('res_model', False):
             read_ok = self.pool.get('ir.model.access').check(cr, user_id, vals.get('res_model'), 'read', context=context)
+            if read_ok:
+                read_ok = self._check_menu_access(cr, user_id, vals.get('res_model'), context=context)
 
         vals['read_ok'] = read_ok
 
