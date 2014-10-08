fb287926679a3c50d1d87192e85a28509a9e2035
jf <jfb@tempo-consulting.fr>
2014-10-08 12:07:58 +0200

    Version Pilot 3.1b10a

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'pilot3.1b10'
+version = 'pilot3.1b10a'
 major_version = '6.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
