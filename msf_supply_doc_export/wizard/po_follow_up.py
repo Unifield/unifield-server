a17920caaff86f81b0323e336f35de0bc3e3a169
Quentin THEURET <qt@tempo-consulting.fr>
2014-11-07 16:09:38 +0100

    UFTP-315 [FIX] Filter the PO only on states that are displayed on Unifield

--- msf_supply_doc_export/wizard/po_follow_up.py
+++ msf_supply_doc_export/wizard/po_follow_up.py
@@ -23,11 +23,19 @@ from osv import osv
 from osv import fields
 from tools.translate import _
 from lxml import etree
-from purchase_override import PURCHASE_ORDER_STATE_SELECTION
 
 import time
 from datetime import datetime
 
+PURCHASE_ORDER_STATE_SELECTION = [
+    ('draft', 'Draft'),
+    ('sourced', 'Sourced'),
+    ('confirmed', 'Validated'),
+    ('confirmed_wait', 'Confirmed (waiting)'),
+    ('approved', 'Confirmed'),
+    ('done', 'Closed'),
+    ('cancel', 'Cancelled'),
+]
 
 class po_follow_up(osv.osv_memory):
     _name = 'po.follow.up'
