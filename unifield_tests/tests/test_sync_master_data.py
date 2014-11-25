86e507a2dd133ecbc256e63ecd7ff4c0716498ce
 VG <vg@tempo-consulting.fr>
2014-11-25 16:09:03 +0100

    [TEST] master data S1-TEC-22

--- unifield_tests/tests/test_sync_master_data.py
+++ unifield_tests/tests/test_sync_master_data.py
@@ -6,15 +6,26 @@ from unifield_test import UnifieldTest
 import time
 
 
-def dfd(d):
+def dfv(vals, include=None, exclude=[]):
     """
-    Domain from dictionary
+    domain from vals (all vals with implicite &)
 
-    Create a domain from a data dictionary
-    :param d: Dictionary that contains data
+    create a domain from a data dictionary
+    include is prior to exclude (exclude not used if include is set)
+    :type d: dict
+    :param include: field or list of field to include in domain
+    :type include: str/list
+    :param exclude: field or list of field not to include in domain
+    :type exclude: str/list
     :return: A list of tuples like [('op1', 'operator', 'op2')]
     """
-    return [(x[0], '=', x[1]) for x in d.iteritems()]
+    if include and isinstance(include, (str, list, )):
+        if isinstance(include, str):
+            include = [include]
+        return [(x[0], '=', x[1]) for x in vals.iteritems() if x[0] in include]
+    if isinstance(exclude, str):
+        exclude = [exclude]
+    return [(x[0], '=', x[1]) for x in vals.iteritems() if x[0] not in exclude]
 
 
 class MasterDataSyncTestException(Exception):
@@ -65,6 +76,30 @@ class MasterDataSyncTest(UnifieldTest):
             if ids:
                 db.get(model_name).unlink(ids)
 
+    def _check_data_set_on_db(self, db, check_batch):
+        """
+        check in db that for model/domain entries there is one data set entry
+        :type db: object
+        :param check_batch: [(model, domain), ]
+        """
+        for model, domain in check_batch:
+            ids = db.get(model).search(domain)
+            if ids:
+                self._set_ids(db, model, ids)
+                count = len(ids)
+            else:
+                count = 0
+
+            self.assertNotEquals(
+                count, 0,
+                "'UF' '%s' entry not found on %s" % (model, db.colored_name, )
+            )
+            self.assertEqual(
+                count, 1,
+                "There is more than 1 'UF' '%s' entry on %s :: %s" % (model,
+                    db.colored_name, count, )
+            )
+
     def tearDown(self):
         self._unlink_model_ids('res.country.state')
         self._unlink_model_ids('res.country')
@@ -73,89 +108,105 @@ class MasterDataSyncTest(UnifieldTest):
         """
         python -m unittest tests.test_sync_master_data.MasterDataSyncTest.test_s1_tec_21
 
-        Create a new country and country state on HQ database.
-        Synchronize between the HQ, Coordination and project.
-        Check if the country and the country state have been synchronized
-        on Coordination and project database.
+        - create a new country and country state on HQ database
+        - synchronize down from hq to coordo and project
+        - check if the country and the country state have been well sync down
         """
+        # country
         hq_country_obj = self.hq1.get('res.country')
-        hq_state_obj = self.hq1.get('res.country.state')
-
-        country_data = {
+        vals = {
             'code': 'UF',
             'name': 'Unifield Country Test',
         }
-        country_domain = dfd(country_data)
-
+        country_domain = dfv(vals)
         if hq_country_obj.search(country_domain):
             country_id = hq_country_obj.search(country_domain)[0]
         else:
-            country_id = hq_country_obj.create(country_data)
+            country_id = hq_country_obj.create(vals)
         self._set_ids(self.hq1, 'res.country', country_id)
 
-        state_data = {
+        # country state
+        hq_state_obj = self.hq1.get('res.country.state')
+        vals = {
             'code': 'UF',
             'name': 'Unifield Country State Test',
             'country_id': country_id,
         }
-        state_domain = dfd(state_data)
-        state_data_sync = state_data.copy()
-        del state_data_sync['country_id']
-        state_domain_sync = dfd(state_data_sync)
-
+        state_domain = dfv(vals, exclude='country_id')
         if hq_state_obj.search(state_domain):
             pass
         else:
-            state_id = hq_state_obj.create(state_data)
+            state_id = hq_state_obj.create(vals)
             self._set_ids(self.hq1, 'res.country.state', state_id)
 
-        # Launch synchronization on HQ1
+        # sync down and check
         self.synchronize(self.hq1)
 
-        def check_on_db(db):
-            ids = db.get('res.country').search(country_domain)
-            if ids:
-                self._set_ids(db, 'res.country', ids)
-                nb_countries = len(ids)
-            else:
-                nb_countries = 0
-            self.assertNotEquals(
-                nb_countries, 0,
-                "'UF' country not found on %s" % db.colored_name,
-            )
-            self.assertEqual(
-                nb_countries, 1,
-                "There is more than 1 'UF' countries on %s :: %s" % (
-                    db.colored_name,
-                    nb_countries,
-                )
-            )
+        check_batch = [
+            ('res.country', country_domain),
+            ('res.country.state', state_domain),
+        ]
 
-            ids = db.get('res.country.state').search(state_domain_sync)
-            if ids:
-                self._set_ids(db, 'res.country.state', ids)
-                nb_states = len(ids)
-            else:
-                nb_states = 0
-            self.assertNotEquals(
-                nb_states, 0,
-                "'UF' states not found on %s" % db.colored_name,
-            )
-            self.assertEqual(
-                nb_states, 1,
-                "There is more than 1 'UF' states on %s :: %s" % (
-                    db.colored_name,
-                    nb_states,
-                )
-            )
-
-        # Checks on C1
+        # c1 check
         self.synchronize(self.c1)
-        check_on_db(self.c1)
+        self._check_data_set_on_db(self.c1, check_batch)
 
-        # Checks on P1
+        # p1 check
         self.synchronize(self.p1)
-        check_on_db(self.p1)
+        self._check_data_set_on_db(self.p1, check_batch)
+
+    def test_s1_tec_22(self):
+        """
+        python -m unittest tests.test_sync_master_data.MasterDataSyncTest.test_s1_tec_22
+
+        - create an uom category and an uom on hq
+        - synchronize down from hq to coordo and project
+        - check if the uom categ and the uom have been well sync down
+        """
+        # uom category
+        hg_puomc_obj = self.hq1.get('product.uom.categ')
+        vals = {
+            'name': 'Unifield Uom Category Test'
+        }
+        uom_categ_domain = dfv(vals)
+        if hg_puomc_obj.search(uom_categ_domain):
+            categ_id = hg_puomc_obj.search(uom_categ_domain)[0]
+        else:
+            categ_id = hg_puomc_obj.create(vals)
+        self._set_ids(self.hq1, 'product.uom.categ', categ_id)
+
+        # uom
+        hg_puom_obj = self.hq1.get('product.uom')
+        vals = {
+            'category_id': categ_id,
+            'factor': 1.,
+            'factor_inv': 1.,
+            'name': 'UF Uom Test',
+            'rounding': 1.,
+            'uom_type': 'reference',
+        }
+        uom_domain = dfv(vals, include=['name', 'uom_type', ])
+        if hg_puom_obj.search(uom_domain):
+            uom_id = hg_puom_obj.search(uom_domain)[0]
+        else:
+            uom_id = hg_puom_obj.create(vals)
+        self._set_ids(self.hq1, 'product.uom', uom_id)
+
+        # sync down and check
+        self.synchronize(self.hq1)
+
+        check_batch = [
+            ('product.uom.categ', uom_categ_domain),
+            ('product.uom', uom_domain),
+        ]
+
+        # c1 check
+        self.synchronize(self.c1)
+        self._check_data_set_on_db(self.c1, check_batch)
+
+        # p1 check
+        self.synchronize(self.p1)
+        self._check_data_set_on_db(self.p1, check_batch)
 
 def get_test_class():
     return MasterDataSyncTest
