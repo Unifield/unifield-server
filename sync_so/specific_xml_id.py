7ee783b2c4653ff6e1fbb120a1884c2af4453c16
jf <jfb@tempo-consulting.fr>
2014-12-10 17:52:53 +0100

    US-15: sync group without any menu

--- sync_so/specific_xml_id.py
+++ sync_so/specific_xml_id.py
@@ -706,6 +706,6 @@ class res_groups(osv.osv):
         if context is None:
             context = {}
         if context.get('sync_update_execution') and 'menu_access/id' in context.get('fields', []) and 'menu_access' not in vals:
-            vals['groups_id'] = [(6, 0, [])]
+            vals['menu_access'] = [(6, 0, [])]
         return super(res_groups, self).write(cr, uid, ids, vals, context)
 res_groups()
