5235c3af98b64e185ce52aadac967ffbb6f2457b
 VG <vg@tempo-consulting.fr>
2015-02-24 10:29:05 +0100

    US-56 [FIX] PO report 'order impact vs budget': totals compute, round at line level sum (not after total sum) to maintain consistency of total line with values line

--- msf_budget/report/report_pdf_engagement.py
+++ msf_budget/report/report_pdf_engagement.py
@@ -240,10 +240,10 @@ class report_pdf_engagement(report_sxw.rml_parse):
                     formatted_line = [cost_center.name]
                     formatted_line += [expense_account.code + " " + expense_account.name]
                     if values[0] != 'Budget missing':
-                        total_values = [sum(pair) for pair in zip(values, total_values)]
+                        total_values = [round(sum(pair)) for pair in zip(values, total_values)]
                         formatted_line += [values[0]]
                     else:
-                        total_values = [sum(pair) for pair in zip([0] + values[1:], total_values)]
+                        total_values = [round(sum(pair)) for pair in zip([0] + values[1:], total_values)]
                         formatted_line += [values[0]]
                     formatted_line += values[1:]
                     formatted_line += [ temp_data2[str(cost_center_id)+'_'+str(destination_id)][expense_account_id] ]
@@ -252,7 +252,7 @@ class report_pdf_engagement(report_sxw.rml_parse):
                 # empty line between cost centers
                 res.append([''] * 7)
             # append formatted total
-            res.append(['TOTALS', ''] + map(int, map(round, total_values)))
+            res.append(['TOTALS', ''] + map(int, total_values))
         return res
 
 report_sxw.report_sxw('report.msf.pdf.engagement', 'purchase.order', 'addons/msf_budget/report/engagement.rml', parser=report_pdf_engagement, header=False)
