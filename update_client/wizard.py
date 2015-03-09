36b19fc9f24d510154696c88b05b0e506138eb8e
jf <jfb@tempo-consulting.fr>
2015-03-09 17:21:49 +0100

    US-124: display warning msg at upgrade if JI not balanced

--- update_client/wizard.py
+++ update_client/wizard.py
@@ -48,6 +48,18 @@ class upgrade(osv.osv_memory):
         restart_server()
         return {'type': 'ir.actions.act_window_close'}
 
+    def _get_text_unbalance_aml(self, cr, uid, context=None):
+        text = ""
+        account_move_obj = self.pool.get('account.move')
+        a_m_ids = account_move_obj.get_valid_but_unbalanced(cr, uid, context)
+        if a_m_ids:
+            text += "Warning ! Before patching please correct the following unbalanced Journal Entries: "
+            am_name = []
+            for a_m in account_move_obj.read(cr, uid, a_m_ids, ['name']):
+                am_name.append(a_m['name'])
+            text += ", ".join(am_name)
+        return text
+
     def download(self, cr, uid, ids, context=None):
         """Downlad the patch to fill the version record"""
         revisions = self.pool.get('sync_client.version')
@@ -67,6 +79,7 @@ class upgrade(osv.osv_memory):
                     raise osv.except_osv(_("Error!"), _("Can't retrieve the patch %(name)s (%(sum)s)!" % rev))
                 revisions.write(cr, uid, rev.id, {'patch':patch[1]})
                 cr.commit()
+        text += self._get_text_unbalance_aml(cr, uid, context)
         return self.write(cr, uid, ids, {
             'message' : text,
             'state' : 'need-install',
@@ -186,6 +199,7 @@ class upgrade(osv.osv_memory):
             if any(rev.need_download for rev in next_revisions) and not is_online():
                 text += _("But it seems you don't have an Internet access. " \
                           "Please provide the patches manually.")+"\n"
+            text += self._get_text_unbalance_aml(cr, uid, context)
         else:
             text += "\n"+_("Your OpenERP version is up-to-date.")+"\n"
         return text
