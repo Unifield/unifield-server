1e955f5765cd18ad8867889e4138853abb4cf23e
Quentin THEURET <qt@tempo-consulting.fr>
2015-07-01 09:17:29 +0200

    US-407 [FIX] If the scheduler is ran two times, the second one should wait the end of the first one

--- procurement_report/wizard/schedulers_all.py
+++ procurement_report/wizard/schedulers_all.py
@@ -53,11 +53,27 @@ class procurement_purchase_compute_all(osv.osv_memory):
         @param ids: List of IDs selected
         @param context: A standard dictionary
         """
+        if context is None:
+            context = {}
+
+        run_id = self.pool.get('procurement.purchase.compute.all.running').create(cr, uid, {}, context=context)
+        context.update({'run_id': run_id})
+
         threaded_calculation = threading.Thread(target=self._procure_calculation_all_purchase, args=(cr, uid, ids, context))
         threaded_calculation.start()
         return {'type': 'ir.actions.act_window_close'}
 
 procurement_purchase_compute_all()
+
+
+class procurement_purchase_compute_all_running(osv.osv_memory):
+    _name = 'procurement.purchase.compute.all.running'
+
+    _columns = {
+    }
+
+procurement_purchase_compute_all_running()
+
 
 class procurement_min_max_compute_all(osv.osv_memory):
     _name = 'procurement.min.max.compute.all'
