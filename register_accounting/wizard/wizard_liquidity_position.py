93a8f9ff041d14bc808f97ec69272a3bd841ce83
Stephane Codazzi stephane.codazzi@tempo-consulting.fr <>
2015-08-14 15:14:06 +0200

    US_336: Add subTotal per devices, add a wizard for choose period, and add PDF export

---
+++ register_accounting/wizard/wizard_liquidity_position.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) TeMPO Consulting (<http://www.tempo-consulting.fr/>), MSF.
+#    All Rigts Reserved
+#    Developer: Stéphane Codazzi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+
+class wizard_liquidity_position(osv.osv_memory):
+    _name = 'wizard.liquidity.position'
+
+    def _get_default_period(self, cr, uid, context=None):
+        args = [('state', '!=', 'created')]
+        period = self.pool.get('account.period')
+        period_ids = period.search(cr, uid, args, limit=1, order='number desc',
+                                   context=context)
+        return period_ids and period_ids[0] or None
+
+    _columns = {
+        'period_id': fields.many2one('account.period', 'Period', required=True,
+                                     domain="[('state', '!=', 'created')]"),
+        'export_type': fields.selection(
+                                        (('excel', 'Excel'),
+                                         ('pdf', 'PDF')),
+                                        'Export type', required=True),
+    }
+
+    _defaults = {
+        'export_type': 'pdf',
+        'period_id': _get_default_period,
+    }
+
+    def create_report(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        wiz_info = self.read(cr, uid, ids, ['export_type', 'period_id'])[0]
+        context['period_id'] = wiz_info.get('period_id', None)
+
+        if wiz_info.get('export_type', None) == 'excel':
+            # Call excel report
+            data = {
+                'ids': [],
+                'model': 'account.bank.statement',
+                'context': context,
+                }
+            return {
+                'type': 'ir.actions.report.xml',
+                'report_name': 'liquidity.position.2',
+                'datas': data
+            }
+        elif wiz_info.get('export_type', None) == 'pdf':
+            # Call pdf report
+            data = {
+                'ids': [],
+                'model': 'account.bank.statement',
+                'context': context,
+            }
+            return {
+                'type': 'ir.actions.report.xml',
+                'report_name': 'liquidity.position.3',
+                'datas': data,
+            }
+        else:
+            raise osv.except_osv(_('Export Liquidity Position'),
+                                 _('Select a valid export type.'))
+
+wizard_liquidity_position()
