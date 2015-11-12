7a929fb963ae5126ef156e26737eb96fbba0e8b3
duy.vo@geneva.msf.org <>
2015-11-12 10:49:52 +0100

    US-702: remove buttons in some more views

--- sync_so/rw_special_view_handler.py
+++ sync_so/rw_special_view_handler.py
@@ -19,6 +19,7 @@
 #
 ##############################################################################
 from osv import osv, fields
+from lxml import etree
 
 class sale_order_rw(osv.osv):
     _inherit = "sale.order"
@@ -112,4 +113,80 @@ class stock_warehouse_automatic_supply_rw(osv.osv):
         return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.REMOTE_WAREHOUSE)
     
 stock_warehouse_automatic_supply_rw()
+
+
+#US-702: remove buttons in some more views
+class stock_inventory_rw(osv.osv):
+    _name = 'stock.inventory'
+    _inherit = 'stock.inventory'
+    
+    # Do not show the button new, duplicate in the tree and form view
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        so_po_common = self.pool.get('so.po.common')
+        res = super(stock_inventory_rw, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+        return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.CENTRAL_PLATFORM)
+    
+stock_inventory_rw()
+
+class initial_stock_inventory_rw(osv.osv):
+    _name = 'initial.stock.inventory'
+    _inherit = 'initial.stock.inventory'
+    
+    # Do not show the button new, duplicate in the tree and form view
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        so_po_common = self.pool.get('so.po.common')
+        res = super(initial_stock_inventory_rw, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+        return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.CENTRAL_PLATFORM)
+    
+initial_stock_inventory_rw()
+
+class res_partner_rw(osv.osv):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+    
+    # Do not show the button new, duplicate in the tree and form view
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        so_po_common = self.pool.get('so.po.common')
+        res = super(res_partner_rw, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+        return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.REMOTE_WAREHOUSE)
+    
+res_partner_rw()
+
+
+class stock_picking_out_rw(osv.osv):
+    _name = 'stock.picking'
+    _inherit = 'stock.picking'
+    
+    # Do not show the button new, duplicate in the tree and form view
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        so_po_common = self.pool.get('so.po.common')
+        res = super(stock_picking_out_rw, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+        
+        if view_type in ['tree','form'] and 'out' in res['name']:
+            return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.CENTRAL_PLATFORM)
+        elif view_type in ['form'] and 'picking' in res['name']:
+            rw_type = self.pool.get('stock.picking')._get_usb_entity_type(cr, uid)
+            if rw_type == so_po_common.REMOTE_WAREHOUSE:
+                root = etree.fromstring(res['arch'])
+                root.set('hide_new_button', 'True')
+                root.set('hide_delete_button', 'True')
+                root.set('hide_duplicate_button', 'True')
+                res['arch'] = etree.tostring(root)            
+        return res
+    
+stock_picking_out_rw()
+
+class pack_type_rw(osv.osv):
+    _name = 'pack.type'
+    _inherit = 'pack.type'
+    
+    # Do not show the button new, duplicate in the tree and form view
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        so_po_common = self.pool.get('so.po.common')
+        res = super(pack_type_rw, self).fields_view_get(cr, uid, view_id=view_id, view_type=view_type, context=context, toolbar=toolbar,submenu=False)
+        return so_po_common.rw_view_remove_buttons(cr, uid, res, view_type, so_po_common.CENTRAL_PLATFORM)
+    
+pack_type_rw()
+
+
 
