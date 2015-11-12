de18d05f47c4e4a4ebfe6b9f44a513030390f546
duy.vo@geneva.msf.org <>
2015-11-12 11:07:56 +0100

    US-702: Add the suffix RW to PPL documents

--- sync_remote_warehouse/wizard/setup_remote_warehouse.py
+++ sync_remote_warehouse/wizard/setup_remote_warehouse.py
@@ -40,6 +40,7 @@ class setup_remote_warehouse(osv.osv_memory):
         'picking.ticket',
         'stock.picking.internal',
         'stock.picking.out',
+        'stock.picking',
         'procurement.request',
         'sale.order',
         'purchase.order',
