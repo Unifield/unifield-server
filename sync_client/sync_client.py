c72d5d4c72815793f7af013babf7b4aa38203010
Siméon Blanc <sbc@sbc.sbc>
2015-11-27 16:59:59 +0100

    A few optimizations
     1) We try to make the packets as full as possible
    
     2) We don't browse updates which are useless for the syncing instances

--- sync_client/sync_client.py
+++ sync_client/sync_client.py
@@ -577,20 +577,22 @@ class Entity(osv.osv):
         last = (last_seq >= max_seq)
         updates_count = 0
         logger_index = None
-        proxy = self.pool.get("sync.client.sync_server_connection").get_connection(cr, uid, "sync.server.sync_manager")
         while not last:
+            proxy = self.pool.get("sync.client.sync_server_connection").get_connection(cr, uid, "sync.server.sync_manager")
             res = proxy.get_update(entity.identifier, self._hardware_id, last_seq, offset, max_packet_size, max_seq, recover)
             if res and res[0]:
-                updates_count += updates.unfold_package(cr, uid, res[1], context=context)
-                if logger and updates_count:
-                    if logger_index is None: logger_index = logger.append()
-                    logger.replace(logger_index, _("Update(s) received: %d") % updates_count)
-                    logger.write()
+                for package in (res[1] or []):
+                    updates_count += updates.unfold_package(cr, uid, package, context=context)
+                    if logger and updates_count:
+                        if logger_index is None: logger_index = logger.append()
+                        logger.replace(logger_index, _("Update(s) received: %d") % updates_count)
+                        logger.write()
+                    if package:
+                        #TODO: Uncomment that before testing
+                        #check_md5(res[3], res[1], _('method get_update'))
+                        offset = package['offset']
+                        self.write(cr, uid, entity.id, {'update_offset' : offset}, context=context)
                 last = res[2]
-                if res[1]:
-                    check_md5(res[3], res[1], _('method get_update'))
-                    offset = res[1]['offset']
-                    self.write(cr, uid, entity.id, {'update_offset' : offset}, context=context)
             elif res and not res[0]:
                 raise Exception, res[1]
             cr.commit()
@@ -909,7 +911,6 @@ class Entity(osv.osv):
 
 
         return True
-
 
     @sync_process()
     def sync_withbackup(self, cr, uid, context=None):
