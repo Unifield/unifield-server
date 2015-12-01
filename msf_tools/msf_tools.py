355ab69a71958d36d062a2d2848a70e5e034584e
Quentin THEURET <qt@tempo-consulting.fr>
2015-12-01 09:18:55 +0100

    SP-218 [FIX] Add a new patch to run again the patch of US-394

--- msf_tools/msf_tools.py
+++ msf_tools/msf_tools.py
@@ -658,14 +658,14 @@ class ir_translation(osv.osv):
 
     # US_394: add xml_id for each lines
     def add_xml_ids(self, cr, uid, context=None):
-        domain = [('type', '=', 'model')]
+        domain = [('type', '=', 'model'), ('xml_id', '=', False)]
         translation_ids = self.search(cr, uid, domain, context=context)
         translation_obj = self.browse(cr, uid, translation_ids, context=context)
         for translation in translation_obj:
-            if translation['xml_id'] is None:
-                xml_id = self.get_xml_id(cr, uid, translation, context=context)
-                vals = {'xml_id': xml_id}
-                self.write(cr, uid, translation['id'], vals, context=context)
+            v = {'name': translation.name, 'res_id': translation.res_id}
+            xml_id = self.get_xml_id(cr, uid, v, context=context)
+            vals = {'xml_id': xml_id}
+            self.write(cr, uid, translation['id'], vals, context=context)
         return
 
     # US_394: remove orphean ir.translation lines
