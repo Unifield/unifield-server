991ed5518b9157e89d68210c3f8840bfda939176
Vincent GREINER <vg@tempo-consulting.fr>
2016-01-21 12:13:57 +0100

    US-822 [IMP] JE/JI entries Period 0/Period 16 are fx rate passthrough (no rate applied via sync of fx rate change

--- res_currency_functional/account_move_compute_currency.py
+++ res_currency_functional/account_move_compute_currency.py
@@ -167,7 +167,8 @@ class account_move_compute_currency(osv.osv):
                 amount += line.debit - line.credit
                 amount_currency += line.amount_currency
             
-            if len(sorted_line_ids) > 2:
+            if move.period_id and not move.period_id.is_system \
+                and len(sorted_line_ids) > 2:
                 if abs(amount_currency) > 10 ** -4 and abs(amount) < 10 ** -4:
                     # The move is balanced, but there is a difference in the converted amounts;
                     # the second-biggest move line is modified accordingly
