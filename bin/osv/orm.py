3e823cad6ad67eb3b647c1e3b7f635448b5037e6
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-01-28 15:01:39 +0100

    US-867 [FIX] the change of the intersect method introduce a small danger. Before introducing it, intersect was also working if the arguments where not list but string, this is bad and sould never happen, but we found at least one place where it was like that. So it is safer to revert this change as the performance gain in an init sync was rediculus.

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -78,7 +78,8 @@ def last_day_of_current_month():
     return time.strftime('%Y-%m-' + last_day)
 
 def intersect(la, lb):
-    return list(set(la).intersection(lb))
+    return filter(lambda x: x in lb, la)
+    #return list(set(la).intersection(lb)) # this is less compatible, see US-867
 
 class except_orm(Exception):
     def __init__(self, name, value):
