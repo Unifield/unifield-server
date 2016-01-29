f28ec6118587cf6145ce8da2c42ca061f58daa67
Vincent GREINER <vg@tempo-consulting.fr>
2016-01-29 16:56:02 +0100

    US-822 [FIX] GL report: account view amounts: initial balance lines were not included

--- account/report/account_general_ledger.py
+++ account/report/account_general_ledger.py
@@ -532,7 +532,12 @@ class general_ledger(report_sxw.rml_parse, common_report_header):
         if self.target_move == 'posted':
             move_state = ['posted','']
 
-        initial_balance = self.init_balance and is_sub_total and ccy or False
+        initial_balance = False
+        if self.init_balance:
+            if booking:
+                initial_balance = is_sub_total and ccy or False
+            else:
+                initial_balance = not is_sub_total
         amount = self.__sum_amount_account(account, move_state, 'debit',
             ccy=ccy, booking=booking, initial_balance=initial_balance)
         return self._currency_conv(amount)
@@ -554,7 +559,12 @@ class general_ledger(report_sxw.rml_parse, common_report_header):
         if self.target_move == 'posted':
             move_state = ['posted','']
 
-        initial_balance = self.init_balance and is_sub_total and ccy or False
+        initial_balance = False
+        if self.init_balance:
+            if booking:
+                initial_balance = is_sub_total and ccy or False
+            else:
+                initial_balance = not is_sub_total
         amount = self.__sum_amount_account(account, move_state, 'credit',
             ccy=ccy, booking=booking, initial_balance=initial_balance)
         return self._currency_conv(amount)
@@ -581,7 +591,12 @@ class general_ledger(report_sxw.rml_parse, common_report_header):
         if self.target_move == 'posted':
             move_state = ['posted','']
 
-        initial_balance = self.init_balance and is_sub_total and ccy or False
+        initial_balance = False
+        if self.init_balance:
+            if booking:
+                initial_balance = is_sub_total and ccy or False
+            else:
+                initial_balance = not is_sub_total
         amount = self.__sum_amount_account(account, move_state, 'balance',
             ccy=ccy, booking=booking, initial_balance=initial_balance)
         return self._currency_conv(amount)
