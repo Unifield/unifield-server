c97076d8a9499a6af180682d2fc53d8fc51f2654
jf <jfb@tempo-consulting.fr>
2016-02-02 15:10:04 +0100

    US-880: on register wizard, display only unused msf.instance, warn if instance name and prop instance name are not the same

--- msf_instance/msf_instance_installer.py
+++ msf_instance/msf_instance_installer.py
@@ -11,12 +11,25 @@ class msf_instance_setup(osv.osv_memory):
     _inherit = 'res.config'
 
     _columns = {
-         'instance_id': fields.many2one('msf.instance', string="Proprietary instance", required=True),
+         'instance_id': fields.many2one('msf.instance', string="Proprietary instance", required=True, domain=[('instance', '=', False)]),
     }
 
+    def check_name(self, cr, uid, id, instance_id, context=None):
+        if instance_id:
+            instance_code = self.pool.get('msf.instance').read(cr, uid, instance_id, ['code'])['code']
+            entity_obj = self.pool.get('sync.client.entity').get_entity(cr, uid, context=context)
+            if instance_code != entity_obj.name: #instance_code not in entity_obj.name:
+                return {'warning': {'title': _('Warning'), 'message': _("The proprietary instance's name is not the same as the instance's name")}}
+        return {}
+
     def get_instance(self, cr, uid, context=None):
-        instance_ids = self.pool.get('msf.instance').search(cr, uid, [], limit=1)
-        return instance_ids and instance_ids[0] or False
+        entity_obj = self.pool.get('sync.client.entity')
+        if entity_obj:
+            entity = entity_obj.get_entity(cr, uid, context=context)
+            instance_ids = self.pool.get('msf.instance').search(cr, uid, [('code', '=', entity.name), ('instance','=', False)])
+            if instance_ids:
+                return instance_ids[0]
+        return False
 
     _defaults = {
         'instance_id': get_instance,
