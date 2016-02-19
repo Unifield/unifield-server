028e478edfdf818223f384e47c5f528f12064488
Julie Nuguet <julie@nuguet.fr>
2016-02-19 15:15:57 +0100

    UF-1014 [IMP] partially posted lines can't be revalidated

--- msf_accrual/wizard/wizard_accrual_validation.py
+++ msf_accrual/wizard/wizard_accrual_validation.py
@@ -37,6 +37,8 @@ class wizard_accrual_validation(osv.osv_memory):
                 # check for periods, distribution, etc.
                 if accrual_line.state == 'posted':
                     raise osv.except_osv(_('Warning !'), _("The line '%s' is already posted!") % accrual_line.description)
+                elif accrual_line.state == 'partially_posted':
+                    raise osv.except_osv(_('Warning !'), _("The line '%s' is already partially posted!") % accrual_line.description)
                 elif not accrual_line.period_id:
                     raise osv.except_osv(_('Warning !'), _("The line '%s' has no period set!") % accrual_line.description)
                 elif not accrual_line.analytic_distribution_id:
