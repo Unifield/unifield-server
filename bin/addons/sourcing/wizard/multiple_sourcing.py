999b445c20e99234e848de1089a6803116c5a9a1
Quentin THEURET <qt@tempo-consulting.fr>
2016-03-01 16:34:24 +0100

    US-574 [CLN] Clean code of bin/addons/sourcing/wizard/multiple_sourcing.py

--- bin/addons/sourcing/wizard/multiple_sourcing.py
+++ bin/addons/sourcing/wizard/multiple_sourcing.py
@@ -23,11 +23,12 @@ from osv import fields
 from osv import osv
 from tools.translate import _
 
-from sourcing.sale_order_line import _SELECTION_PO_CFT
+from ..sale_order_line import _SELECTION_PO_CFT
 
 _SELECTION_TYPE = [
     ('make_to_stock', 'from stock'),
     ('make_to_order', 'on order'), ]
+
 
 class multiple_sourcing_wizard(osv.osv_memory):
     _name = 'multiple.sourcing.wizard'
@@ -74,8 +75,16 @@ class multiple_sourcing_wizard(osv.osv_memory):
     def default_get(self, cr, uid, fields_list, context=None):
         """
         Set lines with the selected lines to source
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param fields_list: List of field names to fill with default value
+        :param context: Context of the call
+        :return: A dictionary that contains the field name as key and the default value for this field as value.
         """
-        if not context:
+        sol_obj = self.pool.get('sale.order.line')
+        user_obj = self.pool.get('res.users')
+
+        if context is None:
             context = {}
 
         active_ids = context.get('active_ids')
@@ -84,19 +93,20 @@ class multiple_sourcing_wizard(osv.osv_memory):
 
         res = super(multiple_sourcing_wizard, self).default_get(cr, uid, fields_list, context=context)
 
-        res['line_ids'] = []
-        res['error_on_lines'] = False
-        res['run_scheduler'] = True
+        res.update({
+            'line_ids': [],
+            'error_on_lines': False,
+            'run_scheduler': True,
+            'type': 'make_to_stock',
+            'po_cft': False,
+        })
 
         # Check if all lines are with the same type, then set that type, otherwise set make_to_order
-
-        # Ignore all lines which have already been sourced, if there are some alredy sourced lines, a message
+        # Ignore all lines which have already been sourced, if there are some already sourced lines, a message
         # will be displayed at the top of the wizard
-        res['type'] = 'make_to_stock'
-        res['po_cft'] = False
-        loc = -1 # first location flag
-        supplier = -1 # first location flag
-        for line in self.pool.get('sale.order.line').browse(cr, uid, active_ids, context=context):
+        loc = -1  # first location flag
+        supplier = -1  # first location flag
+        for line in sol_obj.browse(cr, uid, active_ids, context=context):
             if line.state == 'draft' and line.sale_order_state == 'validated':
                 res['line_ids'].append(line.id)
             else:
@@ -106,52 +116,70 @@ class multiple_sourcing_wizard(osv.osv_memory):
                 res['type'] = 'make_to_order'
                 res['po_cft'] = 'po'
 
-                loc = False # always set False for location if source on order
+                loc = False  # always set False for location if source on order
                 if not line.supplier:
                     supplier = False
                 else:
                     temp = line.supplier.id
-                    if supplier == -1: # first location
+                    if supplier == -1:  # first location
                         supplier = temp
                     elif supplier != temp:
                         supplier = False
             else:
-                # UTP-1021: Calculate the location to set into the wizard view if all lines are sourced from the same location
-                supplier = False # if source from stock, always set False to partner
+                # UTP-1021: Calculate the location to set into the wizard view if all lines
+                # are sourced from the same location
+                supplier = False  # if source from stock, always set False to partner
                 temploc = line.location_id.id
-                if loc == -1: # first location
+                if loc == -1:  # first location
                     loc = temploc
                 elif temploc != loc:
                     loc = False
 
-        # UTP-1021: Set default values on openning the wizard
+        # UTP-1021: Set default values on opening the wizard
         if loc != -1:
             res['location_id'] = loc
         if supplier != -1:
             res['supplier_id'] = supplier
 
         if not res['line_ids']:
-            raise osv.except_osv(_('Error'), _('No non-sourced lines are selected. Please select non-sourced lines'))
+            raise osv.except_osv(
+                _('Error'),
+                _('No non-sourced lines are selected. Please select non-sourced lines'),
+            )
 
-        res['company_id'] = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.id
+        res['company_id'] = user_obj.browse(cr, uid, uid, context=context).company_id.id
 
         return res
 
     def save_lines(self, cr, uid, ids, context=None):
-        '''
-        Set values to sourcing lines
-        '''
-        if not context:
+        """
+        Set values defined on the wizard to the lines of the wizard.
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard to save
+        :param context: Context of the call
+        :return: Close the wizard window
+        """
+        line_obj = self.pool.get('sale.order.line')
+
+        if context is None:
             context = {}
 
-        line_obj = self.pool.get('sale.order.line')
+        if isinstance(ids, (int, long)):
+            ids = [ids]
 
         for wiz in self.browse(cr, uid, ids, context=context):
             if wiz.type == 'make_to_order':
                 if not wiz.po_cft:
-                    raise osv.except_osv(_('Error'), _('The Procurement method should be filled !'))
+                    raise osv.except_osv(
+                        _('Error'),
+                        _('The Procurement method should be filled !'),
+                    )
                 elif wiz.po_cft != 'cft' and not wiz.supplier_id:
-                    raise osv.except_osv(_('Error'), _('You should select a supplier !'))
+                    raise osv.except_osv(
+                        _('Error'),
+                        _('You should select a supplier !'),
+                    )
 
             errors = {}
             for line in wiz.line_ids:
@@ -161,11 +189,12 @@ class multiple_sourcing_wizard(osv.osv_memory):
                     errors[err_msg].append((line.id, '%s of %s' % (line.line_number, line.order_id.name)))
                 else:
                     try:
-                        line_obj.write(cr, uid, [line.id], {'type': wiz.type,
-                                                            'po_cft': wiz.po_cft,
-                                                            'supplier': wiz.supplier_id and wiz.supplier_id.id or False,
-                                                            'location_id': wiz.location_id.id and wiz.location_id.id or False},
-                                                             context=context)
+                        line_obj.write(cr, uid, [line.id], {
+                            'type': wiz.type,
+                            'po_cft': wiz.po_cft,
+                            'supplier': wiz.supplier_id and wiz.supplier_id.id or False,
+                            'location_id': wiz.location_id.id and wiz.location_id.id or False,
+                        }, context=context)
                     except osv.except_osv, e:
                         errors.setdefault(e.value, [])
                         errors[e.value].append((line.id, '%s of %s' % (line.line_number, line.order_id.name)))
@@ -188,13 +217,18 @@ class multiple_sourcing_wizard(osv.osv_memory):
         return {'type': 'ir.actions.act_window_close'}
 
     def source_lines(self, cr, uid, ids, context=None):
-        '''
-        Confirm all lines
-        '''
+        """
+        Confirm the sourcing of all the sale.order.line contained in the wizards.
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard that contain lines to confirm
+        :param context: Context of the call
+        :return: Close the wizard window
+        """
         # Objects
         line_obj = self.pool.get('sale.order.line')
 
-        if not context:
+        if context is None:
             context = {}
 
         if isinstance(ids, (int, long)):
@@ -208,7 +242,8 @@ class multiple_sourcing_wizard(osv.osv_memory):
                 run_scheduler = True
             for line in wiz.line_ids:
                 if line.order_id.procurement_request and wiz.po_cft == 'dpo':
-                    raise osv.except_osv(_('Error'), _('You cannot choose Direct Purchase Order as method to source Internal Request lines.'))
+                    raise osv.except_osv(_('Error'), _(
+                        'You cannot choose Direct Purchase Order as method to source Internal Request lines.'))
                 lines_to_confirm.append(line.id)
 
         line_obj.confirmLine(cr, uid, lines_to_confirm, run_scheduler=run_scheduler, context=context)
@@ -216,10 +251,15 @@ class multiple_sourcing_wizard(osv.osv_memory):
         return {'type': 'ir.actions.act_window_close'}
 
     def save_source_lines(self, cr, uid, ids, context=None):
-        '''
-        Set values to sourcing lines and confirm them
-        '''
-        if not context:
+        """
+        Set values on sale.order.lines of the wizard and confirm them
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard that contain the lines to save and confirmed
+        :param context: Context of the call
+        :return: Close the wizard window
+        """
+        if context is None:
             context = {}
 
         self.save_lines(cr, uid, ids, context=context)
@@ -228,9 +268,22 @@ class multiple_sourcing_wizard(osv.osv_memory):
         return {'type': 'ir.actions.act_window_close'}
 
     def change_type(self, cr, uid, ids, l_type, context=None):
-        '''
+        """
         Unset the other fields if the type is 'from stock'
-        '''
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard on which field values has to be changed
+        :param l_type: Value of the field 'type'
+        :param context: Context of the call
+        :return: A dictionary that contains the field names to change as keys and the value for these fields as values.
+        """
+        global stock_loc
+        wh_obj = self.pool.get('stock.warehouse')
+        sol_obj = self.pool.get('sale.order.line')
+
+        if context is None:
+            context = {}
+
         if l_type == 'make_to_order':
             return {'value': {'location_id': False}}
 
@@ -244,37 +297,53 @@ class multiple_sourcing_wizard(osv.osv_memory):
         if not active_ids:
             return res
 
-        wh_obj = self.pool.get('stock.warehouse')
+        # Get the default stock location of the default warehouse
         wh_ids = wh_obj.search(cr, uid, [], context=context)
+        stock_loc = False
         if wh_ids:
             stock_loc = wh_obj.browse(cr, uid, wh_ids[0], context=context).lot_stock_id.id
 
         all_line_empty = True
-        for line in self.pool.get('sale.order.line').browse(cr, uid, active_ids, context=context):
+        for line in sol_obj.browse(cr, uid, active_ids, context=context):
             if line.location_id and line.location_id.id != stock_loc:
                 all_line_empty = False
 
-        if all_line_empty: # by default, and if all lines has no location, then set by default Stock
+        if all_line_empty:  # by default, and if all lines has no location, then set by default Stock
             return {'value': {'po_cft': False, 'supplier_id': False, 'location_id': stock_loc}}
+
         return {'value': {'po_cft': False, 'supplier_id': False}}
 
     def change_po_cft(self, cr, uid, ids, po_cft, context=None):
-        '''
-        Unset the supplier if tender is choosen
-        '''
+        """
+        Unset the supplier if tender is choose
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard on which field values has to be changed
+        :param po_cft: Value of the field 'po_cft'
+        :param context: Context of the call
+        :return: A dictionary that contains the field names to change as keys and the value for these fields as values.
+        """
         if po_cft == 'cft':
             return {'value': {'supplier_id': False}}
 
         return {}
 
     def change_supplier(self, cr, uid, ids, supplier, context=None):
-        '''
+        """
         Check if the partner has an address.
-        '''
+        :param cr: Cursor to the database
+        :param uid: ID of the res.users that calls the method
+        :param ids: List of ID of multiple.sourcing.wizard on which field values has to be changed
+        :param supplier: Value of the field 'supplier_id'
+        :param context: Context of the call
+        :return: A dictionary that contains the field names to change as keys and the value for these fields as values.
+        """
         partner_obj = self.pool.get('res.partner')
 
-        result = {}
+        if context is None:
+            context = {}
 
+        result = {}
         if supplier:
             partner = partner_obj.browse(cr, uid, supplier, context)
             # Check if the partner has addresses
@@ -286,28 +355,42 @@ class multiple_sourcing_wizard(osv.osv_memory):
         return result
 
     def change_location(self, cr, uid, ids, location_id, line_ids, context=None):
-        res = {'value': {}}
-        if not location_id:
-            return res
-
-        if not line_ids or not line_ids[0] or not line_ids[0][2]:
-            return res
-
+        """
+        Update the stock value of lines according to given location.
+        :param cr: Cursor to the database
+        :param uid: ID of the user that calls this method
+        :param ids: List of ID of multiple.sourcing.wizard that contain lines to change
+        :param location_id: ID of stock.location selected in the wizard
+        :param line_ids: List of ID of sale.order.line contained in the multiple.sourcing.wizard
+        :param context: Context of the call
+        :return: A dictionary that contains the field names to change as keys and the value for these fields as values.
+        """
         line_obj = self.pool.get('sale.order.line')
+
+        res = {}
+        if not location_id or not line_ids or not line_ids[0] or not line_ids[0][2]:
+            return res
+
         active_ids = line_ids[0][2]
 
-        context = {}
-        context.update({'from_multiple_line_sourcing': False})
-        for line in self.pool.get('sale.order.line').browse(cr, uid, active_ids, context=context):
-            line_obj.write(cr, uid, [line.id], {'type': 'make_to_stock',
-                                                        'po_cft': False,
-                                                        'supplier': False,
-                                                        'location_id': location_id}, # UTP-1021: Update loc and ask the view to refresh
-                                                         context=context)
-        res = {'value':
-               {'line_ids': active_ids, 'error_on_lines': False, 'po_cft':False,}}
-        return res
+        context = {
+            'from_multiple_line_sourcing': False
+        }
+        for line in line_obj.browse(cr, uid, active_ids, context=context):
+            line_obj.write(cr, uid, [line.id], {
+                'type': 'make_to_stock',
+                'po_cft': False,
+                'supplier': False,
+                'location_id': location_id,
+            }, context=context)  # UTP-1021: Update loc and ask the view to refresh
 
+        return {
+            'value': {
+                'line_ids': active_ids,
+                'error_on_lines': False,
+                'po_cft': False,
+            },
+        }
 
 multiple_sourcing_wizard()
 
