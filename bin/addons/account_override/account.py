49834717919990d0f6ed96f444719e9e1e56250a
Vincent GREINER <vg@tempo-consulting.fr>
2016-03-07 11:49:39 +0100

    US-672 [IMP] remove by partner allowed type search field since restricted area is used

--- bin/addons/account_override/account.py
+++ bin/addons/account_override/account.py
@@ -308,62 +308,6 @@ class account_account(osv.osv):
                 raise osv.except_osv(_('Error'), _('Filter on field %s not implemented! %s') % (field_names, x,))
         return arg
 
-    def _get_fake(self, cr, uid, ids, name, args, context=None):
-        res = {}
-        if not ids:
-            return res
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        for id in ids:
-            res[id] = False
-        return res
-
-    def _get_search_is_partner_type_allowed(self, cr, uid, obj, name, args,
-        context=None):
-        """
-        US-672
-        search between following partner types operand:
-        'internal'
-        'section'
-        'external'
-        'esc'
-        'intermission'
-        'local'  # NAT employee
-        'ex':  # Expat
-        'book':  # transfer journal
-
-        domain example:
-        [('is_partner_type_allowed', '=', 'external')]
-        [('is_partner_type_allowed', 'in', ['external', 'internal'])]
-        """
-        res = []
-        if not len(args):
-            return res
-        if len(args) != 1:
-            msg = _("Domain %s not suported") % (str(args), )
-            raise osv.except_osv(_('Error'), msg)
-        if args[0][1] not in ('=', '!=', 'in', 'not in', ):
-            msg = _("Operator '%s' not suported") % (args[0][1], )
-            raise osv.except_osv(_('Error'), msg)
-
-        reverse = args[0][1] in ('!=', 'not in', )
-        operand = False if reverse else True
-        partner_types = args[0][2]
-        if not isinstance(partner_types, (list, tuple, )):
-            partner_types = [ partner_types, ]
-
-        # build domain
-        # not reversed: a=True or b=True...
-        # reversed: a=False and b=False...
-        for t in partner_types:
-            res.append(('has_partner_type_%s' % (t, ), '=', operand))
-        if not reverse:
-            count = len(res)
-            if count > 1:
-                for x in xrange(0, count-1):
-                    res.insert(0, '|')
-        return res
-
     _columns = {
         'name': fields.char('Name', size=128, required=True, select=True, translate=True),
         'activation_date': fields.date('Active from', required=True),
@@ -395,9 +339,6 @@ class account_account(osv.osv):
         'has_partner_type_local': fields.boolean('NAT'),  # NAT employee
         'has_partner_type_ex': fields.boolean('Expat'),  # Expat
         'has_partner_type_book': fields.boolean('Book'),  # transfer journal
-        'is_partner_type_allowed': fields.function(_get_fake,
-            fnct_search=_get_search_is_partner_type_allowed, method=True,
-            type='boolean', string="Parner type allowed ?", readonly=True),
     }
 
     _defaults = {
