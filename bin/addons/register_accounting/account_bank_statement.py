c09de5f7b41242a8263380f784336f5a451b3a98
Vincent GREINER <vg@tempo-consulting.fr>
2016-03-09 11:14:54 +0100

    US-672/2 [IMP] reg line create/save: account/partner compat

--- bin/addons/register_accounting/account_bank_statement.py
+++ bin/addons/register_accounting/account_bank_statement.py
@@ -1888,6 +1888,9 @@ class account_bank_statement_line(osv.osv):
                 for row in cr.dictfetchall():
                     msg = _('This cheque number has already been used')
                     raise osv.except_osv(_('Info'), (msg))
+
+        self._check_accounts_partner_compat(cr, uid, values, context=context)
+
         # Then create a new bank statement line
         absl = super(account_bank_statement_line, self).create(cr, uid, values, context=context)
         return absl
@@ -1921,6 +1924,7 @@ class account_bank_statement_line(osv.osv):
         # Case where _update_amount return False ! => this imply there is a problem with amount columns
         if not values:
             return False
+        self._check_accounts_partner_compat(cr, uid, values, context=context)
 
         # Then update analytic distribution
         res = []
@@ -2812,6 +2816,15 @@ class account_bank_statement_line(osv.osv):
         if isinstance(ids, (int, long)):
             ids = [ids]
         return self.unlink(cr, uid, ids, context=context)
+
+    def _check_accounts_partner_compat(self, cr, uid, vals, context=None):
+        # US-672/2
+        if not context.get('sync_update_execution', False) \
+            and vals.get('account_id', False) \
+            and vals.get('partner_type', False):
+            self.pool.get('account.account').is_allowed_for_thirdparty(
+                cr, uid, vals['account_id'], partner_type=vals['partner_type'],
+                from_vals=True, raise_it=True, context=context)
 
 account_bank_statement_line()
 
