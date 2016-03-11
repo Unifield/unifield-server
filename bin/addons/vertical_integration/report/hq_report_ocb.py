370cac19d26953d03940e9193ea3045315632c7d
Vincent GREINER <vg@tempo-consulting.fr>
2016-03-11 11:09:41 +0100

    US-947 [FIX] OCB VI / keep behaviour of using GL journals except for OD AJI: use analytic journal for adhoc journal in case of pure analytic correction

--- bin/addons/vertical_integration/report/hq_report_ocb.py
+++ bin/addons/vertical_integration/report/hq_report_ocb.py
@@ -432,7 +432,9 @@ class hq_report_ocb(report_sxw.report_sxw):
                 """,
             # Pay attention to take analytic line that are not on HQ and MIGRATION journals.
             'rawdata': """
-                SELECT al.id, i.code, aj.code, al.entry_sequence, al.name, al.ref, al.document_date, al.date, 
+                SELECT al.id, i.code,
+                       CASE WHEN j.code = 'OD' THEN j.code ELSE aj.code END AS journal,
+                       al.entry_sequence, al.name, al.ref, al.document_date, al.date,
                        a.code, al.partner_txt, aa.code AS dest, aa2.code AS cost_center_id, aa3.code AS funding_pool, 
                        CASE WHEN al.amount_currency < 0 AND aml.is_addendum_line = 'f' THEN ABS(al.amount_currency) ELSE 0.0 END AS debit, 
                        CASE WHEN al.amount_currency > 0 AND aml.is_addendum_line = 'f' THEN al.amount_currency ELSE 0.0 END AS credit, 
