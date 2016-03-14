3b3edf11dcd0f1344b4727585612741ac81f4925
jf <jfb@tempo-consulting.fr>
2016-03-14 09:08:36 +0100

    US-852 [FIX] AJIs sync: do not execute AJI sync update if the linked JI is not run If coordo allocates a JI to a project CC, at project the AJI is not linked to a JI, but the update should be executed at project

--- sync_client/update.py
+++ sync_client/update.py
@@ -801,7 +801,9 @@ class update_received(osv.osv):
                         #US-852: if account_move_line is given, then cannot use the fallback value, but exit the import!
                         # THIS FIX COULD ALSO OPEN FOR OTHER BUG, BUT CHECK IF THE RULES THAT CONTAIN THE OBJECT (HERE account_move_line) 
                         if 'account_move_line' in xmlid:
-                            return False
+                            m, sep, sdref = xmlid.partition('.')
+                            if self.search(cr, uid, [('sdref', '=', sdref), ('run', '=', False)], order='NO_ORDER', context=context):
+                                return False
                         fb = fallback.get(field, False)
                         if not fb:
                             raise ValueError("no fallback value defined")
