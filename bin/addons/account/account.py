d40c8d011ec49e8bf260934463123b864e1ae3e1
jf <jfb@tempo-consulting.fr>
2016-03-23 16:23:47 +0100

    US-791 [FIX] Undefined variable 'account'

--- bin/addons/account/account.py
+++ bin/addons/account/account.py
@@ -324,7 +324,7 @@ class account_account(osv.osv):
                     for child in current.child_id:
                         # in context of report, if the current account is not
                         # displayed, it should no impact the total amount
-                        if display_only_checked_account and not account.display_in_reports:
+                        if display_only_checked_account and not child.display_in_reports:
                             continue
                         if child.company_id.currency_id.id == current.company_id.currency_id.id:
                             sums[current.id][fn] += sums[child.id][fn]
