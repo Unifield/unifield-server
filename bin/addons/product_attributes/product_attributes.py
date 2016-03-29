097c24c48174c926b29cdb9a811db4247afe4334
jftempo <jfb@tempo-consulting.fr>
2016-03-29 08:09:11 +0200

    UniData [FIX] wrong variable user US-979  UD: Synched Inactivation of product treatment US-749  UD: closed_article / Closed Article US-750  UD: heat_sensitive_item / Temperature Sensitive item & cold_chain / Cold Chain US-751  UD: controlled_substance / Controlled substance (NP/Narcotic Psychotropic) US-762  UD: Volume / Volume US-780  UD: New field UniData status US-760  UD: single_use / Single Use & sterilized / Sterile US-763  UD: international_status / Product creator US-757  UD: old_code / Old code US-758  UD: product_catalog_path / Product Catalogue Path US-752  UD: dangerous_goods / Dangerous goods US-755  UD: life_time / Product Life time & short_shelf_life / Short Shelf life US-754  UD: justification_code_id / Justification Code US-753  UD: standard_ok / Standard

--- bin/addons/product_attributes/product_attributes.py
+++ bin/addons/product_attributes/product_attributes.py
@@ -1197,9 +1197,9 @@ class product_attributes(osv.osv):
 
         if 'heat_sensitive_item' in vals:
             if not vals.get('heat_sensitive_item'):
-                heat2_id = self.pool.get('ir.model.data').get_object_reference(cr, user, 'product_attributes', 'heat_no')[1]
+                heat2_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'product_attributes', 'heat_no')[1]
                 vals['heat_sensitive_item'] = heat2_id
-            vals.update(self.onchange_heat(cr, user, False, vals['heat_sensitive_item'], context=context).get('value', {}))
+            vals.update(self.onchange_heat(cr, uid, False, vals['heat_sensitive_item'], context=context).get('value', {}))
 
         for f in ['sterilized', 'closed_article', 'single_use']:
             if f in vals and not vals.get(f):
