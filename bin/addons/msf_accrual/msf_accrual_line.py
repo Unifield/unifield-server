8a0caf0aa77e213d51b378868312da190331ff2b
Julie Nuguet <julie@nuguet.fr>
2016-04-08 14:50:27 +0200

    US-1178 [IMP] remove redundancy: posting date was set twice

--- bin/addons/msf_accrual/msf_accrual_line.py
+++ bin/addons/msf_accrual/msf_accrual_line.py
@@ -178,18 +178,13 @@ class msf_accrual_line(osv.osv):
             context = {}
         if isinstance(ids, (int, long, )):
             ids = [ids]
-        if 'period_id' in vals:
-            # US-1178 set the posting date according to the selected period
-            # (by using the read-only field the new "date" value wasn't taken into account)
-            period = self.pool.get('account.period').browse(cr, uid, vals['period_id'], context)
-            vals['date'] = period.date_stop
-        posting_date = 'date' in vals and vals['date'] or self.read(cr, uid, ids, ['date', ], context=context)[0]['date']
-        if 'document_date' in vals:
-            # US-192 check doc date regarding post date
-            self.pool.get('finance.tools').check_document_date(cr, uid,
-                vals['document_date'], posting_date, context=context)
-
         self._create_write_set_vals(cr, uid, vals, context=context)
+        # US-192 check doc date regarding post date
+        current_values = self.read(cr, uid, ids, ['document_date', 'date'], context=context)[0]
+        document_date = 'document_date' in vals and vals['document_date'] or current_values['document_date']
+        posting_date = 'date' in vals and vals['date'] or current_values['date']
+        self.pool.get('finance.tools').check_document_date(cr, uid, document_date, posting_date, context=context)
+
         return super(msf_accrual_line, self).write(cr, uid, ids, vals, context=context)
     
     def button_cancel(self, cr, uid, ids, context=None):
