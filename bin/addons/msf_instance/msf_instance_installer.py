6ae6f747cab10f2901b43b62aec25ddae793b66d
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-06-02 09:48:08 +0200

    US-1362 [IMP] add one more step to warn the user about which instance he selected and that this choice cannot been changed.

--- bin/addons/msf_instance/msf_instance_installer.py
+++ bin/addons/msf_instance/msf_instance_installer.py
@@ -12,15 +12,9 @@ class msf_instance_setup(osv.osv_memory):
 
     _columns = {
          'instance_id': fields.many2one('msf.instance', string="Proprietary instance", required=True, domain=[('instance', '=', False), ('restrict_level_from_entity', '=', True)]),
+         'message': fields.text(string='message', readonly=True),
+         'first_run': fields.boolean(string='First Run'),
     }
-
-    def check_name(self, cr, uid, id, instance_id, context=None):
-        if instance_id:
-            instance_code = self.pool.get('msf.instance').read(cr, uid, instance_id, ['code'])['code']
-            entity_obj = self.pool.get('sync.client.entity').get_entity(cr, uid, context=context)
-            if instance_code != entity_obj.name: #instance_code not in entity_obj.name:
-                return {'warning': {'title': _('Warning'), 'message': _("The proprietary instance's name is not the same as the instance's name")}}
-        return {}
 
     def get_instance(self, cr, uid, context=None):
         return False
@@ -34,11 +28,40 @@ class msf_instance_setup(osv.osv_memory):
 
     _defaults = {
         'instance_id': get_instance,
+        'message': None,
+        'first_run': lambda *a: True,
     }
 
     def execute(self, cr, uid, ids, context=None):
         res = self.read(cr, uid, ids)
         self.pool.get('res.company').write(cr, uid, [self.pool.get('res.users').browse(cr, uid, uid).company_id.id], {'instance_id': res[0]['instance_id']})
         return {}
+
+    def action_check(self, cr, uid, ids, context=None):
+        current_obj = self.read(cr, uid, ids)[0]
+        if not current_obj['first_run']:
+            res = self.read(cr, uid, ids)
+            self.pool.get('res.company').write(cr, uid, [self.pool.get('res.users').browse(cr, uid, uid).company_id.id], {'instance_id': res[0]['instance_id']})
+            return self.action_next(cr, uid, ids, context=context)
+        if current_obj['instance_id']:
+            instance_code = self.pool.get('msf.instance').read(cr, uid, current_obj['instance_id'], ['code'])['code']
+            entity_obj = self.pool.get('sync.client.entity').get_entity(cr, uid, context=context)
+            vals = {
+                    'message': "You have selected the proprietary instance '%s' "\
+                        "for this instance '%s'. This choice cannot be changed.\n"\
+                        "Do you want to continue?" % (instance_code, entity_obj.name),
+                    'first_run': False,
+            }
+            self.write(cr, uid, [ids[0]], vals, context)
+        return {
+            'view_type': 'form',
+            "view_mode": 'form',
+            'res_model': 'msf_instance.setup',
+            'res_id': ids[0],
+            'view_id':self.pool.get('ir.ui.view')\
+                .search(cr,uid,[('name','=','Instance Configuration')]),
+            'type': 'ir.actions.act_window',
+            'target':'new',
+            }
 
 msf_instance_setup()
