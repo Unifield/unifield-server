2b5b43fe27ab1c7da56bb76564ba0f9d34c67206
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-06-08 11:13:14 +0200

    US-1246 [IMP] partially revert a wrong commit (committed in r3756) as the write do not concern the same id, it cannot be grouped like thi

--- bin/addons/stock/stock.py
+++ bin/addons/stock/stock.py
@@ -2066,15 +2066,14 @@ class stock_move(osv.osv):
                              }
                 move_data = self._create_chained_picking_move_values_hook(cr, uid, context=context, move_data=move_data, move=move)
                 new_id = move_obj.copy(cr, uid, move.id, move_data)
-                vals = {
+                move_obj.write(cr, uid, [move.id], {
                     'move_dest_id': new_id,
                     'move_history_ids': [(4, new_id)]
-                }
+                })
                 # UF-2424: If it's an internal move, just remove the asset_id
                 if ptype == 'internal' and move.product_id.subtype == 'asset':
-                    vals.update({'asset_id': False})
+                    move_obj.write(cr, uid, [new_id], {'asset_id': False})
 
-                move_obj.write(cr, uid, [move.id], vals)
                 new_moves.append(self.browse(cr, uid, [new_id])[0])
             if pickid:
                 self._create_chained_picking_internal_request(cr, uid, context=context, picking=pickid)
