47fd97b8002f1765685fae9ece50d7e84f02e4ae
Vincent GREINER <vg@tempo-consulting.fr>
2016-06-09 13:59:48 +0200

    US-1404 [IMP] update database duplicates message

--- bin/addons/msf_homere_interface/wizard/hr_payroll_employee_import.py
+++ bin/addons/msf_homere_interface/wizard/hr_payroll_employee_import.py
@@ -246,7 +246,7 @@ class hr_payroll_employee_import(osv.osv_memory):
         if search_ids and len(search_ids) > 1:
             self.pool.get('hr.payroll.employee.import.errors').create(cr, uid, {
                 'wizard_id': wizard_id,
-                'msg': _("Database have more than one employee with the combinaison key codeterrain/id_staff(/id_unique) of this employee: %s") % (employee_name,)
+                'msg': _("Database has already one employee with the combinaison key codeterrain/id_staff(/id_unique) of this employee: %s") % (employee_name,)
                 })
             return (res, what_changed)
 
