bcbf8e9dfec6343f8c6375ae02186ffa495de3fe
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-07-25 14:08:12 +0200

    US-1246 [IMP] add the possibility to use the new parameter name_get=False for read() method. This commit should not break anything as it just give the possibiity to use name_get=False but keep by default the old behaviour (name_get=True).

--- bin/addons/msf_outgoing/wizard/picking_processor.py
+++ bin/addons/msf_outgoing/wizard/picking_processor.py
@@ -287,7 +287,8 @@ class stock_move_processor(osv.osv):
         move_dict = dict([(x['id'], x['product_id'][0]) for x in self.read(cr, uid, ids,
                                                                ['id',
                                                                 'product_id'],
-                                                               context=context)])
+                                                               context=context,
+                                                               name_get=False)])
         product_module = self.pool.get('product.product')
         product_list_dict = product_module.read(cr, uid,
                                                 move_dict.values(),
