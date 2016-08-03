9b7ae60e4b260424a60c158fd97ffad0ed85ef9f
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-08-03 17:47:46 +0200

    US-1246 [IMP] replace a browse that read many ids to get only the first with a read that read only one.

--- bin/addons/analytic_distribution_supply/purchase.py
+++ bin/addons/analytic_distribution_supply/purchase.py
@@ -302,8 +302,8 @@ class purchase_order_line(osv.osv):
             ids = [ids]
 
         if ids:
-            line = self.browse(cr, uid, ids, context=context)[0]
-            if 'price_unit' in vals and vals['price_unit'] == 0.00 and self.pool.get('purchase.order').browse(cr, uid, vals.get('order_id', line.order_id.id), context=context).from_yml_test:
+            order_id = self.read(cr, uid, ids[0], ['order_id'], context=context)['order_id'][0]
+            if 'price_unit' in vals and vals['price_unit'] == 0.00 and self.pool.get('purchase.order').read(cr, uid, vals.get('order_id', order_id), ['from_yml_test'], context=context)['from_yml_test']:
                 vals['price_unit'] = 1.00
 
         return super(purchase_order_line, self).write(cr, uid, ids, vals, context=context)
