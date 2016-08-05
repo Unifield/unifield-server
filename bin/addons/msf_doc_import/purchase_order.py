ce5a7c00cb63fae8a8d6363beb5589b79f788d3b
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-08-05 17:11:59 +0200

    US-1246 [IMP] desactivate _check_active_product for test purpose as this method could be responsible of big perf degradation.

--- bin/addons/msf_doc_import/purchase_order.py
+++ bin/addons/msf_doc_import/purchase_order.py
@@ -124,15 +124,15 @@ class purchase_order(osv.osv):
         '''
         Check if the Purchase order contains a line with an inactive products
         '''
-        inactive_lines = self.pool.get('purchase.order.line').search(cr, uid, [('product_id.active', '=', False),
-                                                                               ('order_id', 'in', ids),
-                                                                               ('order_id.state', 'not in', ['draft', 'cancel', 'done'])], context=context)
+        #inactive_lines = self.pool.get('purchase.order.line').search(cr, uid, [('product_id.active', '=', False),
+        #                                                                       ('order_id', 'in', ids),
+        #                                                                       ('order_id.state', 'not in', ['draft', 'cancel', 'done'])], context=context)
 
-        if inactive_lines:
-            plural = len(inactive_lines) == 1 and _('A product has') or _('Some products have')
-            l_plural = len(inactive_lines) == 1 and _('line') or _('lines')
-            raise osv.except_osv(_('Error'), _('%s been inactivated. If you want to validate this document you have to remove/correct the line containing those inactive products (see red %s of the document)') % (plural, l_plural))
-            return False
+        #if inactive_lines:
+        #    plural = len(inactive_lines) == 1 and _('A product has') or _('Some products have')
+        #    l_plural = len(inactive_lines) == 1 and _('line') or _('lines')
+        #    raise osv.except_osv(_('Error'), _('%s been inactivated. If you want to validate this document you have to remove/correct the line containing those inactive products (see red %s of the document)') % (plural, l_plural))
+        #    return False
         return True
 
     _constraints = [
