4b431e699f3188766c9d0ee63eeb20e3ac917bab
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2016-10-18 09:09:04 +0200

    US-1491 - US-1690 [MERGE] the 2 tickets in one branch to be able to have only one AIO for tests

--- bin/tools/config.py
+++ bin/tools/config.py
@@ -104,6 +104,7 @@ class configmanager(object):
             'osv_memory_count_limit': None, # number of records in each osv_memory virtual table
             'osv_memory_age_limit': 1, # hours
             'additional_xml': False,
+            'attachments_path': None,
             'create_db_dir_for_attachment': True,
         }
 
