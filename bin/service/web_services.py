c8c95d0b6ca835ab59366aff5029f3f8b5fb3bc5
Jeff Allen <jeff.allen@geneva.msf.org>
2016-10-19 12:52:18 +0200

    Turned tabs back into spaces.

--- bin/service/web_services.py
+++ bin/service/web_services.py
@@ -210,11 +210,11 @@ class db(netsvc.ExportService):
         return True
 
     def _set_pg_psw_env_var(self):
-	if tools.config['db_password']:
+        if tools.config['db_password']:
             os.environ['PGPASSWORD'] = tools.config['db_password']
 
     def _unset_pg_psw_env_var(self):
-	if 'PGPASSWORD' in os.environ:
+        if 'PGPASSWORD' in os.environ:
             del os.environ['PGPASSWORD']
 
     def exp_dump_file(self, db_name):
