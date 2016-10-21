097b306e2188fac1e5846588fc3d480b632fdad5
Jeff Allen <jeff.allen@geneva.msf.org>
2016-10-21 16:10:11 +0200

    More helpful error messages.

--- tools/pyflakes/__init__.py
+++ tools/pyflakes/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from bzrlib import branch, errors
+from distutils.spawn import find_executable
 import subprocess
 
 def pyflakes_check(local_branch, master_branch, old_revision_number, old_revision_id, future_revision_number, future_revision_id, tree_delta, future_tree):
@@ -14,13 +15,16 @@ def pyflakes_check(local_branch, master_branch, old_revision_number, old_revisio
     if len(files_to_check) == 0:
         return
 
+    for exe in [ 'autopep8', 'pyflakes' ]:
+        if find_executable(exe) is None:
+            raise errors.BzrError("Required command %s is missing." % exe)
+
     cmd = [ 'autopep8', '--select=E1', '--diff' ]
     cmd.extend(files_to_check)
     res = subprocess.check_output(cmd)
     if res != '':
-        print "Running:", " ".join(cmd)
-        subprocess.call(cmd)
-        raise errors.BzrError("Fix autopep8 warnings before committing.")
+        cmd[2] = '--in-place'
+        raise errors.BzrError("Fix autopep8 warnings before committing. You might want to use this command: %s" % " ".join(cmd))
 
     cmd = [ 'pyflakes' ]
     cmd.extend(files_to_check)
