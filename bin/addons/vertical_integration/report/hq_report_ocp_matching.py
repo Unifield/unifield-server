c0e77cbe83260261b3179988fa94ee9341214724
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2016-10-28 11:06:05 +0200

    US-1838 [IMP] OCP Matching Export: take the date into account

--- bin/addons/vertical_integration/report/hq_report_ocp_matching.py
+++ bin/addons/vertical_integration/report/hq_report_ocp_matching.py
@@ -76,19 +76,21 @@ class hq_report_ocp_matching(report_sxw.report_sxw):
             # Do not take lines that come from a HQ or MIGRATION journal
             'reconcilable': """
                 SELECT aml.id, m.name AS "entry_sequence", aml.name, aml.ref, aml.document_date, aml.date, a.code, aml.partner_txt, debit_currency, credit_currency, c.name AS "Booking Currency", ROUND(aml.debit, 2), ROUND(aml.credit, 2), cc.name AS "functional_currency", aml.reconcile_id
-                FROM account_move_line AS aml, account_move AS m, account_account AS a, res_currency AS c, res_company AS e, res_currency AS cc, account_journal AS j, account_period AS p
-                WHERE aml.move_id = m.id
-                AND aml.account_id = a.id
-                AND aml.currency_id = c.id
-                AND aml.company_id = e.id
-                AND e.currency_id = cc.id
-                AND aml.journal_id = j.id
-                AND p.id = aml.period_id
-                AND a.reconcile = 't'
-                AND j.type not in %s
+                FROM account_move_line AS aml
+                INNER JOIN account_move AS m ON aml.move_id = m.id
+                INNER JOIN account_account AS a ON aml.account_id = a.id
+                INNER JOIN res_currency AS c ON aml.currency_id = c.id
+                INNER JOIN res_company AS e ON aml.company_id = e.id
+                INNER JOIN res_currency AS cc ON e.currency_id = cc.id
+                INNER JOIN account_journal AS j ON aml.journal_id = j.id
+                INNER JOIN account_period AS p ON p.id = aml.period_id
+                INNER JOIN account_move_reconcile amr ON aml.reconcile_id = amr.id
+                WHERE j.type not in %s
                 AND p.number not in (0, 16)
-                AND aml.instance_id in %s;
-                """,
+                AND aml.instance_id in %s
+                AND aml.date <= %s
+                AND ((aml.unreconcile_date >= %s and aml.unreconcile_date <= %s) OR aml.reconcile_date >= %s);
+            """,
         }
 
         # Define the file name according to the following format:
@@ -96,8 +98,10 @@ class hq_report_ocp_matching(report_sxw.report_sxw):
         # (ex: KE1_201610_171116110306_Check on reconcilable entries.csv)
         instance = instance_obj.browse(cr, uid, instance_id, context=context, fields_to_fetch=['code'])
         instance_code = instance and '_' in instance.code and instance.code.split('_')[1][:3] or ''
-        period = period_obj.browse(cr, uid, period_id, context=context, fields_to_fetch=['date_start'])
-        selected_period = strftime('%Y%m', strptime(period.date_start, '%Y-%m-%d')) or ''
+        period = period_obj.browse(cr, uid, period_id, context=context, fields_to_fetch=['date_start', 'date_stop'])
+        date_start = period.date_start
+        date_stop = period.date_stop
+        selected_period = strftime('%Y%m', strptime(date_start, '%Y-%m-%d')) or ''
         current_time = time.strftime('%d%m%y%H%M%S')
         reconcilable_entries_filename = '%s_%s_%s_Check on reconcilable entries.csv' % (instance_code, selected_period, current_time)
 
@@ -106,7 +110,7 @@ class hq_report_ocp_matching(report_sxw.report_sxw):
                 'headers': ['DB ID', 'Entry Sequence', 'Description', 'Reference', 'Document Date', 'Posting Date', 'G/L Account', 'Third Party', 'Booking Debit', 'Booking Credit', 'Booking Currency', 'Functional Debit', 'Functional Credit', 'Functional Currency', 'Reconcile reference'],
                 'filename': reconcilable_entries_filename,
                 'key': 'reconcilable',
-                'query_params': (tuple(excluded_journal_types), tuple(instance_ids),),
+                'query_params': (tuple(excluded_journal_types), tuple(instance_ids), date_stop, date_start, date_stop, date_start),
                 'function': 'postprocess_reconciliable',
                 'fnct_params': 'account.move.line',
                 },
