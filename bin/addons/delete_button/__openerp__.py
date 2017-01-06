6e58bbdbf60d48f22b62f6330c990b79e916a568
jf <jfb@tempo-consulting.fr>
2017-01-06 17:05:37 +0100

    US-2124 [FIX] Remove all default shortcuts

--- bin/addons/delete_button/__openerp__.py
+++ bin/addons/delete_button/__openerp__.py
@@ -42,7 +42,6 @@
                    'view/sale_view.xml',
                    'view/tender_view.xml',
                    'view/product_view.xml',
-                   'view/shortcut_data.xml',
                    ],
     'demo_xml': [
     ],
