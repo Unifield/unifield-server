ab2192013572b1692ab0c639f08ceba238334021
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-01-25 15:33:32 +0100

    US-1613 [IMP] display the entry sequence for account.move.line Use a special view for that to be able to display the good columns.

--- bin/addons/msf_audittrail/audittrail.py
+++ bin/addons/msf_audittrail/audittrail.py
@@ -429,6 +429,7 @@ class audittrail_rule(osv.osv):
         "field_ids": fields.many2many('ir.model.fields', 'audit_rule_field_rel', 'rule_id', 'field_id', string='Fields'),
         "parent_field_id": fields.many2one('ir.model.fields', string='Parent fields'),
         "name_get_field_id": fields.many2one('ir.model.fields', string='Displayed field value'),
+        "field_other_id": fields.many2one('ir.model.fields', string='Displayed other field value'),
     }
 
     _defaults = {
@@ -515,6 +516,9 @@ class audittrail_rule(osv.osv):
             if thisrule.object_id.model == 'account.bank.statement.line' or\
                     thisrule.object_id.model == 'account.move.line':
                 # for register line we allow to select many lines in track changes view
+                # it is required to use fct_object_id and fct_res_id instead
+                # of object_id and res_id because account.bank.statement.line are sub object of
+                # register and track changes are created this way.
                 val['domain'] = "[('fct_object_id','=', %s), ('fct_res_id', 'in', active_ids)]" % str(thisrule.object_id.id)
 
             # search if the view does not already exists
@@ -684,6 +688,13 @@ class audittrail_rule(osv.osv):
                     'res_id': parent_field_id or inherit_field_id or res_id,
                 }
 
+                if rule.field_other_id:
+                    vals.update({
+                        'other_column': self.pool.get(rule.object_id.model).read(cr, uid,
+                            res_id, [rule.field_other_id.name])[rule.field_other_id.name]
+                        }
+                    )
+
                 # Add the name of the created sub-object
                 if parent_field_id:
                     # get the parent model_id
@@ -793,6 +804,18 @@ class audittrail_log_line(osv.osv):
     _name = 'audittrail.log.line'
     _description = "Log Line"
     _order = 'timestamp desc, id desc'
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        """
+        Change the tracking view depending on the model_object
+        """
+        if view_type == 'tree' and\
+                context.get('active_model', False) in\
+                ('account.move', 'account.move.line'):
+            dataobj = self.pool.get('ir.model.data')
+            dummy, view_id = dataobj.get_object_reference(cr, 1, 'account', 'view_audittrail_log_line_account_move_tree')
+        view = super(audittrail_log_line, self).fields_view_get(cr, uid, view_id, view_type, context, toolbar, submenu)
+        return view
 
     def _get_values(self, cr, uid, ids, field_name, arg, context=None):
         '''
@@ -944,6 +967,7 @@ class audittrail_log_line(osv.osv):
           'new_value': fields.text("New Value"),
           'field_description': fields.char('Field Description', size=64),
           'trans_field_description': fields.function(_get_field_name, fnct_search=_src_field_name, method=True, type='char', size=64, string='Field Description', store=False),
+          'other_column': fields.char(size=64, string='Other information'),
           'sub_obj_name': fields.char(size=64, string='Order line'),
           # These 3 fields allows the computation of the name of the subobject (sub_obj_name)
           'rule_id': fields.many2one('audittrail.rule', string='Rule'),
