fb0a35330f0a11e9041f209c6b811b3638613949
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2017-03-23 10:02:35 +0100

    US-1643 [IMP] Partner ledger: column name 'Func. Cur.' corrected to 'Book. Curr.' + replace everywhere the currency symbol by the currency code

--- bin/addons/account/report/account_partner_ledger.py
+++ bin/addons/account/report/account_partner_ledger.py
@@ -203,7 +203,8 @@ class third_party_ledger(report_sxw.rml_parse, common_report_header):
         else:
             RECONCILE_TAG = "AND l.reconcile_id IS NULL"
         self.cr.execute(
-            "SELECT l.id, l.date, j.code, acc.code as a_code, acc.name as a_name, l.ref, m.name as move_name, l.name, COALESCE(l.debit, 0) as debit, COALESCE(l.credit, 0) as credit, l.amount_currency,l.currency_id, c.symbol AS currency_code " \
+            "SELECT l.id, l.date, j.code, acc.code as a_code, acc.name as a_name, l.ref, m.name as move_name, l.name, "
+            "COALESCE(l.debit, 0) as debit, COALESCE(l.credit, 0) as credit, l.amount_currency,l.currency_id, c.name AS currency_code "
             "FROM account_move_line l " \
             "LEFT JOIN account_journal j " \
                 "ON (l.journal_id = j.id) " \
