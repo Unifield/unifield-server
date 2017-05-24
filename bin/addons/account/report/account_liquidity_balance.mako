3a257c836f7758cf15edaf7b65883efc69fd6532
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2017-05-24 18:06:22 +0200

    US-1649 [IMP] Liquidity Balance: creation of the report (in progress)

---
+++ bin/addons/account/report/account_liquidity_balance.mako
@@ -0,0 +1,152 @@
+<?xml version="1.0"?>
+<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+xmlns:o="urn:schemas-microsoft-com:office:office"
+xmlns:x="urn:schemas-microsoft-com:office:excel"
+xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
+xmlns:html="http://www.w3.org/TR/REC-html40">
+<DocumentProperties xmlns="urn:schemas-microsoft-com:office:office">
+<Title>Liquidity Balance</Title>
+</DocumentProperties>
+<Styles>
+<Style ss:ID="ssCell">
+<Alignment ss:Vertical="Top" ss:WrapText="1"/>
+</Style>
+<Style ss:ID="ssCellRight">
+<Alignment ss:Horizontal="Right" ss:Vertical="Top" ss:WrapText="1"/>
+</Style>
+<Style ss:ID="ssCellRightBold">
+<Alignment ss:Horizontal="Right" ss:Vertical="Top" ss:WrapText="1"/>
+<Font ss:Bold="1" />
+</Style>
+<Style ss:ID="ssH">
+<Alignment ss:Horizontal="Center" ss:Vertical="Center" ss:WrapText="1"/>
+<Font ss:Bold="1" />
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+</Style>
+<Style ss:ID="ssBorder">
+<Alignment ss:Vertical="Center" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+</Style>
+<Style ss:ID="ssBorderDate">
+<Alignment ss:Vertical="Center" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+<NumberFormat ss:Format="Short Date" />
+</Style>
+<Style ss:ID="ssNumber">
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+<Alignment ss:Horizontal="Right" ss:Vertical="Center" ss:WrapText="1"/>
+<NumberFormat ss:Format="#,##0.00"/>
+</Style>
+<Style ss:ID="ssHeader">
+<Alignment ss:Vertical="Top" ss:Horizontal="Center" ss:WrapText="1"/>
+<Font ss:Bold="1" />
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+</Style>
+<Style ss:ID="ssHeaderNumber">
+<Font ss:Bold="1" />
+<Alignment ss:Horizontal="Right" ss:Vertical="Top" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+<NumberFormat ss:Format="#,##0.00"/>
+</Style>
+<Style ss:ID="ssHeaderRight">
+<Font ss:Bold="1" />
+<Alignment ss:Horizontal="Right" ss:Vertical="Top" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+</Style>
+<Style ss:ID="ssHeaderCell">
+<Alignment ss:Vertical="Top" ss:Horizontal="Center" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+</Style>
+<Style ss:ID="ssHeaderNumberCell">
+<Alignment ss:Horizontal="Right" ss:Vertical="Top" ss:WrapText="1"/>
+<Borders>
+  <Border ss:Position="Bottom" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Left" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Right" ss:LineStyle="Continuous" ss:Weight="1" />
+  <Border ss:Position="Top" ss:LineStyle="Continuous" ss:Weight="1" />
+</Borders>
+<NumberFormat ss:Format="#,##0.00"/>
+</Style>
+</Styles>
+<Worksheet ss:Name="Liquidity Balances">
+<Table x:FullColumns="1" x:FullRows="1">
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+<Column ss:AutoFitWidth="1" ss:Width="80" />
+
+% for line in get_register_data():
+<Row>
+ <Cell ss:StyleID="ssHeader"><Data ss:Type="String">${line['instance']|x}</Data></Cell>
+</Row>
+% endfor
+
+</Table>
+<WorksheetOptions xmlns="urn:schemas-microsoft-com:office:excel">
+   <FitToPage/>
+   <PageSetup>
+    <Layout x:Orientation="Portrait"/>
+    <Header x:Data="&amp;C&amp;&quot;Arial,Bold&quot;&amp;14Liquidity Balance"/>
+    <Footer x:Data="Page &amp;P of &amp;N"/>
+   </PageSetup>
+   <Print>
+    <ValidPrinterInfo/>
+    <PaperSizeIndex>9</PaperSizeIndex>
+    <HorizontalResolution>600</HorizontalResolution>
+    <VerticalResolution>600</VerticalResolution>
+   </Print>
+   <Selected/>
+   <Panes>
+    <Pane>
+     <Number>3</Number>
+     <ActiveRow>17</ActiveRow>
+    </Pane>
+   </Panes>
+   <ProtectObjects>False</ProtectObjects>
+   <ProtectScenarios>False</ProtectScenarios>
+</WorksheetOptions>
+</Worksheet>
+</Workbook>
--- bin/addons/account/report/account_liquidity_balance.py
+++ bin/addons/account/report/account_liquidity_balance.py
@@ -19,42 +19,92 @@
 #
 ##############################################################################
 
-from osv import fields
-from osv import osv
+from report import report_sxw
+from common_report_header import common_report_header
+from spreadsheet_xml.spreadsheet_xml_write import SpreadsheetReport
+from time import strptime
 
-from time import strftime
+class account_liquidity_balance(report_sxw.rml_parse, common_report_header):
 
-class liquidity_balance_wizard(osv.osv_memory):
-    _name = 'liquidity.balance.wizard'
+    def __init__(self, cr, uid, name, context=None):
+        self.instance_id = False
+        self.instance_ids = False
+        self.period_id = False
+        self.fiscalyear_id = False
+        super(account_liquidity_balance, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'get_register_data': self._get_register_data,
+        })
 
-    _columns = {
-        'instance_id': fields.many2one('msf.instance', 'Top proprietary instance', required=True),
-        'fiscalyear_id': fields.many2one('account.fiscalyear', 'Fiscal year', required=True),
-        'period_id': fields.many2one('account.period', 'Period', required=True),
-    }
+    def _get_register_data(self):
+        liquidity_sql = """
+            SELECT i.code AS instance, j.code, j.name, %s AS period, req.opening, req.calculated, req.closing
+            FROM (
+                SELECT journal_id, account_id, SUM(col1) AS opening, SUM(col2) AS calculated, SUM(col3) AS closing
+                FROM (
+                    (
+                        SELECT aml.journal_id AS journal_id, aml.account_id AS account_id, ROUND(SUM(amount_currency), 2) as col1, 0.00 as col2, 0.00 as col3
+                        FROM account_move_line AS aml 
+                        LEFT JOIN account_journal j 
+                            ON aml.journal_id = j.id 
+                        WHERE j.type IN ('cash', 'bank', 'cheque')
+                        AND aml.date < %s
+                        AND aml.account_id IN (j.default_debit_account_id, j.default_credit_account_id)
+                        GROUP BY aml.journal_id, aml.account_id
+                    )
+                UNION
+                    (
+                        SELECT aml.journal_id AS journal_id, aml.account_id AS account_id, 0.00 as col1, ROUND(SUM(amount_currency), 2) as col2, 0.00 as col3
+                        FROM account_move_line AS aml 
+                        LEFT JOIN account_journal j 
+                            ON aml.journal_id = j.id 
+                        WHERE j.type IN ('cash', 'bank', 'cheque')
+                        AND aml.period_id = %s
+                        AND aml.account_id IN (j.default_debit_account_id, j.default_credit_account_id)
+                        GROUP BY aml.journal_id, aml.account_id
+                    )
+                UNION
+                    (
+                        SELECT aml.journal_id AS journal_id, aml.account_id AS account_id, 0.00 as col1, 0.00 as col2, ROUND(SUM(amount_currency), 2) as col3
+                        FROM account_move_line AS aml 
+                        LEFT JOIN account_journal j 
+                            ON aml.journal_id = j.id 
+                        WHERE j.type IN ('cash', 'bank', 'cheque')
+                        AND aml.date <= %s
+                        AND aml.account_id IN (j.default_debit_account_id, j.default_credit_account_id)
+                        GROUP BY aml.journal_id, aml.account_id
+                    )
+                ) AS ssreq
+                GROUP BY journal_id, account_id
+                ORDER BY journal_id, account_id
+            ) AS req, account_journal j, msf_instance i
+            WHERE req.journal_id = j.id
+            AND j.instance_id = i.id
+            AND j.instance_id IN %s;
+            """
+        instance_ids = self.instance_ids
+        period_id = self.period_id
+        period = self.pool.get('account.period').browse(self.cr, self.uid, period_id, context=self.context,
+                                                        fields_to_fetch=['date_start', 'date_stop'])
+        last_day_of_period = period.date_stop
+        first_day_of_period = period.date_start
+        tm = strptime(first_day_of_period, '%Y-%m-%d')
+        year_num = tm.tm_year
+        year = str(year_num)
+        month = '%02d' % (tm.tm_mon)
+        period_yyyymm = "{0}{1}".format(year, month)
+        params = (tuple([period_yyyymm]), first_day_of_period, period.id, last_day_of_period, tuple(instance_ids))
+        self.cr.execute(liquidity_sql, params)
+        return self.cr.dictfetchall()
 
-    _defaults = {
-        'fiscalyear_id': lambda self, cr, uid, c: self.pool.get('account.fiscalyear').find(cr, uid, strftime('%Y-%m-%d'), context=c),
-    }
+    def set_context(self, objects, data, ids, report_type=None):
+        self.instance_id = data['form'].get('instance_id', False)
+        self.instance_ids = data['form'].get('instance_ids', False)
+        self.period_id = data['form'].get('period_id', False)
+        self.fiscalyear_id = data['form'].get('fiscalyear_id', False)
+        self.context = data.get('context', {})
+        return super(account_liquidity_balance, self).set_context(objects, data, ids, report_type)
 
-    def print_liquidity_balance_report(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        wizard = self.browse(cr, uid, ids[0], context=context)
-        data = {}
-        data['form'] = {}
-        data['form'].update({'instance_id': wizard.instance_id and wizard.instance_id.id or False})
-        data['form'].update({'period_id': wizard.period_id and wizard.period_id.id or False})
-        data['form'].update({'fiscalyear_id': wizard.fiscalyear_id and wizard.fiscalyear_id.id or False})
-        data['context'] = context
-        return {
-            'type': 'ir.actions.report.xml',
-            'report_name': 'liquidity.balance',
-            'datas': data,
-            'context': context,
-        }
-
-liquidity_balance_wizard()
+SpreadsheetReport('report.account.liquidity.balance', 'account.bank.statement',
+        'addons/account/report/account_liquidity_balance.mako', parser=account_liquidity_balance)
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
