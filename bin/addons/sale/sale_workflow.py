7a0c9715dbc85260b4c0b4e64ec47dfa19065448
Mallory MARCOT <mma@tempo-consulting.fr>
2017-06-02 17:36:17 +0200

    sale workflow to a new class

---
+++ bin/addons/sale/sale_workflow.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+from osv import osv
+
+class sale_order(osv.osv):
+    _name = "sale.order"
+    _inherit = "sale.order"
+
+    def wkf_validated(self, cr, uid, ids, context=None):
+        return True
+
+    def wkf_split(self, cr, uid, ids, context=None):
+        return True
+
+    def wkf_split_done(self, cr, uid, ids, context=None):
+        return True
