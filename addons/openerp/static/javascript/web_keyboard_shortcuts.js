17f41c82fb296bbfa533aa7a09513643f30ec56b
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-06-14 11:15:59 +0200

    US-2869 [FIX] add SHIFT to the combination of keys to do to activate the shortcut. Now it accept shortcut only with combination of CTRL + SHIFT + SOMETHING [FIX] if the button have an onclick attribute, call it instead of trigger click (as it is not the same behaviour). [ADD] a method fake_click to factorize the code

--- addons/openerp/static/javascript/web_keyboard_shortcuts.js
+++ addons/openerp/static/javascript/web_keyboard_shortcuts.js
@@ -29,7 +29,7 @@
 $.ctrl = function(key, callback, args) {
     $(document).keydown(function(e) {
         if(!args) args=[]; // IE barks when args is null
-        if((e.keyCode == key.charCodeAt(0) || e.keyCode == key) && e.ctrlKey) {
+        if((e.keyCode == key.charCodeAt(0) || e.keyCode == key) && e.ctrlKey && e.shiftKey) {
             e.preventDefault();  // override the browser shortcut keys
             callback.apply(this, args);
             return false;
@@ -37,134 +37,118 @@ $.ctrl = function(key, callback, args) {
     });
 };
 
+
+fake_click = function(button) {
+    if($(button).parents('div:hidden').length == 0){
+        if (button.hasAttribute('onclick')) {
+            button.onclick();
+        }
+        else {
+            $(button).trigger('click');
+        }
+    }
+};
+
 //Save the current object
 $.ctrl('S', function() {
     var saved = false;
     $('.oe_form_button_save_line').each(function() {
         if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
+            fake_click(this);
             saved = true;  // if a line is under edition, save only this line, not the whole object
         }
     });
     $('.oe_form_button_save_close').each(function() {
         if(!saved && $(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
+            fake_click(this);
             saved = true;
         }
     });
     $('.oe_form_button_save').each(function() {
-        if(!saved && $(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Save & Edit the current object
 $.ctrl('E', function() {
     $('.oe_form_button_save_edit').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
     $('.oe_form_button_edit').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Delete the current object
 $.ctrl('46', function() {
     $('.oe_form_button_delete').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Cancel the modifiactions
 $.ctrl('Z', function() {
     $('.oe_form_button_cancel').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //New object
 $.ctrl('C', function() {
     $('.oe_form_button_create').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
     $('.oe_form_button_save_create').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Duplicate the current object
 $.ctrl('D', function() {
     $('.oe_form_button_duplicate').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Search (enter)
 $.ctrl('13', function() {
     $('.oe_form_button_search').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Clear search
 $.ctrl('R', function() {
     $('.oe_form_button_clear').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //First object (arrow up)
 $.ctrl('38', function(event) {
     $('.oe_button_pager[action="first"]').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Previous object (arrow right)
 $.ctrl('37', function() {
     $('.oe_button_pager[action="previous"]').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Next object (arrow left)
 $.ctrl('39', function() {
     $('.oe_button_pager[action="next"]').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
 //Last object (arrow down)
 $.ctrl('40', function() {
     $('.oe_button_pager[action="last"]').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
 
@@ -172,8 +156,6 @@ $.ctrl('40', function() {
 //Close (escape)
 $.ctrl('27', function() {
     $('.oe_form_button_close').each(function() {
-        if($(this).parents('div:hidden').length == 0){
-            $(this).trigger('click');
-        }
+        fake_click(this);
     });
 });
