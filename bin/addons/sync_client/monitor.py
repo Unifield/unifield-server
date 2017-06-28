8e755f07d188306d219e402e94162147293ef80e
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-06-28 09:47:09 +0200

    US-2916 [ADD] instance state column into Version Instances Monitor tree view

--- bin/addons/sync_client/monitor.py
+++ bin/addons/sync_client/monitor.py
@@ -24,7 +24,6 @@ import pooler
 import tools
 from tools.translate import _
 
-
 class MonitorLogger(object):
     def __init__(self, cr, uid, defaults={}, context=None):
         db, pool = pooler.get_db_and_pool(cr.dbname)
@@ -316,6 +315,10 @@ class sync_version_instance_monitor(osv.osv):
         'backup_path': fields.char('Backup Location', size=128),
         'backup_date': fields.datetime("Backup Date", readonly=True,
             required=True),
+        'instance_state': fields.related('instance_id', 'state', type='selection',
+                                         selection=[('draft', 'Draft'),
+                                                    ('active', 'Active'),
+                                                    ('inactive', 'Inactive')], string='Instance State', readonly=True,)
     }
 
     _defaults = {
