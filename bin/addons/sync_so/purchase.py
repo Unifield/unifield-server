45c81a9df30eb702befa02bccea3b3b278956317
Mallory MARCOT <mma@tempo-consulting.fr>
2017-07-27 12:16:42 +0200

    update PO line as done when receiving the sync msg

--- bin/addons/sync_so/purchase.py
+++ bin/addons/sync_so/purchase.py
@@ -91,6 +91,8 @@ class purchase_order_line_sync(osv.osv):
             wf_service.trg_validate(uid, 'purchase.order.line', pol_to_update[0], 'sourced_v', cr)
         elif sol_dict['state'] == 'confirmed':
             wf_service.trg_validate(uid, 'purchase.order.line', pol_to_update[0], 'confirmed', cr)
+        elif sol_dict['state'] == 'done':
+            wf_service.trg_validate(uid, 'purchase.order.line', pol_to_update[0], 'done', cr)
 
         # log me:
         po_name = self.pool.get('purchase.order').read(cr, uid, po_ids[0], ['name'], context=context)['name'] or ''
