91c0f6bf5d3726bdc7b5b3f358a28fb5dfbff334
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-08-04 17:26:29 +0200

    US-3067 [FIX] call write with groups_id on all user that have some groups_id defined. The write of the res.users is updating the related groups by removing all to high level groups automatically. Just call write is enough.

--- bin/addons/msf_instance/msf_instance_installer.py
+++ bin/addons/msf_instance/msf_instance_installer.py
@@ -39,6 +39,24 @@ class msf_instance_setup(osv.osv_memory):
         current_obj = self.read(cr, uid, ids)[0]
         if not current_obj['first_run']:
             self.pool.get('res.company').write(cr, uid, [self.pool.get('res.users').browse(cr, uid, uid).company_id.id], {'instance_id': current_obj['instance_id']})
+
+            # clean instance groups if not HQ:
+            level = self.pool.get('msf.instance').read(cr, uid, current_obj['instance_id'], ['level'])['level']
+            if level != 'section':
+                # do a write on all users to update their group by removing the to hight level ones
+                user_obj = self.pool.get('res.users')
+                user_ids = user_obj.search(cr, uid, [], context=context)
+                if 1 in user_ids:
+                    # admin can stay member of higher groups
+                    user_ids.remove(1)
+                read_result = user_obj.read(cr, uid, user_ids, ['groups_id'],
+                                            context=context)
+                for user in read_result:
+                    if user['groups_id']:
+                        user_obj.write(cr, uid, user['id'],
+                                       {'groups_id': [(6, 0, user['groups_id'])]},
+                                       context=context)
+
             return self.action_next(cr, uid, ids, context=context)
         if current_obj['instance_id']:
             instance_code = self.pool.get('msf.instance').read(cr, uid, current_obj['instance_id'], ['code'])['code']
