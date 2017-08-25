d364a59ef50a2371e489f10e9a52d350d8bc97df
jf <jfb@tempo-consulting.fr>
2017-08-25 16:29:25 +0200

    US-3326 [FIX] Check to close a period ignore cheque registers

--- bin/addons/account_period_closing_level/account_period.py
+++ bin/addons/account_period_closing_level/account_period.py
@@ -204,7 +204,7 @@ class account_period(osv.osv):
                 if not period.special:
                     prev_period_id = register_tools.previous_period_id(self, cr, uid, period.id, context=context, raise_error=False)
                     if prev_period_id:
-                        all_prev_reg_ids = reg_obj.search(cr, uid, [('period_id', '=', prev_period_id)], order='NO_ORDER', context=context)
+                        all_prev_reg_ids = reg_obj.search(cr, uid, [('period_id', '=', prev_period_id), ('journal_id.type', 'in', ['bank', 'cash'])], order='NO_ORDER', context=context)
                         # get the registers of the previous period which are NOT linked to a register of the period to close
                         orphan_prev_reg_ids = [reg_id for reg_id in all_prev_reg_ids if reg_id not in linked_prev_reg_ids]
                         reg_ko = []
