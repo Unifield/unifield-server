a0bb8a6ddf8f37af6dc8375a1248af5046ba7baf
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-09-15 10:26:03 +0200

    US-2619 [ADD] display_all_errors parameter to be able in some situations to display all errors not only the first one. By default it is False to keep the previous behaviour.

--- bin/osv/orm.py
+++ bin/osv/orm.py
@@ -820,7 +820,9 @@ class orm_template(object):
 
         return processed, rejected, headers
 
-    def import_data(self, cr, uid, fields, datas, mode='init', current_module='', noupdate=False, context=None, filename=None):
+    def import_data(self, cr, uid, fields, datas, mode='init',
+            current_module='', noupdate=False, context=None, filename=None,
+            display_all_errors=False):
         """
         Import given data in given module
 
@@ -1039,35 +1041,56 @@ class orm_template(object):
 
         from osv import except_osv
         position = 0
+        error_list = []
         while position<len(datas):
             res = {}
 
             (res, position, warning, res_id, xml_id) = \
                 process_liness(self, datas, [], current_module, self._name, fields_def, position=position)
             if len(warning):
-                cr.rollback()
-                return (-1, res, 'Line ' + str(position) +' : ' + '!\n'.join(warning), '')
+                if display_all_errors:
+                    error_list.append(_('Line %s: %s') % (str(position),
+                        '\n'.join(warning)))
+                    continue
+                else:
+                    cr.rollback()
+                    return (-1, res, 'Line ' + str(position) +' : ' + '!\n'.join(warning), '')
 
             try:
                 ir_model_data_obj._update(cr, uid, self._name,
                                           current_module, res, mode=mode, xml_id=xml_id,
                                           noupdate=noupdate, res_id=res_id, context=context)
             except except_osv, e:
-                cr.rollback()
-                return (-1, res, 'Line ' + str(position) +' : ' + tools.ustr(e.value), '')
+                if display_all_errors:
+                    error_list.append(_('Line %s: %s') % (str(position),
+                        tools.ustr(e.value)))
+                    continue
+                else:
+                    cr.rollback()
+                    return (-1, res, 'Line ' + str(position) +' : ' + tools.ustr(e.value), '')
             except Exception, e:
                 #US-88: If this from an import account analytic, and there is sql error, AND not sync context, then just clear the cache
                 if 'account.analytic.account' in self._name and not context.get('sync_update_execution', False):
                     cache.clean_caches_for_db(cr.dbname)
-                return (-1, res, 'Line ' + str(position) +' : ' + tools.ustr(e) + "\n" + tools.ustr(traceback.format_exc()), '')
+                if display_all_errors:
+                    error_list.append(_('Line %s: %s') % (str(position),
+                        tools.ustr(e) + "\n" +
+                        tools.ustr(traceback.format_exc())))
+                    continue
+                else:
+                    return (-1, res, 'Line ' + str(position) +' : ' + tools.ustr(e) + "\n" + tools.ustr(traceback.format_exc()), '')
 
-            if config.get('import_partial', False) and filename and (not (position%100)):
+            if not error_list and config.get('import_partial', False) and filename and (not (position%100)):
                 data = pickle.load(file(config.get('import_partial')))
                 data[filename] = position
                 pickle.dump(data, file(config.get('import_partial'), 'wb'))
                 if context.get('defer_parent_store_computation'):
                     self._parent_store_compute(cr)
                 cr.commit()
+
+            if error_list:
+                cr.rollback()
+                return (-1, {}, '\n'.join(error_list), '')
 
         if context.get('defer_parent_store_computation'):
             self._parent_store_compute(cr)
