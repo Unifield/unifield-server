97c3aee8ca1605980cf6ef4b3b699303c698b868
jf <jfb@tempo-consulting.fr>
2017-09-22 14:18:33 +0200

    Testfield: fill stock

--- bin/addons/unifield_tests/testfield/init_data/set_rb_partial_tf.py
+++ bin/addons/unifield_tests/testfield/init_data/set_rb_partial_tf.py
@@ -47,11 +47,17 @@ if p_ids:
 loc_o = oerp.get('stock.location')
 loc_ids = loc_o.search([('name', '=', 'LOG')])
 prod_o = oerp.get('product.product')
-p_ids = prod_o.search([('default_code', '=', 'ADAPCABL1S-')])
+p1_ids = prod_o.search([('default_code', '=', 'ADAPCABL1S-')])
+p2_ids = prod_o.search([('default_code', '=', 'ADAPDCDRB--')])
+p3_ids = prod_o.search([('default_code', '=', 'ADAPMEMK1--')])
 inv_o = oerp.get('stock.inventory')
 inv_id = inv_o.create({
     'name': 'inv %s' % time.time(),
-    'inventory_line_id': [(0, 0, {'location_id': loc_ids[0], 'product_id': p_ids[0], 'product_uom': 1, 'product_qty': 1000, 'reason_type_id': 12})],
+    'inventory_line_id': [
+        (0, 0, {'location_id': loc_ids[0], 'product_id': p1_ids[0], 'product_uom': 1, 'product_qty': 1000, 'reason_type_id': 12}),
+        (0, 0, {'location_id': loc_ids[0], 'product_id': p2_ids[0], 'product_uom': 1, 'product_qty': 1000, 'reason_type_id': 12}),
+        (0, 0, {'location_id': loc_ids[0], 'product_id': p3_ids[0], 'product_uom': 1, 'product_qty': 1000, 'reason_type_id': 12}),
+    ],
 })
 inv_o.action_confirm([inv_id])
 inv_o.action_done([inv_id])
