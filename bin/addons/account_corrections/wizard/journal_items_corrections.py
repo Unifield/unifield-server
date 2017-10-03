8b1f03697244bc839431319df1aa8c18bc63ed69
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2017-10-03 15:53:28 +0200

    US-1255 [IMP] JI marked as corrected manually: use variable already created to handle the JI id

--- bin/addons/account_corrections/wizard/journal_items_corrections.py
+++ bin/addons/account_corrections/wizard/journal_items_corrections.py
@@ -377,9 +377,8 @@ class journal_items_corrections(osv.osv_memory):
         if aal_obj.search_exist(cr, uid, aji_reallocated_domain, context=context):
             raise osv.except_osv(_('Error'), _('One AJI related to this entry has already been corrected.'))
         # set the JI as corrected
-        manual_corr_vals = {'corrected': True,
-                            'have_an_historic': True}  # is_corrigible will be seen as "False"
-        aml_obj.write(cr, uid, wizard.move_line_id.id, manual_corr_vals, context=context)
+        manual_corr_vals = {'corrected': True, 'have_an_historic': True}  # is_corrigible will be seen as "False"
+        aml_obj.write(cr, uid, ji_id, manual_corr_vals, context=context)
         # set the AJIs as corrected (get the aji_ids AFTER aml correction to get the new AJI ids generated)
         aji_ids = aal_obj.search(cr, uid, [('move_id', '=', ji_id)], order='NO_ORDER', context=context)
         aal_obj.write(cr, uid, aji_ids, {'is_reallocated': True}, context=context)
