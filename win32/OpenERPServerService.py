9e70c1de316a2adc6c35ae20284f14d6116990d6
Fabien MORIN <fabien.morin@tempo-consulting.fr>
2017-10-09 11:39:52 +0200

    US-3534-3533 [IMP] have a try with return instead of exit

--- win32/OpenERPServerService.py
+++ win32/OpenERPServerService.py
@@ -91,7 +91,7 @@ class OpenERPServerService(win32serviceutil.ServiceFramework):
             exit_status = self.terpprocess.wait()
             if exit_status == EXIT_UPDATE_REQUIRE_RESTART:
                 servicemanager.LogInfoMsg("OpenERP has been updated, restarting...")
-                sys.exit(0)  # after that, the Windows Service Manager would execute the Recovery operation for the process, which is to restart it
+                return  # after that, the Windows Service Manager would execute the Recovery operation for the process, which is to restart it
             if exit_status == 0:
                 break  # normal exit
             sys.exit(exit_status)
