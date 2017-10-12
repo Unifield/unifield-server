4aaa5b4bf871c5cd4371fcf886b9470a7b854062
Mallory MARCOT <mma@tempo-consulting.fr>
2017-10-12 17:15:46 +0200

    US-3577 [FIX] IR line not updated when PO line is closed

--- bin/addons/purchase/purchase_workflow.py
+++ bin/addons/purchase/purchase_workflow.py
@@ -512,9 +512,9 @@ class purchase_order_line(osv.osv):
 
         for pol in self.browse(cr, uid, ids, context=context):
             # no PICK/OUT needed in this cases; close SO line:
-            internal_ir = pol.linked_sol_id and pol.linked_sol_id.procurement_request and pol.linked_sol_id.order_id.location_requestor_id.usage == 'internal' or False # PO line from Internal IR
+            internal_ir = pol.linked_sol_id and pol.linked_sol_id.order_id.procurement_request and pol.linked_sol_id.order_id.location_requestor_id.usage == 'internal' or False # PO line from Internal IR
             dpo = pol.order_id.order_type == 'direct' or False # direct PO
-            ir_non_stockable = pol.linked_sol_id and pol.linked_sol_id.procurement_request and pol.linked_sol_id.product_id.type in ('consu', 'service', 'service_recep') or False
+            ir_non_stockable = pol.linked_sol_id and pol.linked_sol_id.order_id.procurement_request and pol.linked_sol_id.product_id.type in ('consu', 'service', 'service_recep') or False
 
             if internal_ir or dpo or ir_non_stockable:
                 wf_service.trg_validate(uid, 'sale.order.line', pol.linked_sol_id.id, 'done', cr)
