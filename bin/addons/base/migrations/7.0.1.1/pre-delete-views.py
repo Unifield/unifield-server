1398896cfac1eba950a3edd1220536e24496d61d
jf <jfb@tempo-consulting.fr>
2017-12-11 10:34:43 +0100

    [SLL MIG] Change sdref on sale_override models

--- bin/addons/base/migrations/7.0.1.1/pre-delete-views.py
+++ bin/addons/base/migrations/7.0.1.1/pre-delete-views.py
@@ -6,6 +6,7 @@ def migrate(cr, version):
     cr.execute("delete from ir_ui_view where id in (select res_id from ir_model_data where module='sale_override' and model='ir.ui.view')")
     cr.execute("delete from ir_ui_menu where id in (select res_id from ir_model_data where model='ir.ui.menu' and module='sale_override')")
     cr.execute("update ir_module_module set state='to upgrade' where name='msf_button_access_rights'")
+    cr.execute("update ir_model_data set name=regexp_replace(name,'^sale_override_(.*)$', 'sale_\\1') where model='ir.model' and module='sd'")
 
     # WKF
     cr.execute("delete from wkf_workitem where act_id in (select id from  wkf_activity  where wkf_id in (select id from wkf  where osv in ('purchase.order', 'sale.order', 'procurement.order')))")
