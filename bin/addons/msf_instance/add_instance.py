a11d4754bf5e9dd2f9736fc099d4cf7229f65ac9
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2018-01-17 15:30:44 +0100

    US-3849 [FIX] Concurrency changes on register: remove values to ignore instead of replacing them with the old ones (=> avoid crashing if the Closing Balance LINES are modified at the same time by 2 users)

--- bin/addons/msf_instance/add_instance.py
+++ bin/addons/msf_instance/add_instance.py
@@ -429,19 +429,18 @@ class account_bank_statement(osv.osv):
             journal = self.pool.get('account.journal').read(cr, uid, vals['journal_id'], ['instance_id'], context=context)
             vals['instance_id'] = journal.get('instance_id')[0]
         res = True
-        fields_list = ['closing_balance_frozen', 'balance_end_real', 'balance_start', 'ending_details_ids']
-        for reg in self.browse(cr, uid, ids, fields_to_fetch=fields_list, context=context):
+        for reg in self.browse(cr, uid, ids, fields_to_fetch=['closing_balance_frozen'], context=context):
             # if the End-of-the-Month Balance has already been confirmed for a register, ignore changes on fields that
             # should be read-only in that case (cover the use case of concurrent changes by 2 users)
             newvals = vals.copy()
             if reg.closing_balance_frozen:
-                # update the values for each register
+                # remove the values for each register with a confirmed balance
                 if 'balance_end_real' in newvals:
-                    newvals['balance_end_real'] = reg.balance_end_real
+                    del newvals['balance_end_real']
                 if 'balance_start' in newvals:
-                    newvals['balance_start'] = reg.balance_start
+                    del newvals['balance_start']
                 if 'ending_details_ids' in newvals:
-                    newvals['ending_details_ids'] = reg.ending_details_ids
+                    del newvals['ending_details_ids']
             res = res and super(account_bank_statement, self).write(cr, uid, [reg.id], newvals, context=context)
         return res
 
