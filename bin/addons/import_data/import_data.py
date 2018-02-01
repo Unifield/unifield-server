95d868b5cdbaa377cdb0bbf62b66cfac89ad9609
Dorian <dk@tempo-consulting.fr>
2018-02-01 10:26:31 +0100

    US-3994 [IMP] Product import checks on ir_translation for nomenclature name first before looking at product_nomenclature

--- bin/addons/import_data/import_data.py
+++ bin/addons/import_data/import_data.py
@@ -243,42 +243,27 @@ class import_data(osv.osv_memory):
                                    'product.asset.type': {'name': {}},
                                    'product.international.status': {'name': {}},
                                    }
-            # Product nomenclature
-            cr.execute('SELECT name, id FROM product_nomenclature;')
+            # Product nomenclature and complete name
+            temp_nomen_by_id = {}
+            cr.execute('''
+                SELECT n.id, coalesce(t.value,n.name), n.parent_id 
+                FROM product_nomenclature n 
+                LEFT JOIN ir_translation t ON t.lang='en_MF' AND t.name='product.nomenclature,name' AND t.res_id=n.id 
+                ORDER BY n.level;
+            ''')
             for nv in cr.dictfetchall():
-                self._cache[dbname]['product.nomenclature']['name'].update({nv['name']: nv['id']})
+                self._cache[dbname]['product.nomenclature']['name'].update({nv['coalesce']: nv['id']})
+                if nv['parent_id'] and temp_nomen_by_id.get(nv['parent_id'], False):
+                    temp_full_name = temp_nomen_by_id[nv['parent_id']] + ' | ' + nv['coalesce']
+                    temp_nomen_by_id.update({nv['id']: temp_full_name})
+                    self._cache[dbname]['product.nomenclature']['complete_name'].update({temp_full_name: nv['id']})
+                else:
+                    temp_nomen_by_id.update({nv['id']: nv['coalesce']})
+                    self._cache[dbname]['product.nomenclature']['complete_name'].update({nv['coalesce']: nv['id']})
             # Product category
             cr.execute('SELECT id, family_id FROM product_category;')
             for pc in cr.dictfetchall():
                 self.pool.get('product.nomenclature')._cache[dbname].update({pc['family_id']: pc['id']})
-            # Product nomenclature complete name
-            cr.execute('''SELECT id, name FROM
-(
-(SELECT
-    n0.id, n0.name AS name
-FROM product_nomenclature n0
-WHERE n0.level = 0)
-UNION
-(SELECT n1.id, n0.name ||' | '|| n1.name AS name
-FROM product_nomenclature n1
-  LEFT JOIN product_nomenclature n0 ON n1.parent_id = n0.id
-WHERE n1.level = 1)
-UNION
-(SELECT n2.id, n0.name ||' | '|| n1.name ||' | '|| n2.name AS name
-FROM product_nomenclature n1
-  LEFT JOIN product_nomenclature n0 ON n1.parent_id = n0.id
-  LEFT JOIN product_nomenclature n2 ON n2.parent_id = n1.id
-WHERE n2.level = 2)
-UNION
-(SELECT n3.id, n0.name ||' | '|| n1.name ||' | '|| n2.name ||' | '|| n3.name AS name
-FROM product_nomenclature n1
-  LEFT JOIN product_nomenclature n0 ON n1.parent_id = n0.id
-  LEFT JOIN product_nomenclature n2 ON n2.parent_id = n1.id
-  LEFT JOIN product_nomenclature n3 ON n3.parent_id = n2.id
-WHERE n3.level = 3)
-) AS cn''')
-            for cnv in cr.dictfetchall():
-                self._cache[dbname]['product.nomenclature']['complete_name'].update({cnv['name']: cnv['id']})
             # Product UoM
             cr.execute('SELECT name, id FROM product_uom;')
             for uv in cr.dictfetchall():
