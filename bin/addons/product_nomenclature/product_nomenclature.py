c49c38c76c33c374a3cb126aa480a7bdf98e7b4b
Dorian <dk@tempo-consulting.fr>
2018-02-20 10:14:46 +0100

    US-1981 [FIX] Changed nomenclature order and added sequence to MED, LOG, LIB, SRV

--- bin/addons/product_nomenclature/product_nomenclature.py
+++ bin/addons/product_nomenclature/product_nomenclature.py
@@ -491,7 +491,7 @@ class product_nomenclature(osv.osv):
         'active': True,
     }
 
-    _order = "msfid, sequence, id"
+    _order = "sequence, level, msfid, id"
 
     _sql_constraints = [('check_msfid_unique', 'unique (msfid)', 'MSFID must be unique !')]
 
