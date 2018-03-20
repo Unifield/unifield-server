8077cbe38664a2014109ecea80341c8d244600c8
jf <jfb@tempo-consulting.fr>
2018-03-20 09:19:41 +0100

    Release UF8.0

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'UF8.0rc3'
+version = 'UF8.0'
 major_version = '7.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
