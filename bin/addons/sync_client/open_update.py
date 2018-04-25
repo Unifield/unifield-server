ff16851b68f0e4eb6ab7ef0b333f8bc9a38b84cd
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2018-04-25 10:59:12 +0200

    US-1630 [IMP] Link to Updates Sent & Received: add them in partners/registers/HQ entries views + use the inheritance to avoid repetition

--- bin/addons/sync_client/open_update.py
+++ bin/addons/sync_client/open_update.py
@@ -23,66 +23,73 @@
 from osv import osv
 
 
-class account_move_line(osv.osv):
-    _name = 'account.move.line'
-    _inherit = 'account.move.line'
+class open_update(osv.osv):
+    _name = 'open.update'
 
     def open_update_received(self, cr, uid, ids, context=None):
         """
-        Opens the Updates Received View with the SD ref of the selected JI already filled in
+        Opens the Updates Received View with the SD ref of the selected entry already filled in
         """
         update_received_obj = self.pool.get('sync.client.update_received')
-        return update_received_obj.open_update_list(cr, uid, ids, model='account.move.line', context=context)
+        return update_received_obj.open_update_list(cr, uid, ids, model=self._name, context=context)
 
     def open_update_sent(self, cr, uid, ids, context=None):
         """
-        Opens the Updates Sent View with the SD ref of the selected JI already filled in
+        Opens the Updates Sent View with the SD ref of the selected entry already filled in
         """
         update_sent_obj = self.pool.get('sync.client.update_to_send')
-        return update_sent_obj.open_update_list(cr, uid, ids, model='account.move.line', context=context)
+        return update_sent_obj.open_update_list(cr, uid, ids, model=self._name, context=context)
+
+open_update()
+
+
+class account_move_line(osv.osv):
+    _name = 'account.move.line'
+    _inherit = ['account.move.line', 'open.update']
 
 account_move_line()
 
 
 class account_analytic_line(osv.osv):
     _name = 'account.analytic.line'
-    _inherit = 'account.analytic.line'
-
-    def open_update_received(self, cr, uid, ids, context=None):
-        """
-        Opens the Updates Received View with the SD ref of the selected AJI already filled in
-        """
-        update_received_obj = self.pool.get('sync.client.update_received')
-        return update_received_obj.open_update_list(cr, uid, ids, model='account.analytic.line', context=context)
-
-    def open_update_sent(self, cr, uid, ids, context=None):
-        """
-        Opens the Updates Sent View with the SD ref of the selected AJI already filled in
-        """
-        update_sent_obj = self.pool.get('sync.client.update_to_send')
-        return update_sent_obj.open_update_list(cr, uid, ids, model='account.analytic.line', context=context)
+    _inherit = ['account.analytic.line', 'open.update']
 
 account_analytic_line()
 
 
 class account_move(osv.osv):
     _name = 'account.move'
-    _inherit = 'account.move'
-
-    def open_update_received(self, cr, uid, ids, context=None):
-        """
-        Opens the Updates Received View with the SD ref of the selected JE already filled in
-        """
-        update_received_obj = self.pool.get('sync.client.update_received')
-        return update_received_obj.open_update_list(cr, uid, ids, model='account.move', context=context)
-
-    def open_update_sent(self, cr, uid, ids, context=None):
-        """
-        Opens the Updates Sent View with the SD ref of the selected JE already filled in
-        """
-        update_sent_obj = self.pool.get('sync.client.update_to_send')
-        return update_sent_obj.open_update_list(cr, uid, ids, model='account.move', context=context)
+    _inherit = ['account.move', 'open.update']
 
 account_move()
+
+
+class res_partner(osv.osv):
+    _name = 'res.partner'
+    _inherit = ['res.partner', 'open.update']
+
+res_partner()
+
+
+class account_bank_statement(osv.osv):
+    _name = 'account.bank.statement'
+    _inherit = ['account.bank.statement', 'open.update']
+
+account_bank_statement()
+
+
+class account_bank_statement_line(osv.osv):
+    _name = 'account.bank.statement.line'
+    _inherit = ['account.bank.statement.line', 'open.update']
+
+account_bank_statement_line()
+
+
+class hq_entries(osv.osv):
+    _name = 'hq.entries'
+    _inherit = ['hq.entries', 'open.update']
+
+hq_entries()
+
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
