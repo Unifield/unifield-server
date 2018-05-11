181b548aa1abb6afb8a9ce2c1d7b80e24f3a7f92
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2018-05-11 16:43:55 +0200

    US-4426 [IMP] Related entries feature added in Analytic lines: make the results consistent depending on the view (AJI/Free1/Free2)

--- bin/addons/account/account_analytic_line.py
+++ bin/addons/account/account_analytic_line.py
@@ -144,53 +144,64 @@ class account_analytic_line(osv.osv):
 
     def get_aal_related_entries(self, cr, uid, ids, context=None):
         """
-        Returns an AJI view with all the AJIs related to the selected one, i.e.:
-        1) those having the same Entry Sequence as the selected one (including the selected AJI itself)
-        2) those having the same reference as one of the AJIs found in 1)
-        3) those having an Entry Sequence matching exactly with the reference of one of the AJIs found in 1)
-        4) those whose reference contains EXACTLY the Entry Sequence of the selected AJI
-        5) those having the same Entry Sequence as one of the AJIs found in 2), 3), or 4)
+        Returns a view with all the Analytic Lines related to the selected one, i.e.:
+        1) those having the same Entry Sequence as the selected one (including the selected line itself)
+        2) those having the same reference as one of the lines found in 1)
+        3) those having an Entry Sequence matching exactly with the reference of one of the lines found in 1)
+        4) those whose reference contains EXACTLY the Entry Sequence of the selected line
+        5) those having the same Entry Sequence as one of the lines found in 2), 3), or 4)
         """
         if context is None:
             context = {}
         if isinstance(ids, (int, long)):
             ids = [ids]
-        active_ids = context.get('active_ids', [])  # to detect if the user has selected several AJIs
+        active_ids = context.get('active_ids', [])  # to detect if the user has selected several lines
         if len(ids) != 1 or len(active_ids) > 1:
             raise osv.except_osv(_('Error'),
-                                 _('The related entries feature can only be used with one Analytic Journal Item.'))
+                                 _('The related entries feature can only be used with one Analytic Line.'))
         ir_model_obj = self.pool.get('ir.model.data')
+        analytic_acc_obj = self.pool.get('account.analytic.account')
         related_aals = set()
-        selected_aal = self.browse(cr, uid, ids[0], fields_to_fetch=['entry_sequence'], context=context)
-        selected_entry_seq = selected_aal.entry_sequence or ''
 
-        # get the ids of all the related AJIs
-        # AJIs having the same Entry Sequence
-        same_seq_aji_ids = self.search(cr, uid, [('entry_sequence', '=', selected_entry_seq)], order='NO_ORDER', context=context)
-        related_aals.update(same_seq_aji_ids)
+        selected_aal = self.browse(cr, uid, ids[0], fields_to_fetch=['entry_sequence', 'account_id'], context=context)
+        selected_entry_seq = selected_aal.entry_sequence or ''
+        selected_category = selected_aal.account_id.category or ''
+
+        # get the ids of all the related lines
+        analytic_account_ids = analytic_acc_obj.search(cr, uid, [('category', '=', selected_category)],
+                                                       order='NO_ORDER', context=context)
+        # lines having the same Entry Sequence
+        same_seq_domain = [('entry_sequence', '=', selected_entry_seq),
+                           ('account_id', 'in', analytic_account_ids)]
+        same_seq_line_ids = self.search(cr, uid, same_seq_domain, order='NO_ORDER', context=context)
+        related_aals.update(same_seq_line_ids)
 
         # check on ref
         set_of_refs = set()
-        for aal in self.browse(cr, uid, same_seq_aji_ids, fields_to_fetch=['ref'], context=context):
+        for aal in self.browse(cr, uid, same_seq_line_ids, fields_to_fetch=['ref'], context=context):
             aal.ref and set_of_refs.add(aal.ref)
 
-        domain_related_ajis = ['|', '|',
-                               '&', ('ref', 'in', list(set_of_refs)), ('ref', '!=', ''),
-                               ('entry_sequence', 'in', list(set_of_refs)),
-                               ('ref', '=', selected_entry_seq)]
-        related_aji_ids = self.search(cr, uid, domain_related_ajis, order='NO_ORDER', context=context)
-        related_aals.update(related_aji_ids)
+        domain_related_lines = ['&', '|', '|',
+                                '&', ('ref', 'in', list(set_of_refs)), ('ref', '!=', ''),
+                                ('entry_sequence', 'in', list(set_of_refs)),
+                                ('ref', '=', selected_entry_seq),
+                                ('account_id', 'in', analytic_account_ids)]
+        related_line_ids = self.search(cr, uid, domain_related_lines, order='NO_ORDER', context=context)
+        related_aals.update(related_line_ids)
 
-        # check on Entry Seq. (compared with those of the related AJIs found)
-        aji_seqs = set(aal.entry_sequence for aal in self.browse(cr, uid, related_aji_ids,
+        # check on Entry Seq. (compared with those of the related lines found)
+        aal_seqs = set(aal.entry_sequence for aal in self.browse(cr, uid, related_line_ids,
                                                                  fields_to_fetch=['entry_sequence'], context=context))
-        same_seq_related_aji_ids = self.search(cr, uid, [('entry_sequence', 'in', list(aji_seqs))], order='NO_ORDER', context=context)
-        related_aals.update(same_seq_related_aji_ids)
+        same_seq_related_line_domain = [('entry_sequence', 'in', list(aal_seqs)),
+                                        ('account_id', 'in', analytic_account_ids)]
+        same_seq_related_line_ids = self.search(cr, uid, same_seq_related_line_domain, order='NO_ORDER', context=context)
+        related_aals.update(same_seq_related_line_ids)
 
         domain = [('id', 'in', list(related_aals))]
+        # same views whatever the category displayed (FP or Free1/2)
         view_id = ir_model_obj.get_object_reference(cr, uid, 'account', 'view_account_analytic_line_tree')
-        view_id = view_id and view_id[1] or False
         search_view_id = ir_model_obj.get_object_reference(cr, uid, 'account', 'view_account_analytic_line_filter')
+        view_id = view_id and view_id[1] or False
         search_view_id = search_view_id and search_view_id[1] or False
         return {
             'name': _('Related entries: Entry Sequence %s') % selected_entry_seq,
