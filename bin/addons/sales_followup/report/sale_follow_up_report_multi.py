5790863769d81fe330731bb794fb5a31b93c3305
Dorian <dk@tempo-consulting.fr>
2018-05-17 10:40:51 +0200

    US-2974-4274 [FIX] Fixed FO followup backorder qty for for unshipped moves

--- bin/addons/sales_followup/report/sale_follow_up_report_multi.py
+++ bin/addons/sales_followup/report/sale_follow_up_report_multi.py
@@ -192,8 +192,7 @@ class sale_follow_up_multi_report_parser(report_sxw.rml_parse):
                             'shipment': shipment,
                             'is_delivered': is_delivered,
                             'delivered_qty': not only_bo and (is_shipment_done or is_delivered) and move.product_qty or 0.00,
-                            'delivered_uom': (not only_bo and (is_shipment_done or is_delivered) and move.product_uom.name)
-                                             or (not only_bo and ppl_not_shipped and move.product_uom.name) or '-',
+                            'delivered_uom': not only_bo and (is_shipment_done or is_delivered) and move.product_uom.name or '-',
                             'backordered_qty': not is_shipment_done and not is_delivered and line.order_id.state != 'cancel' and move.product_qty or 0.00,
                             'rts': not only_bo and move.picking_id.shipment_id and move.picking_id.shipment_id.shipment_expected_date[0:10] or '',
                             'eta': not only_bo and eta and eta.strftime('%Y-%m-%d'),
@@ -227,6 +226,11 @@ class sale_follow_up_multi_report_parser(report_sxw.rml_parse):
                                 if not grouped or (grouped and line.line_number == key[3]):
                                     rline.update({
                                         'delivered_qty': rline['delivered_qty'] + data['delivered_qty'],
+                                    })
+                            if rline['packing'] == key[0] and rline['shipment'] == key[1] and move.product_uom.name == key[2]\
+                                    and (ppl_not_shipped or not is_shipment_done):
+                                if not grouped or (grouped and line.line_number == key[3]):
+                                    rline.update({
                                         'backordered_qty': rline['backordered_qty'] + data['backordered_qty'],
                                     })
                     else:
