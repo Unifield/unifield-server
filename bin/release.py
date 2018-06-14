0f53971162bf9a8eb526bc3fa790a16871be1cd3
jf <jfb@tempo-consulting.fr>
2018-06-14 09:18:00 +0200

    Release UF9.0

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'UF9.0rc5'
+version = 'UF9.0'
 major_version = '7.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
