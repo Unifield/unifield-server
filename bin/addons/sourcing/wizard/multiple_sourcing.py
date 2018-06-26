5fa991fb232c7fdc71a0b5559427e8af7083a0c3
Mallory MARCOT <mma@tempo-consulting.fr>
2018-06-26 15:14:58 +0200

    US-4708 [FIX] OST : a line on FO loan cannot be edited during OST

--- bin/addons/sourcing/wizard/multiple_sourcing.py
+++ bin/addons/sourcing/wizard/multiple_sourcing.py
@@ -282,6 +282,9 @@ class multiple_sourcing_wizard(osv.osv_memory):
                         raise osv.except_osv(_('Error'), _('You cannot choose Direct Purchase Order as method to source Internal Request lines.'))
                     lines_to_confirm.append(sol.id)
 
+                if wiz.type == 'make_to_order' and sol.order_id.order_type == 'loan':
+                    raise osv.except_osv(_('Error'), _('Line #%s of %s You cannot cannot source a loan on order') % (sol.line_number, sol.order_id.name))
+
         line_obj.confirmLine(cr, uid, lines_to_confirm, context=context)
 
         return {'type': 'ir.actions.act_window_close'}
