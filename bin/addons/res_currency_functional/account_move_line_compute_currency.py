08f3da53d6ae5c239b40cc27b843dd1206a7aafe
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2018-09-06 18:07:02 +0200

    US-5011 [IMP] Reconciliation: by default use the 'Default Debit/Credit Account for Reconciliation' of the account reconciled as G/L account of the FXA lines generated

--- bin/addons/res_currency_functional/account_move_line_compute_currency.py
+++ bin/addons/res_currency_functional/account_move_line_compute_currency.py
@@ -125,6 +125,21 @@ class account_move_line_compute_currency(osv.osv):
         addendum_line_credit_account_id = journal.default_credit_account_id.id
         addendum_line_debit_account_default_destination_id = journal.default_debit_account_id.default_destination_id.id
         addendum_line_credit_account_default_destination_id = journal.default_credit_account_id.default_destination_id.id
+        acc_to_reconcile = lines and self.browse(cr, uid, lines[0], fields_to_fetch=['account_id'], context=context).account_id
+        # Since US-5011 use the Default Accounts for Reconciliation of the account to reconcile if it has any
+        # (the checks on the FXA journal are kept)
+        # Debit account
+        if acc_to_reconcile and acc_to_reconcile.reconciliation_debit_account_id:
+            rec_debit_acc = acc_to_reconcile.reconciliation_debit_account_id
+            addendum_line_debit_account_id = rec_debit_acc.id
+            if rec_debit_acc.default_destination_id:
+                addendum_line_debit_account_default_destination_id = rec_debit_acc.default_destination_id.id
+        # Credit account
+        if acc_to_reconcile and acc_to_reconcile.reconciliation_credit_account_id:
+            rec_credit_acc = acc_to_reconcile.reconciliation_credit_account_id
+            addendum_line_credit_account_id = rec_credit_acc.id
+            if rec_credit_acc.default_destination_id:
+                addendum_line_credit_account_default_destination_id = rec_credit_acc.default_destination_id.id
         # Create analytic distribution if this account is an analytic-a-holic account
         distrib_id = False
         different_currency = False
