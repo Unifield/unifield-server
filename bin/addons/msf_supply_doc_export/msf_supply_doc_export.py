aaefd1136695965241039affe8be2b925dace1d1
jf <jfb@tempo-consulting.fr>
2018-10-31 10:25:47 +0100

    FO / IR: sidebar

--- bin/addons/msf_supply_doc_export/msf_supply_doc_export.py
+++ bin/addons/msf_supply_doc_export/msf_supply_doc_export.py
@@ -790,7 +790,7 @@ class ir_values(osv.osv):
     _inherit = 'ir.values'
 
 
-    def get(self, cr, uid, key, key2, models, meta=False, context=None, res_id_req=False, without_user=True, key2_req=True, view_id=False):
+    def get_(self, cr, uid, key, key2, models, meta=False, context=None, res_id_req=False, without_user=True, key2_req=True, view_id=False):
         if context is None:
             context = {}
         values = super(ir_values, self).get(cr, uid, key, key2, models, meta, context, res_id_req, without_user, key2_req, view_id=view_id)
