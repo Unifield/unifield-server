0f86f608021e5abc1866453f0fd46571b094f03e
Dorian <dk@tempo-consulting.fr>
2019-02-04 15:49:12 +0100

    US-5357 [FIX] Product Nomenclature: Status field is readonly and only visible for root level

--- bin/addons/product_nomenclature/product_nomenclature.py
+++ bin/addons/product_nomenclature/product_nomenclature.py
@@ -481,7 +481,7 @@ class product_nomenclature(osv.osv):
 
         'nomen_type_s': fields.function(_get_fake, method=True, type='selection', selection=[('mandatory', 'Mandatory'), ('optional', 'Optional')], string='Nomenclature type', fnct_search=_search_nomen_type_s),
         'msfid': fields.char('MSFID', size=128, select=True),
-        'status': fields.selection([('valid', 'Valid'), ('archived', 'Archived')], 'Status', required=True),
+        'status': fields.selection([('valid', 'Valid'), ('archived', 'Archived')], 'Status', readonly=True),
     }
 
     _defaults = {
