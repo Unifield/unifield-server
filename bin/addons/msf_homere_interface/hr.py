662b89eaf1fb9a164e6416d8feacc38818b89134
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2019-04-08 16:33:50 +0200

    US-5771 [IMP] Employee create/write: add check on Dest/CC compatibility

--- bin/addons/msf_homere_interface/hr.py
+++ bin/addons/msf_homere_interface/hr.py
@@ -208,6 +208,22 @@ class hr_employee(osv.osv):
         (_check_unicity, "Another employee has the same Identification No.", ['identification_id']),
     ]
 
+    def _check_employe_dest_cc_compatibility(self, cr, uid, employee_id, context=None):
+        """
+        Raises an error in case the employee Destination and Cost Center are not compatible
+        """
+        if context is None:
+            context = {}
+        ad_obj = self.pool.get('analytic.distribution')
+        employee_fields = ['destination_id', 'cost_center_id', 'name_resource']
+        employee = self.browse(cr, uid, employee_id, fields_to_fetch=employee_fields, context=context)
+        emp_dest = employee.destination_id
+        emp_cc = employee.cost_center_id
+        if emp_dest and emp_cc:
+            if not ad_obj.check_dest_cc_compatibility(cr, uid, emp_dest.id, emp_cc.id, context=context):
+                raise osv.except_osv(_('Error'), _('Employee %s: the Cost Center %s is not compatible with the Destination %s.') %
+                                     (employee.name_resource, emp_cc.code or '', emp_dest.code or ''))
+
     def create(self, cr, uid, vals, context=None):
         """
         Block creation for local staff if no 'from' in context
@@ -228,7 +244,9 @@ class hr_employee(osv.osv):
             # Raise an error if employee is created manually
             if (not context.get('from', False) or context.get('from') not in ['yaml', 'import']) and not context.get('sync_update_execution', False) and not allow_edition:
                 raise osv.except_osv(_('Error'), _('You are not allowed to create a local staff! Please use Import to create local staff.'))
-        return super(hr_employee, self).create(cr, uid, vals, context)
+        employee_id = super(hr_employee, self).create(cr, uid, vals, context)
+        self._check_employe_dest_cc_compatibility(cr, uid, employee_id, context=context)
+        return employee_id
 
     def write(self, cr, uid, ids, vals, context=None):
         """
@@ -277,6 +295,7 @@ class hr_employee(osv.osv):
             employee_id = super(hr_employee, self).write(cr, uid, emp.id, new_vals, context)
             if employee_id:
                 res.append(employee_id)
+            self._check_employe_dest_cc_compatibility(cr, uid, emp.id, context=context)
         return res
 
     def unlink(self, cr, uid, ids, context=None):
