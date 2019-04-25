17e9a79f241c0da34fddbe69efe9a50195a37216
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2019-04-25 16:58:12 +0200

    US-5655 [IMP] Import of International CV: DEST/CC/FP in the import file must contain the CODE of the analytic account and not its name

--- bin/addons/analytic_distribution/wizard/import_commitment_wizard.py
+++ bin/addons/analytic_distribution/wizard/import_commitment_wizard.py
@@ -225,7 +225,7 @@ class import_commitment_wizard(osv.osv_memory):
                         raise osv.except_osv(_('Error'), raise_msg_prefix + _('No account code found!'))
                     # Destination
                     if destination:
-                        dest_id = self.pool.get('account.analytic.account').search(cr, uid, ['|', ('code', '=', destination), ('name', '=', destination), ('type', '!=', 'view')])
+                        dest_id = self.pool.get('account.analytic.account').search(cr, uid, [('code', '=', destination), ('type', '!=', 'view')])
                         if dest_id:
                             vals.update({'destination_id': dest_id[0]})
                         else:
@@ -242,7 +242,7 @@ class import_commitment_wizard(osv.osv_memory):
                             raise osv.except_osv(_('Error'), raise_msg_prefix + msg)
                     # Cost Center
                     if cost_center:
-                        cc_id = self.pool.get('account.analytic.account').search(cr, uid, ['|', ('code', '=', cost_center), ('name', '=', cost_center), ('type', '!=', 'view')])
+                        cc_id = self.pool.get('account.analytic.account').search(cr, uid, [('code', '=', cost_center), ('type', '!=', 'view')])
                         if cc_id:
                             vals.update({'cost_center_id': cc_id[0]})
                         else:
@@ -251,7 +251,7 @@ class import_commitment_wizard(osv.osv_memory):
                         raise osv.except_osv(_('Error'), raise_msg_prefix + _('No cost center code found!'))
                     # Funding Pool
                     if funding_pool:
-                        fp_id = self.pool.get('account.analytic.account').search(cr, uid, ['|', ('code', '=', funding_pool), ('name', '=', funding_pool), ('type', '!=', 'view')])
+                        fp_id = self.pool.get('account.analytic.account').search(cr, uid, [('code', '=', funding_pool), ('type', '!=', 'view')])
                         if fp_id:
                             vals.update({'account_id': fp_id[0]})
                         else:
