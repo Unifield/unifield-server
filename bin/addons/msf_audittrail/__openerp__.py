95fc8c1c9a2256b3cde26a1c9c26583c728eccd0
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2019-05-03 11:57:36 +0200

    US-5582 [IMP] account.account: add Track Changes

--- bin/addons/msf_audittrail/__openerp__.py
+++ bin/addons/msf_audittrail/__openerp__.py
@@ -50,6 +50,7 @@
         'data/audittrail_res_users.yml',
         'data/audittrail_hr_employee.yml',
         'data/audittrail_res_partner.yml',
+        'data/audittrail_account_account.yml',
         'audittrail_report.xml',
         'audittrail_invoice_data.yml',
     ],
