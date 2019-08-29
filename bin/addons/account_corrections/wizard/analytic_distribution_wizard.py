c31fab39cb1b911b4c6e86c7865e660973a77b44
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2019-08-29 17:21:38 +0200

    US-6100 [FIX] AD corrections: the adjustment wasn't made on the COR line in case the distrib. line was deleted and recreated (as to_reverse is empty in that case) [FIX TYPO]

--- bin/addons/account_corrections/wizard/analytic_distribution_wizard.py
+++ bin/addons/account_corrections/wizard/analytic_distribution_wizard.py
@@ -482,8 +482,8 @@ class analytic_distribution_wizard(osv.osv_memory):
         if greater_amount['gap_amount']:
             aal_obj = self.pool.get('account.analytic.line')
 
-            has_generated_corr = new_line_ids and (to_reverse or any_reverse)  # check if COR lines have been generated
-            if not greater_amount['aji_id'] and greater_amount['wl'] and not has_generated_corr:
+            has_generated_cor = new_line_ids and (to_reverse or any_reverse)  # check if COR lines have been generated
+            if not greater_amount['aji_id'] and greater_amount['wl'] and not has_generated_cor:
                 # untouched greater amount, get analytic line id:
                 # (not in to_create, to_delete, to_override, to_reverse)
                 aji_ids = aal_obj.search(cr, uid, [
@@ -496,7 +496,7 @@ class analytic_distribution_wizard(osv.osv_memory):
 
             # US-6100 in case of a corr. the adjustment should be made on the biggest COR amount
             # instead of the biggest amount of all AJIs (cf. don't modify the entry being corrected)
-            if has_generated_corr:
+            if has_generated_cor:
                 greater_amount.update({
                     'aji_id': False,
                     'amount': 0.,
