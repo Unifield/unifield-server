2f173c876fccfe04ac198dcb34f1aafe4a07e2a4
Dorian <dk@tempo-consulting.fr>
2019-09-13 15:09:14 +0200

    US-6420 [FIX] Reception report: fixed move search domain

--- bin/addons/stock/wizard/stock_reception_wizard.py
+++ bin/addons/stock/wizard/stock_reception_wizard.py
@@ -77,6 +77,7 @@ class stock_reception_wizard(osv.osv_memory):
             move_domain = [
                 ('type', '=', 'in'),
                 ('purchase_line_id', '!=', False),
+                ('picking_id.state', '=', 'done'),
                 ('state', '=', 'done'),
             ]
             if wizard.start_date:
