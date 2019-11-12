4d3837a0fa92bfcd4316c4a26962dd4d3d078fcc
Dorian <dk@tempo-consulting.fr>
2019-11-12 15:02:49 +0100

    US-5449 [FIX] IN automatic import: allow the import of 'Available Shipped' INs

--- bin/addons/msf_doc_import/stock_picking.py
+++ bin/addons/msf_doc_import/stock_picking.py
@@ -88,7 +88,7 @@ class stock_picking(osv.osv):
         in_id = self.pool.get('stock.picking').search(cr, uid, [
             ('purchase_id', '=', po_id[0]),
             ('type', '=', 'in'),
-            ('state', '=', 'assigned'),
+            ('state', 'in', ['assigned', 'shipped']),
         ], context=context)
         if not in_id:
             raise osv.except_osv(_('Error'), _('No available IN found for the given PO %s') % po_name)
