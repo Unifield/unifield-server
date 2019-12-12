5e4d202ef286518b2464a1c5ec3102f494b6fc94
jf <jfb@tempo-consulting.fr>
2019-12-12 15:14:01 +0100

    threshold_value / procurement_report: remove old menu

--- bin/addons/base/migrations/8.0.1.6/pre-remove_account_voucher.py
+++ bin/addons/base/migrations/8.0.1.6/pre-remove_account_voucher.py
@@ -5,4 +5,5 @@ def migrate(cr, version):
 
     cr.execute("update ir_module_module set state='uninstalled' where name in ('threshold_value', 'procurement_report')")
     cr.execute("update wkf_transition set sequence = id where act_to in (select id from wkf_activity where wkf_id = (select id from wkf where name='procurement.order.basic'))")
+    cr.execute("delete from ir_ui_menu where id in (select res_id from ir_model_data where module in ('threshold_value', 'procurement_report') and model='ir.ui.menu')")
     return True
