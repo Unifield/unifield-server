1699204d7ed2410891b6d09cc0d91ffbb72eb752
jf <jfb@tempo-consulting.fr>
2019-12-18 16:32:22 +0100

    Data migration

--- bin/addons/base/migrations/8.0.1.6/pre-replenishment.py
+++ bin/addons/base/migrations/8.0.1.6/pre-replenishment.py
@@ -4,6 +4,7 @@ def migrate(cr, version):
         return
 
     cr.execute("update ir_module_module set state='uninstalled' where name in ('threshold_value', 'procurement_report')")
-    cr.execute("update wkf_transition set sequence = id where act_to in (select id from wkf_activity where wkf_id = (select id from wkf where name='procurement.order.basic'))")
+    if cr.column_exists('wkf_transition', 'sequence'):
+        cr.execute("update wkf_transition set sequence = id where act_to in (select id from wkf_activity where wkf_id = (select id from wkf where name='procurement.order.basic'))")
     cr.execute("delete from ir_ui_menu where id in (select res_id from ir_model_data where module in ('threshold_value', 'procurement_report') and model='ir.ui.menu')")
     return True
