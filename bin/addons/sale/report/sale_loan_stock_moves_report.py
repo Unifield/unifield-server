9ff7e0c2f9be2e0a5d338ed49423d661509f40cd
Dorian <dk@tempo-consulting.fr>
2020-04-20 14:15:39 +0200

    US-7420 [FIX] Loan report: Fixed keys during report creation

--- bin/addons/sale/report/sale_loan_stock_moves_report.py
+++ bin/addons/sale/report/sale_loan_stock_moves_report.py
@@ -148,8 +148,8 @@ class sale_loan_stock_moves_report_parser(report_sxw.rml_parse):
                 setattr(move, 'balance', 0)
 
                 key = (
-                    so_found and so_found.id or 'NF%s' % po_found.id,
-                    po_found and po_found.id or 'NF%s' % so_found.id,
+                    so_found and so_found.id or 'NF%s' % (po_found and po_found.id or '',),
+                    po_found and po_found.id or 'NF%s' % (so_found and so_found.id or '',),
                     move.product_id.id
                 )
                 if key not in move_by_fo_po_prod:
