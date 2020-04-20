a9145f7d3e3591d0d48dc502e63a48517d019eec
jf <jfb@tempo-consulting.fr>
2020-04-20 15:27:41 +0200

    RAC: Manage error on file import

--- bin/addons/consumption_calculation/consumption_calculation.py
+++ bin/addons/consumption_calculation/consumption_calculation.py
@@ -409,7 +409,6 @@ class real_average_consumption(osv.osv):
 
             if rac.created_ok:
                 return {'type': 'ir.actions.close_window'}
-            line_obj._unique_lot_poduct(cr, uid, [x.id for x in rac.line_ids])
             line_obj._check_qty(cr, uid, [x.id for x in rac.line_ids])
 
         partner_id = self.pool.get('res.users').browse(cr, uid, uid, context=context).company_id.partner_id.id
@@ -821,6 +820,8 @@ class real_average_consumption_line(osv.osv):
             #recursion: can't use write
             cr.execute('UPDATE '+self._table+' SET product_qty=%s, batch_mandatory=%s, date_mandatory=%s, asset_mandatory=%s, prodlot_id=%s, expiry_date=%s, asset_id=%s  where id=%s', (product_qty, batch_mandatory, date_mandatory, asset_mandatory, prodlot_id, expiry_date, asset_id, obj.id))  # not_a_user_entry
 
+        self._unique_lot_poduct(cr, uid, ids)
+
         return True
 
     def _get_product(self, cr, uid, ids, context=None):
@@ -878,16 +879,16 @@ class real_average_consumption_line(osv.osv):
     def _unique_lot_poduct(self, cr, uid, ids, context=None):
         if not ids:
             return True
-
         cr.execute('''
             select product.default_code, bn.name, bn.id, rac.id, rac.name
-                from real_average_consumption_line line
-                inner join real_average_consumption rac on rac.id = line.rac_id
+                from real_average_consumption rac
+                inner join real_average_consumption_line line on line.rac_id = rac.id
                 inner join product_product product on product.id = line.product_id
                 inner join stock_production_lot bn on bn.id = line.prodlot_id
+                left join real_average_consumption_line write_line on write_line.rac_id = rac.id
             where
                 rac.state = 'draft' and
-                line.id in %s
+                write_line.id in %s
             group by
                 product.default_code, bn.name, bn.id, rac.id, rac.name
             having count(*) > 1
@@ -903,9 +904,6 @@ class real_average_consumption_line(osv.osv):
 
         return True
 
-    _constraints = [
-        (_unique_lot_poduct, "The couple product, batch number has to be unique", ['product_id'])
-    ]
 
     def create(self, cr, uid, vals=None, context=None):
         '''
