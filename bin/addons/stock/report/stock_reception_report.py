154aa429220bce2976b3594b4363b146edbafe87
Dorian <dk@tempo-consulting.fr>
2020-05-06 17:38:12 +0200

    US-6420 [FIX] Reception report: UC FO->IN-to-stock display the IN's destination as final destination. UC IN->INT with IN move done but not the INT's, display nothing on final destination

--- bin/addons/stock/report/stock_reception_report.py
+++ bin/addons/stock/report/stock_reception_report.py
@@ -24,6 +24,7 @@ class stock_reception_report(report_sxw.rml_parse):
     def get_moves(self, moves_ids):
         move_obj = self.pool.get('stock.move')
         curr_obj = self.pool.get('res.currency')
+        model_obj = self.pool.get('ir.model.data')
         res = []
 
         for move in move_obj.browse(self.cr, self.uid, moves_ids, context=self.localcontext):
@@ -40,25 +41,31 @@ class stock_reception_report(report_sxw.rml_parse):
                                                          move.company_id.currency_id.id, move.price_unit,
                                                          round=False, context=self.localcontext), 2)
 
-            # Get the linked INT's move destination location
+            # Get the linked INT's move destination location and look if it's done
             int_move_dest_loc = False
+            linked_int_move_done = False
             int_domain = [('type', '=', 'internal'), ('line_number', '=', move.line_number),
                           ('picking_id.previous_chained_pick_id', '=', pick.id)]
             linked_int_move_ids = move_obj.search(self.cr, self.uid, int_domain, context=self.localcontext)
             if linked_int_move_ids:
-                int_move_dest_loc = move_obj.browse(self.cr, self.uid, linked_int_move_ids[0],
-                                                    fields_to_fetch=['location_dest_id'],
-                                                    context=self.localcontext).location_dest_id.name
-            if sol:
-                if sol.procurement_request:
-                    if int_move_dest_loc and sol.order_id.location_requestor_id.usage == 'internal':
-                        final_dest_loc = int_move_dest_loc
-                    else:
-                        final_dest_loc = sol.order_id.location_requestor_id.name
+                linked_int_move = move_obj.browse(self.cr, self.uid, linked_int_move_ids[0],
+                                                  fields_to_fetch=['location_dest_id', 'state'],
+                                                  context=self.localcontext)
+                int_move_dest_loc = linked_int_move.location_dest_id.name
+                linked_int_move_done = linked_int_move.state == 'done'
+            if sol and sol.procurement_request:
+                if int_move_dest_loc and sol.order_id.location_requestor_id.usage == 'internal':
+                    final_dest_loc = int_move_dest_loc
                 else:
-                    final_dest_loc = sol.order_id.partner_id.name
+                    final_dest_loc = sol.order_id.location_requestor_id.name
+            elif sol and move.location_dest_id.id == model_obj.get_object_reference(self.cr, self.uid, 'msf_cross_docking',
+                                                                                    'stock_location_cross_docking')[1]:
+                final_dest_loc = sol.order_id.partner_id.name
             elif int_move_dest_loc:
-                final_dest_loc = int_move_dest_loc
+                if linked_int_move_done:
+                    final_dest_loc = int_move_dest_loc
+                else:  # Do not show the destination if the linked INT move is not done
+                    final_dest_loc = ''
             elif move.location_dest_id:
                 final_dest_loc = move.location_dest_id.name
             else:
