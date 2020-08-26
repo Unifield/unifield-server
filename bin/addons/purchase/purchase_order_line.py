6901db8ae643769a2b6f003e7030ad2a73913b63
jf <jfb@tempo-consulting.fr>
2020-08-26 14:53:50 +0200

    Prod forbidden: cancel IN for FO after partial process, cancel parially porcessed Pick

--- bin/addons/purchase/purchase_order_line.py
+++ bin/addons/purchase/purchase_order_line.py
@@ -1505,7 +1505,6 @@ class purchase_order_line(osv.osv):
             new_po_line = split_obj.split_line(cr, uid, [split_id], context=context)
             context.pop('return_new_line_id')
             context.pop('keepLineNumber')
-            context.pop('cancel_only')
 
             # udpate linked FO lines if has:
             self.write(cr, uid, [new_po_line], {'origin': pol.origin}, context=context) # otherwise not able to link with FO
@@ -1517,6 +1516,7 @@ class purchase_order_line(osv.osv):
             # cancel the new split PO line:
             signal = 'cancel_r' if resource else 'cancel'
             wf_service.trg_validate(uid, 'purchase.order.line', new_po_line, signal, cr)
+            context.pop('cancel_only')
 
         return new_po_line
 
