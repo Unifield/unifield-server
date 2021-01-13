4911c8bfce188b2470ba179349dd3e6554be7d03
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2021-01-13 19:20:28 +0100

    US-8012 [IMP] FO Follow-up Finance: create new field linking SI lines and PO lines, to be used in case of merged SI lines

--- bin/addons/finance/invoice.py
+++ bin/addons/finance/invoice.py
@@ -84,7 +84,9 @@ class account_invoice_line(osv.osv):
         'order_line_id': fields.many2one('purchase.order.line', string="Purchase Order Line", readonly=True,
                                          help="Purchase Order Line from which this invoice line has been generated (when coming from a purchase order)."),
         'order_line_ids': fields.one2many('purchase.order.line', 'merged_invoice_line_id', string='Purchase Order Lines',
-                                          help="Used in case of merged invoice lines")
+                                          help="Used in case of merged invoice lines"),
+        'purchase_order_line_ids': fields.many2many('purchase.order.line', 'inv_line_po_line_rel', 'inv_line_id', 'po_line_id',
+                                                    string="Purchase Order Lines", help="Used in case of merged invoice lines"),
     }
 
 account_invoice_line()
