4234d17b62d1988e0975d4afcd6c1dd42cb7ad88
Dorian <dk@tempo-consulting.fr>
2021-02-04 09:49:10 +0100

    US-8030 [PROGRESS] Manage Expired Stock: Added a secondary wizard to also add BNs to expire in X weeks

--- bin/addons/stock/wizard/manage_expired_stock.py
+++ bin/addons/stock/wizard/manage_expired_stock.py
@@ -23,7 +23,11 @@ from osv import osv, fields
 from tools.translate import _
 
 from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+
+IN_NEXT_X_WEEKS = tuple([("%s" % i, _("%s weeks") % str(i)) for i in range(1, 13)])
 
 
 class manage_expired_stock(osv.osv):
@@ -44,11 +48,19 @@ class manage_expired_stock(osv.osv):
             required=True,
             domain=['&', '|', ('quarantine_location', '=', True), ('destruction_location', '=', True), ('usage', '!=', 'view')],
         ),
+        'bned_date_sel': fields.selection(
+            [('exp_prod', 'Select expired products'), ('soon_exp_prod', 'Select expired products and soon to be expired')],
+            'Batch/Expiry Date Selection'
+        ),
+        'in_next_x_weeks': fields.selection(
+            IN_NEXT_X_WEEKS,
+            'In the next',
+        ),
     }
 
-    def create_int_expired_stock(self, cr, uid, ids, context=False):
+    def get_exp_data(self, cr, uid, ids, context=False, expiring=False):
         '''
-        Creates an Internal Picking to send all expired products to destruction or quarantine
+        Fetch the data for the expired prodlot and sometimes soon to expire as well
         '''
         if context is None:
             context = {}
@@ -57,14 +69,18 @@ class manage_expired_stock(osv.osv):
         lot_obj = self.pool.get('stock.production.lot')
 
         wizard = self.browse(cr, uid, ids[0], context=context)
-
         if wizard.location_id.id == wizard.dest_loc_id.id:
             raise osv.except_osv(
                 _('Error'),
                 _('You cannot have the sourcing location and destination location with the same location.')
             )
 
-        lot_ids = lot_obj.search(cr, uid, [('life_date', '<=', date.today())])
+        today = date.today()
+        if expiring:
+            expiring_date = (today + relativedelta(weeks=-int(wizard.in_next_x_weeks))).strftime('%Y-%m-%d 00:00:00')
+            lot_ids = lot_obj.search(cr, uid, [('life_date', '<=', today), ('life_date', '>=', expiring_date)])
+        else:
+            lot_ids = lot_obj.search(cr, uid, [('life_date', '<=', today)])
 
         dest_loc_id = wizard.dest_loc_id.id
         exp_rt_id = ir_obj.get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_expiry')[1]
@@ -90,16 +106,49 @@ class manage_expired_stock(osv.osv):
         if not moves_lines:
             raise osv.except_osv(_('Warning !'), _('There is no expired quantity for this location.'))
 
+        return moves_lines
+
+    def open_view_expired_expiring_stock(self, cr, uid, ids, context=False):
+        '''
+        Open a wizard that displays all products with BN/ED according to set criteria
+        '''
+        if context is None:
+            context = {}
+        if not ids:
+            return True
+
+        v_exp_obj = self.pool.get('view.expired.expiring.stock')
+        data = {'mng_exp_id': ids[0], 'v_mng_exp_lines_ids': self.get_exp_data(cr, uid, ids, context=context, expiring=True)}
+        wiz_id = v_exp_obj.create(cr, uid, data, context=context)
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'view.expired.expiring.stock',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'res_id': wiz_id,
+            'context': context
+        }
+
+    def create_int_expired_stock(self, cr, uid, ids, context=False):
+        '''
+        Creates an Internal Picking to send all expired products to destruction or quarantine
+        '''
+        if context is None:
+            context = {}
+        if not ids:
+            return True
+
         int_values = {
             'name': self.pool.get('ir.sequence').get(cr, uid, 'stock.picking.internal'),
             'type': 'internal',
             'subtype': 'standard',
             'invoice_state': 'none',
-            'reason_type_id': exp_rt_id,
+            'reason_type_id': self.pool.get('ir.model.data').get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_expiry')[1],
             'from_manage_expired': True,
-            'move_lines': moves_lines,
+            'move_lines': self.get_exp_data(cr, uid, ids, context=context, expiring=False),
         }
-
         new_int_id = self.pool.get('stock.picking').create(cr, uid, int_values, context=context)
 
         return {
@@ -115,3 +164,46 @@ class manage_expired_stock(osv.osv):
 
 
 manage_expired_stock()
+
+
+class view_expired_expiring_stock(osv.osv):
+    _name = 'view.expired.expiring.stock'
+    _description = 'View Expired/Expiring Products'
+
+    _columns = {
+        'mng_exp_id': fields.many2one('manage.expired.stock', string='Manage Expired Stock Id', readonly=True),
+        'v_mng_exp_lines_ids': fields.one2many('view.expired.expiring.stock.lines', 'v_mng_exp_id', 'View Expired/Expiring products', readonly=True),
+    }
+
+    def create_int_expired_expiring_stock(self, cr, uid, ids, context=None):
+        return {'type': 'ir.actions.act_window_close'}
+
+    def del_sel_prod(self, cr, uid, ids, context=None):
+        return {'type': 'ir.actions.act_window_close'}
+
+    def cancel(self, cr, uid, ids, context=None):
+        return {'type': 'ir.actions.act_window_close'}
+
+
+view_expired_expiring_stock()
+
+
+class view_expired_expiring_stock_lines(osv.osv):
+    _name = 'view.expired.expiring.stock.lines'
+    _description = 'Expired/Expiring Products'
+
+    _columns = {
+        'v_mng_exp_id': fields.many2one('view.expired.expiring.stock', string='View Expired/Expiring Products', readonly=True),
+        'name': fields.char('Description', size=256, readonly=True),
+        'product_id': fields.many2one('product.product', string='Product', readonly=True),
+        'prodlot_id': fields.many2one('stock.production.lot', string='Batch Number', readonly=True),
+        'expired_date': fields.date(string='Expiry Date', readonly=True),
+        'product_qty': fields.float(digits=(16, 2), string='Quantity', related_uom='product_uom', readonly=True),
+        'product_uom': fields.many2one('product.uom', string='UoM', readonly=True),
+        'location_id': fields.many2one('stock.location', 'Source Location', readonly=True),
+        'location_dest_id': fields.many2one('stock.location', 'Destination Location', readonly=True),
+        'reason_type_id': fields.many2one('stock.reason.type', string='Reason type', readonly=True),
+    }
+
+
+view_expired_expiring_stock()
