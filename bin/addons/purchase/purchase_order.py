2aa8c2fad944e57f2b0e499ec698fd4941d67c63
Dorian <dk@tempo-consulting.fr>
2021-02-11 11:06:13 +0100

    US-8272 [FIX] PO partner change: before changing partner, use the currency of the catalogue if it exist to change the price from. If there is a comment product, convert the price using the currency from the line to the one of the new partner

--- bin/addons/purchase/purchase_order.py
+++ bin/addons/purchase/purchase_order.py
@@ -1082,26 +1082,43 @@ class purchase_order(osv.osv):
                 if order.state in ('draft', 'draft_p', 'validated') and vals['partner_id'] != order.partner_id.id:
                     for line in order.order_line:
                         if line.state in ('draft', 'validated_n', 'validated'):
-                            suppinf_ids = suppinf_obj.search(cr, uid, [('name', '=', partner.id), ('product_id', '=', line.product_id.id)], context=context)
-                            price_to_convert = line.product_id.standard_price
-                            to_curr_id = partner.property_product_pricelist_purchase.currency_id.id
-                            if vals.get('pricelist_id') and partner.partner_type == 'external':
-                                to_curr_id = self.pool.get('product.pricelist').browse(cr, uid, vals['pricelist_id'], fields_to_fetch=['currency_id'], context=context).currency_id.id
-                            if suppinf_ids:
-                                domain = [('uom_id', '=', line.product_uom.id), ('suppinfo_id', 'in', suppinf_ids),
-                                          '|', ('valid_from', '<=', order.date_order), ('valid_from', '=', False),
-                                          '|', ('valid_till', '>=', order.date_order), ('valid_till', '=', False)]
-                                domain_cur = [('currency_id', '=', to_curr_id)]
-                                domain_cur.extend(domain)
-
-                                part_inf_ids = partinf_obj.search(cr, uid, domain_cur, order='sequence asc, min_quantity asc, id desc', limit=1, context=context)
-                                if not part_inf_ids:
+                            if line.product_id:
+                                # Currency to compute from, use the one from the linked catalogue if it exists
+                                from_suppinf_ids = suppinf_obj.search(cr, uid, [('name', '=', order.partner_id.id), ('product_id', '=', line.product_id.id)], context=context)
+                                from_curr_id = line.product_id.currency_id.id
+                                if from_suppinf_ids:
+                                    domain = [('uom_id', '=', line.product_uom.id), ('suppinfo_id', 'in', from_suppinf_ids),
+                                              '|', ('valid_from', '<=', order.date_order), ('valid_from', '=', False),
+                                              '|', ('valid_till', '>=', order.date_order), ('valid_till', '=', False)]
                                     part_inf_ids = partinf_obj.search(cr, uid, domain, order='sequence asc, min_quantity asc, id desc', limit=1, context=context)
-                                if part_inf_ids:
-                                    price_to_convert = partinf_obj.browse(cr, uid, part_inf_ids[0], context=context).price
+                                    if part_inf_ids:
+                                        from_curr_id = partinf_obj.browse(cr, uid, part_inf_ids[0], context=context).currency_id.id
+                                # Currency to compute to
+                                to_suppinf_ids = suppinf_obj.search(cr, uid, [('name', '=', partner.id), ('product_id', '=', line.product_id.id)], context=context)
+                                price_to_convert = line.product_id.standard_price
+                                to_curr_id = partner.property_product_pricelist_purchase.currency_id.id
+                                if vals.get('pricelist_id') and partner.partner_type == 'external':
+                                    to_curr_id = self.pool.get('product.pricelist').browse(cr, uid, vals['pricelist_id'], fields_to_fetch=['currency_id'], context=context).currency_id.id
+                                if to_suppinf_ids:
+                                    domain = [('uom_id', '=', line.product_uom.id), ('suppinfo_id', 'in', to_suppinf_ids),
+                                              '|', ('valid_from', '<=', order.date_order), ('valid_from', '=', False),
+                                              '|', ('valid_till', '>=', order.date_order), ('valid_till', '=', False)]
+                                    domain_cur = [('currency_id', '=', to_curr_id)]
+                                    domain_cur.extend(domain)
 
-                            new_price = cur_obj.compute(cr, uid, line.product_id.currency_id.id, to_curr_id, price_to_convert, round=True)
-                            pol_obj.write(cr, uid, line.id, {'price_unit': new_price}, context=context)
+                                    part_inf_ids = partinf_obj.search(cr, uid, domain_cur, order='sequence asc, min_quantity asc, id desc', limit=1, context=context)
+                                    if not part_inf_ids:
+                                        part_inf_ids = partinf_obj.search(cr, uid, domain, order='sequence asc, min_quantity asc, id desc', limit=1, context=context)
+                                    if part_inf_ids:
+                                        price_to_convert = partinf_obj.browse(cr, uid, part_inf_ids[0], context=context).price
+
+                                new_price = cur_obj.compute(cr, uid, from_curr_id, to_curr_id, price_to_convert, round=True)
+                                pol_obj.write(cr, uid, line.id, {'price_unit': new_price}, context=context)
+                            else:
+                                new_price = cur_obj.compute(cr, uid, line.product_id.currency_id.id,
+                                                            partner.property_product_pricelist_purchase.currency_id.id,
+                                                            line.price_unit, round=True)
+                                pol_obj.write(cr, uid, line.id, {'price_unit': new_price}, context=context)
                 break
 
         return super(purchase_order, self).write_web(cr, uid, ids, vals, context=context)
