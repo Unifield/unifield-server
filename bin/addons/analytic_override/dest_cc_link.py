838ed6a8e075cfe084814590fac48f02739d0d40
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2021-03-10 18:52:25 +0100

    US-7295 [IMP] Dest CC Link: don't prevent CC or Dest deletion if they have been selected into a "Dest / CC combination"

--- bin/addons/analytic_override/dest_cc_link.py
+++ bin/addons/analytic_override/dest_cc_link.py
@@ -29,9 +29,9 @@ class dest_cc_link(osv.osv):
 
     _columns = {
         'dest_id': fields.many2one('account.analytic.account', string="Destination", required=True,
-                                   domain="[('category', '=', 'DEST'), ('type', '!=', 'view')]"),
+                                   domain="[('category', '=', 'DEST'), ('type', '!=', 'view')]", ondelete='cascade'),
         'cc_id': fields.many2one('account.analytic.account', "Cost Center", required=True,
-                                 domain="[('category', '=', 'OC'), ('type', '!=', 'view')]"),
+                                 domain="[('category', '=', 'OC'), ('type', '!=', 'view')]", ondelete='cascade'),
         'cc_name': fields.related('cc_id', 'name', type="char", string="Cost Center Name", readonly=True, store=False),
         'inactive_from': fields.date('Inactivation Combination Dest / CC from', required=False),
     }
