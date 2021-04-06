96c51244509f1bc605733219107b82ba1775b43a
jf <jfb@tempo-consulting.fr>
2021-04-06 18:19:48 +0200

    US-8436 [FIX] FO sourced to several DPOs: do not generate NR msg at coordo to close the FO/DPO lines

--- bin/addons/sync_so/purchase.py
+++ bin/addons/sync_so/purchase.py
@@ -124,6 +124,7 @@ class purchase_order_line_sync(osv.osv):
         pol_values['sync_linked_sol'] = sol_dict['sync_local_id']
         pol_values['modification_comment'] = sol_dict.get('modification_comment', False)
         pol_values['from_dpo_line_id'] = sol_dict.get('dpo_line_id') and sol_dict.get('dpo_line_id', {}).get('.id', False) or False
+        pol_values['from_dpo_id'] = sol_dict.get('dpo_id') and sol_dict.get('dpo_id', {}).get('.id', False) or False
         if 'line_number' in pol_values:
             del(pol_values['line_number'])
 
