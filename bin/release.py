eec3efd3e75ffa8407817f741dbf1e2a18ec78ba
jf <jfb@tempo-consulting.fr>
2021-05-25 11:53:28 +0200

    Release UF21.0rc2dev

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'UF21.0rc1'
+version = 'UF21.0rc2dev'
 major_version = '8.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
