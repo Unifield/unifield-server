17782cb958a64b15fb7cf9ac81f5fe835290e435
jf <jfb@tempo-consulting.fr>
2021-08-11 15:26:09 +0200

    Python explicit dependencies

--- setup.py
+++ setup.py
@@ -259,6 +259,8 @@ setup(name=name,
           'paramiko==2.4.1',
           'pysftp==0.2.9',
           'openpyxl==2.6.4',
+          'jdcal==1.4.1',
+          'et-xmlfile==1.0.1',
       ],
       **py2exe_keywords
       )
