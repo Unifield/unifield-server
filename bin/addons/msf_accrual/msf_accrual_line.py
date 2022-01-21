751c334723f41400ec9a8fce332bb15eb9e0de35
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2022-01-21 18:20:54 +0100

    US-7563 [IMP] Accruals: make sure the journal selected by default (readonly) is ACC (always active)

--- bin/addons/msf_accrual/msf_accrual_line.py
+++ bin/addons/msf_accrual/msf_accrual_line.py
@@ -133,7 +133,8 @@ class msf_accrual_line(osv.osv):
     _defaults = {
         'third_party_type': 'res.partner',
         'journal_id': lambda self,cr,uid,c: self.pool.get('account.journal').search(cr, uid, [('type', '=', 'accrual'),
-                                                                                              ('is_current_instance', '=', True)])[0],
+                                                                                              ('is_current_instance', '=', True)],
+                                                                                    order='id')[0],
         'functional_currency_id': lambda self,cr,uid,c: self.pool.get('res.users').browse(cr, uid, uid, c).company_id.currency_id.id,
         'state': 'draft',
         'accrual_type' : 'reversing_accrual',
