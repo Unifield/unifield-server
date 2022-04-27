1f1a02304e0bfe6e2283d67f52535fbe5ac3be39
Julie Nuguet <julie.nuguet@tempo-consulting.fr>
2022-04-27 18:30:45 +0200

    US-5722 [IMP] Accrual Expense Lines: add ondelete='restrict' on Expense Account

--- bin/addons/msf_accrual/msf_accrual_line_expense.py
+++ bin/addons/msf_accrual/msf_accrual_line_expense.py
@@ -128,7 +128,7 @@ class msf_accrual_line_expense(osv.osv):
         'description': fields.char('Description', size=64, required=True),
         'reference': fields.char('Reference', size=64),
         'expense_account_id': fields.many2one('account.account', 'Expense Account', required=True,
-                                              domain=[('restricted_area', '=', 'accruals')]),
+                                              domain=[('restricted_area', '=', 'accruals')], ondelete='restrict'),
         'accrual_amount': fields.float('Accrual Amount', required=True),
         'functional_amount': fields.function(_get_functional_amount, type='float', method=True, readonly=True,
                                              string="Functional Amount",
