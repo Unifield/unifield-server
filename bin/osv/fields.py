dd98e372b4df35460637423f0fa607d554cff0e5
jf <jfb@tempo-consulting.fr>
2022-06-20 18:24:40 +0200

    [FIX] issue on fields.related store=True set as hidden in form view

--- bin/osv/fields.py
+++ bin/osv/fields.py
@@ -1023,6 +1023,13 @@ class related(function):
             write_fnct = self._fnct_write
 
         super(related, self).__init__(self._fnct_read, arg, write_fnct, fnct_inv_arg=arg, method=True, fnct_search=self._fnct_search, **args)
+
+        if not write_fnct and args.get('store'):
+            # example on account.move, partner_id fields.related store=True
+            # is set as hidden in form view
+            # without this the value s not cleaned by the web, and empty partner_id is written as '' instead of False
+            self.readonly = args.get('readonly')
+
         if self.store is True:
             # TODO: improve here to change self.store = {...} according to related objects
             pass
