b8af780f83e296ed0b0bcd860c965ed95948fa71
Dorian <dk@tempo-consulting.fr>
2022-07-08 11:12:04 +0200

    US-7434 [IMP] Pick/Out: Added Track Changes

--- bin/addons/msf_audittrail/audittrail.py
+++ bin/addons/msf_audittrail/audittrail.py
@@ -559,9 +559,13 @@ class audittrail_rule(osv.osv):
                 view_ids.append(obj_model.get_object_reference(cr, uid, 'purchase', 'purchase_order_form')[1])
 
             elif thisrule.object_id.model == 'stock.picking':
-                # TC only on IN
+                # TC only on IN, OUT and Picks
                 view_ids.append(obj_model.get_object_reference(cr, uid, 'stock', 'view_picking_in_tree')[1])
                 view_ids.append(obj_model.get_object_reference(cr, uid, 'stock', 'view_picking_in_form')[1])
+                view_ids.append(obj_model.get_object_reference(cr, uid, 'stock', 'view_picking_out_tree')[1])
+                view_ids.append(obj_model.get_object_reference(cr, uid, 'stock', 'view_picking_out_form')[1])
+                view_ids.append(obj_model.get_object_reference(cr, uid, 'msf_outgoing', 'view_picking_ticket_tree')[1])
+                view_ids.append(obj_model.get_object_reference(cr, uid, 'msf_outgoing', 'view_picking_ticket_form')[1])
 
             # search if the view does not already exists
             search_domain = [('name', '=', val['name']),
