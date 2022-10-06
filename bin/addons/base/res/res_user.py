46dcfc80215c12ee08eaba6474fa571cc9c06adc
jf <jfb@tempo-consulting.fr>
2022-10-06 17:44:12 +0200

    UR sync groups: list inactive menu items

--- bin/addons/base/res/res_user.py
+++ bin/addons/base/res/res_user.py
@@ -47,7 +47,7 @@ class groups(osv.osv):
         'model_access': fields.one2many('ir.model.access', 'group_id', 'Access Controls'),
         'rule_groups': fields.many2many('ir.rule', 'rule_group_rel',
                                         'group_id', 'rule_group_id', 'Rules', domain=[('global', '=', False)]),
-        'menu_access': fields.many2many('ir.ui.menu', 'ir_ui_menu_group_rel', 'gid', 'menu_id', 'Access Menu'),
+        'menu_access': fields.many2many('ir.ui.menu', 'ir_ui_menu_group_rel', 'gid', 'menu_id', 'Access Menu', display_inactive=True),
         'comment': fields.text('Comment',size=250),
         'level': fields.selection([('hq', 'HQ'),
                                    ('coordo', 'Coordination'),
