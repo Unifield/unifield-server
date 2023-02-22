9d54e8842408f3e80d9b3f3be415ce99fe08c73c
jf <jfb@tempo-consulting.fr>
2023-02-22 11:34:39 +0100

    Release UF28.0rc2

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'UF28.0rc1'
+version = 'UF28.0rc2'
 major_version = '8.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
