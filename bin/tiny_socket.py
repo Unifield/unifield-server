46e61adcd52154f2186aa060f48633143775c081
jf <jfb@tempo-consulting.fr>
2023-03-15 11:23:32 +0100

    [FIX] tiny_socket: unable to retrieve from the Web, a 100MB sync server patch file

--- bin/tiny_socket.py
+++ bin/tiny_socket.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
 #
@@ -15,7 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -88,11 +88,11 @@ class mysocket:
 
         size = int(read(self.sock, 8))
         buf = read(self.sock,1)
-        if buf == "3":
+        if buf == b"3":
             newsize = int(read(self.sock, 16))
             size = newsize*10**8+size
             buf = read(self.sock,1)
-        if buf != "0":
+        if buf != b"0":
             exception = buf
         else:
             exception = False
