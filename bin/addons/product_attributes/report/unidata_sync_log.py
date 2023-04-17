d2561dd5aadb22c3403a8298e48533f40ad5553b
jf <jfb@tempo-consulting.fr>
2023-04-17 11:13:51 +0200

    Download mutliple log files

--- bin/addons/product_attributes/report/unidata_sync_log.py
+++ bin/addons/product_attributes/report/unidata_sync_log.py
@@ -11,10 +11,11 @@ import os
 class export_unidata_sync_log(report_sxw.report_sxw):
 
     def create(self, cr, uid, ids, data, context=None):
-        d = pooler.get_pool(cr.dbname).get('unidata.sync.log').read(cr, uid, ids[0], ['log_file'], context=context)
         null1, tmpzipname = tempfile.mkstemp()
         zf = zipfile.ZipFile(tmpzipname, 'w')
-        zf.write(d['log_file'], os.path.basename(d['log_file']), compress_type=zipfile.ZIP_DEFLATED)
+        for d in pooler.get_pool(cr.dbname).get('unidata.sync.log').read(cr, uid, ids, ['log_file', 'log_exists'], context=context):
+            if d['log_exists']:
+                zf.write(d['log_file'], os.path.basename(d['log_file']), compress_type=zipfile.ZIP_DEFLATED)
         zf.close()
         os.close(null1)
         return (Path(tmpzipname, delete=True), 'zip')
