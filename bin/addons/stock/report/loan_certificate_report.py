7f54875c2b1e5bfece87361b05c5e13d82d9e628
Dorian <dk@tempo-consulting.fr>
2023-04-20 16:18:43 +0200

    US-8455 [IMP] IN/OUT/SHIP: New Loan (+ Return) Certificates to display data coming from documents with this RT

---
+++ bin/addons/stock/report/loan_certificate_report.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 TeMPO Consulting, MSF
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from report import report_sxw
+
+
+class loan_certificate_report(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(loan_certificate_report, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'displayValue': self._display_value,
+            'getTotalValue': self._get_total_value,
+            'getCurrencyName': self._get_currency_name,
+            'getEstimatedDate': self._get_estimated_date,
+        })
+
+    def _display_value(self):
+        return self.localcontext.get('data') and self.localcontext['data'].get('display_value', False) or False
+
+    def _get_total_value(self, pick):
+        '''
+        Return the total with a calculated price
+        '''
+        tot_value = 0
+        for move in pick.move_lines:
+            tot_value += move.product_qty * (move.price_unit or move.product_id.standard_price or 0.00)
+
+        return tot_value
+
+    def _get_currency_name(self, pick):
+        '''
+        Return the currency to use
+        '''
+        curr_name = pick.company_id.currency_id.name
+        if pick.sale_id:
+            curr_name = pick.sale_id.pricelist_id.currency_id.name
+        elif pick.purchase_id:
+            curr_name = pick.purchase_id.pricelist_id.currency_id.name
+
+        return curr_name
+
+    def _get_estimated_date(self, pick):
+        esti_date = pick.min_date
+        if pick.sale_id:
+            esti_date = (datetime.strptime(pick.sale_id.date_order, '%Y-%m-%d') +
+                         relativedelta(months=pick.sale_id.loan_duration)).strftime('%Y-%m-%d %H:%M:%S')
+        elif pick.purchase_id:
+            esti_date = (datetime.strptime(pick.purchase_id.date_order, '%Y-%m-%d') +
+                         relativedelta(months=pick.purchase_id.loan_duration)).strftime('%Y-%m-%d %H:%M:%S')
+        return esti_date
+
+
+report_sxw.report_sxw('report.loan.certificate', 'stock.picking', 'addons/order_types/report/loan_certificate.rml',
+                      parser=loan_certificate_report, header=False)
+
+report_sxw.report_sxw('report.loan.return.certificate', 'stock.picking', 'addons/order_types/report/loan_return_certificate.rml',
+                      parser=loan_certificate_report, header=False)
+
+
+class ship_loan_certificate_report(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(ship_loan_certificate_report, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'getLoansData': self._get_loans_data,
+            'displayValue': self._display_value,
+            'getTotalValue': self._get_total_value,
+            'getLines': self._get_lines,
+            'getCompany': self._get_company,
+        })
+
+    def _get_loans_data(self, packs, ship_actual_date, loan_return=False):
+        loan_id = self.localcontext['data'].get('loan_id', False)
+        loan_return_id = self.localcontext['data'].get('loan_return_id', False)
+
+        loan_id_to_check = loan_id
+        if loan_return:
+            loan_id_to_check = loan_return_id
+        loans_data = []
+        for pack in packs:
+            if pack.ppl_id.reason_type_id.id == loan_id_to_check:
+                loan_esti_date = ship_actual_date
+                if pack.ppl_id.sale_id:
+                    loan_esti_date = (datetime.strptime(pack.ppl_id.sale_id.date_order, '%Y-%m-%d') +
+                                      relativedelta(months=pack.ppl_id.sale_id.loan_duration)).strftime('%Y-%m-%d %H:%M:%S')
+                loans_data.append({
+                    'loan_ref': pack.ppl_id.sale_id and pack.ppl_id.sale_id.name or '',
+                    'loan_origin': pack.ppl_id.sale_id and (pack.ppl_id.sale_id.loan_id and
+                                   pack.ppl_id.sale_id.loan_id.name or pack.ppl_id.sale_id.origin) or '',
+                    'loan_esti_date': loan_esti_date,
+                })
+        return loans_data
+
+    def _display_value(self):
+        return self.localcontext.get('data') and self.localcontext['data'].get('display_value', False) or False
+
+    def _get_total_value(self):
+        return self.localcontext.get('data') and self.localcontext['data'].get('value', 0.00) or 0.00
+
+    def _get_lines(self, packs, loan_return=False):
+        loan_id = self.localcontext['data'].get('loan_id', False)
+        loan_return_id = self.localcontext['data'].get('loan_return_id', False)
+
+        loan_id_to_check = loan_id
+        if loan_return:
+            loan_id_to_check = loan_return_id
+        lines = []
+        for pack in packs:
+            if pack.ppl_id.reason_type_id.id == loan_id_to_check:
+                for move in pack.move_lines:
+                    lines.append({
+                        'product_code': move.product_id.default_code,
+                        'product_name': move.product_id.name,
+                        'qty': move.product_qty,
+                        'prodlot_name': move.prodlot_id and move.prodlot_id.name or '',
+                        'expiry_date': move.prodlot_id and move.prodlot_id.life_date or '',
+                    })
+        return lines
+
+    def _get_company(self):
+        '''
+        Return information about the company.
+        '''
+        company = self.pool.get('res.users').browse(self.cr, self.uid, self.uid).company_id
+
+        res = {}
+        if company:
+            res['partner'] = company.partner_id and company.partner_id.name or False
+            if company.partner_id and len(company.partner_id.address):
+                res['street'] = company.partner_id.address[0].street
+                res['street2'] = company.partner_id.address[0].street2
+                if company.partner_id.address[0].zip is not False:
+                    res['zip'] = company.partner_id.address[0].zip
+                else:
+                    res['zip'] = ""
+                res['city'] = company.partner_id.address[0].city
+                res['country'] = company.partner_id.address[0].country_id and company.partner_id.address[0].country_id.name or False
+
+        return res
+
+
+report_sxw.report_sxw('report.ship.loan.certificate', 'shipment', 'addons/order_types/report/ship_loan_certificate.rml',
+                      parser=ship_loan_certificate_report, header=False)
+
+report_sxw.report_sxw('report.ship.loan.return.certificate', 'shipment', 'addons/order_types/report/ship_loan_return_certificate.rml',
+                      parser=ship_loan_certificate_report, header=False)
---
+++ bin/addons/stock/wizard/loan_certificate_wizard.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) TeMPO Consulting (<http://www.tempo-consulting.fr/>), MSF.
+#    All Rigts Reserved
+#    Developer: Stéphane Codazzi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import osv
+from osv import fields
+from tools.translate import _
+
+
+class loan_certificate_wizard(osv.osv_memory):
+    _name = 'loan.certificate.wizard'
+
+    def _get_default_loan_return(self, cr, uid, context=None):
+        loan_return_id = self.pool.get('ir.model.data').get_object_reference(cr, uid, 'reason_types_moves',
+                                                                             'reason_type_loan_return')[1]
+        pick = False
+        if context.get('active_id'):
+            pick = self.pool.get('stock.picking').read(cr, uid, context['active_id'], ['reason_type_id'], context=context)
+        return pick and pick['reason_type_id'][0] == loan_return_id or False
+
+    _columns = {
+        'display_value': fields.boolean(string='Display total value', help='Tick to display the Total Value in the report'),
+        'loan_return': fields.boolean(string='Is Loan Return'),
+    }
+
+    _defaults = {
+        'display_values': False,
+        'loan_return': _get_default_loan_return,
+    }
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        """
+        Prevent the use of the report if the document (IN or OUT) does not have the Loan or Loan Return Reason Type
+        """
+        if context is None:
+            context = {}
+        data_obj = self.pool.get('ir.model.data')
+        loan_id = data_obj.get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_loan')[1]
+        loan_return_id = data_obj.get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_loan_return')[1]
+
+        if not context.get('picking_type') or context['picking_type'] not in ['incoming_shipment', 'delivery_order']:
+            raise osv.except_osv(_('Error'), _('You can only use this Report for INs and OUTs'))
+        if context.get('active_id'):
+            pick = self.pool.get('stock.picking').read(cr, uid, context['active_id'], ['reason_type_id'], context=context)
+            if pick['reason_type_id'][0] not in [loan_id, loan_return_id]:
+                raise osv.except_osv(_('Error'), _('You can not use this Report if the document does not have the Loan or Loan Return Reason Type'))
+        else:
+            raise osv.except_osv(_('Error'), _('No active ID'))
+
+        return super(loan_certificate_wizard, self).fields_view_get(cr, uid, view_id, view_type, context=context, toolbar=toolbar, submenu=submenu)
+
+    def create_report(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+
+        for wiz in self.browse(cr, uid, ids, context=context):
+            data = {
+                'ids': [context.get('active_id')],
+                'model': 'stock_picking',
+                'display_value': wiz.display_value,
+                'context': context,
+            }
+            report_name = 'loan.certificate'
+            if wiz.loan_return:
+                report_name = 'loan.return.certificate'
+            return {
+                'type': 'ir.actions.report.xml',
+                'report_name': report_name,
+                'datas': data,
+            }
+        return {'type': 'ir.actions.act_window_close'}
+
+
+loan_certificate_wizard()
+
+
+class ship_loan_certificate_wizard(osv.osv_memory):
+    _name = 'ship.loan.certificate.wizard'
+
+    def _get_ship_id(self, cr, uid, context=None):
+        ship = context.get('active_id') and self.pool.get('shipment').browse(cr, uid, context['active_id'],
+                                                                             fields_to_fetch=['id'], context=context)
+        return ship and ship.id or False
+
+    def _get_has_loan(self, cr, uid, context=None):
+        ship = context.get('active_id') and self.pool.get('shipment').browse(cr, uid, context['active_id'],
+                                                                             fields_to_fetch=['has_loan'], context=context)
+        return ship and ship.has_loan or False
+
+    def _get_has_ret_loan(self, cr, uid, context=None):
+        ship = context.get('active_id') and self.pool.get('shipment').browse(cr, uid, context['active_id'],
+                                                                             fields_to_fetch=['has_ret_loan'], context=context)
+        return ship and ship.has_ret_loan or False
+
+    _columns = {
+        'display_value': fields.boolean(string='Display total value', help='Tick to display the Total Value in the report'),
+        'ship_id': fields.many2one('shipment', string='Shipment'),
+        'has_loan': fields.boolean(string='Has Loan Pack(s)'),
+        'has_ret_loan': fields.boolean(string='Has Loan Return Pack(s)'),
+    }
+
+    _defaults = {
+        'display_value': False,
+        'ship_id': _get_ship_id,
+        'has_loan': _get_has_loan,
+        'has_ret_loan': _get_has_ret_loan,
+    }
+
+    def fields_view_get(self, cr, uid, view_id=None, view_type='form', context=None, toolbar=False, submenu=False):
+        """
+        Prevent the use of the report if the SHIP does not contain a PPL with the Loan or Loan Return Reason Type
+        """
+        if context is None:
+            context = {}
+
+        if context.get('active_id'):
+            ftf = ['backshipment_id', 'has_loan', 'has_ret_loan']
+            ship = self.pool.get('shipment').browse(cr, uid, context['active_id'], fields_to_fetch=ftf, context=context)
+            if not ship.backshipment_id:
+                raise osv.except_osv(_('Warning !'), _('Loan Certificate is only available for Shipment Objects (not draft)!'))
+            if not ship.has_loan and not ship.has_ret_loan:
+                raise osv.except_osv(_('Error'), _('You can not use this Report if the Shipment does not contain at least a Pack with the Loan or Loan Return Reason Type'))
+        else:
+            raise osv.except_osv(_('Error'), _('No active ID'))
+
+        return super(ship_loan_certificate_wizard, self).fields_view_get(cr, uid, view_id, view_type, context=context, toolbar=toolbar, submenu=submenu)
+
+    def create_return_report(self, cr, uid, ids, context=None):
+        return self.create_report(cr, uid, ids, context=context, loan_return=True)
+
+    def create_report(self, cr, uid, ids, context=None, loan_return=False):
+        if context is None:
+            context = {}
+
+        data_obj = self.pool.get('ir.model.data')
+        loan_id = data_obj.get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_loan')[1]
+        loan_return_id = data_obj.get_object_reference(cr, uid, 'reason_types_moves', 'reason_type_loan_return')[1]
+
+        for wiz in self.browse(cr, uid, ids, context=context):
+            report_name = 'ship.loan.certificate'
+            value = 0.00
+            loan_id_to_check = loan_id
+            if loan_return:
+                report_name = 'ship.loan.return.certificate'
+                loan_id_to_check = loan_return_id
+            if wiz.display_value:
+                for pack in wiz.ship_id.pack_family_memory_ids:
+                    if pack.ppl_id.reason_type_id.id == loan_id_to_check:
+                        for move in pack.move_lines:
+                            move_price = move.price_unit or move.product_id.standard_price or 0.00
+                            if move.price_currency_id.id != wiz.ship_id.currency_id.id:
+                                move_price = self.pool.get('res.currency').\
+                                    compute(self.cr, self.uid, move.price_currency_id.id, wiz.ship_id.currency_id.id,
+                                            move_price, round=False, context=self.localcontext)
+                            value += move.product_qty * move_price
+            data = {
+                'ids': [wiz.ship_id.id],
+                'model': 'shipment',
+                'loan_id': loan_id,
+                'loan_return_id': loan_return_id,
+                'display_value': wiz.display_value,
+                'value': value,
+                'context': context,
+            }
+
+            return {
+                'type': 'ir.actions.report.xml',
+                'report_name': report_name,
+                'datas': data,
+            }
+        return {'type': 'ir.actions.act_window_close'}
+
+
+ship_loan_certificate_wizard()
