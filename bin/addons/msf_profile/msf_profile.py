dc4d08022b415a037c1e9f51daafda5ec417e3e1
Dorian <dk@tempo-consulting.fr>
2023-04-27 15:25:13 +0200

    US-11177 [FIX] KCL: fixed BN/ED creation in patch script

--- bin/addons/msf_profile/msf_profile.py
+++ bin/addons/msf_profile/msf_profile.py
@@ -72,9 +72,7 @@ class patch_scripts(osv.osv):
             if kcl_item.item_exp and not kcl_item.item_product_id.perishable:
                 continue
             if kcl_item.item_product_id.batch_management or kcl_item.item_product_id.perishable:
-                lot_name = kcl_item.item_lot or False
-                if kcl_item.item_product_id.batch_management and not lot_name:
-                     lot_name = 'TO-BE-REPLACED'
+                lot_name = kcl_item.item_product_id.batch_management and (kcl_item.item_lot or 'TO-BE-REPLACED') or False
                 lot_date = kcl_item.item_exp or time.strftime('%Y-%m-%d')
                 new_bn_id = bn_obj._get_or_create_lot(cr, uid, lot_name, lot_date, kcl_item.item_product_id.id)
                 kcl_item_obj.write(cr, uid, kcl_item.id, {'item_lot_id': new_bn_id, 'item_exp': lot_date})
