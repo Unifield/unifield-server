8db897b0c5267cfb0843f4fae95d6ad39c89aad7
jf <jfb@tempo-consulting.fr>
2023-09-20 15:28:04 +0200

    ILL import: reset running wizard on restart

--- bin/addons/account_hq_entries/wizard/esc_lines_import.py
+++ bin/addons/account_hq_entries/wizard/esc_lines_import.py
@@ -53,9 +53,9 @@ class esc_line_import_wizard(osv.osv):
 
     def __init__(self, pool, cr):
         super(esc_line_import_wizard, self).__init__(pool, cr)
-        if cr.column_exists('esc_line_import_wizard', 'state'):
+        if cr.column_exists('esc_line_import', 'state'):
             cr.execute("update esc_line_import_wizard set state='error' where state='inprogress'")
-        if cr.column_exists('esc_line_import_wizard', 'file'):
+        if cr.column_exists('esc_line_import', 'file'):
             cr.execute("update esc_line_import_wizard set file=null where file is not null")
 
     def write(self, cr, uid, ids, vals, context=None):
