fa9d406c59dc594f53ad7ea1724082e2a1fbf782
jf <jfb@tempo-consulting.fr>
2025-04-11 12:09:33 +0200

    new column, manage upgrade

--- bin/addons/msf_profile/msf_profile.py
+++ bin/addons/msf_profile/msf_profile.py
@@ -7233,8 +7233,7 @@ class email_configuration(osv.osv):
         res = cr.fetchone()
         if res:
             for i, key in enumerate(data):
-                if key in tools.config:
-                    tools.config[key] = res[i] or False
+                tools.config[key] = res[i] or False
         return True
 
     def __init__(self, pool, cr):
