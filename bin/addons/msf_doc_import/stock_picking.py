fc24dac2af76803976e0321948ed8e4e0ade514c
Dorian <dk@tempo-consulting.fr>
2025-11-04 14:57:20 +0000

    US-15045-15046 [FIX] ITO/OTO: Macroprocess is mandatory on view only, document can't be processed without macroprocess, steps are readonly when there is no macroprocess

--- bin/addons/msf_doc_import/stock_picking.py
+++ bin/addons/msf_doc_import/stock_picking.py
@@ -506,10 +506,8 @@ class stock_picking(osv.osv):
                     'supplier_partner_id': in_partner_id,
                     'ship_ref': in_shipment_ref,
                     'zone_type': company_address and company_address.country_id and supplier_address and supplier_address.country_id and
-                    company_address.country_id.id == supplier_address.country_id.id and 'domestic' or 'int',
-                    'cargo_category': ito_categ,
-                    'macroprocess_id': self.pool.get('ir.model.data').get_object_reference(cr, uid, 'transport_mgmt',
-                                                                                           'macroprocess_international_transport_import')[1]
+                        company_address.country_id.id == supplier_address.country_id.id and 'domestic' or 'int',
+                    'cargo_category': ito_categ
                 }
                 ito_id = ito_obj.create(cr, uid, ito_data, context=context)
                 crea_upd = _('created')
