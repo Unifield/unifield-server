6c916982d5ee18f82a2d409c3a86282ffc6be13e
Dorian Kemps <dk@tempo-consulting.fr>
2025-12-04 16:36:28 +0100

    US-14434 [IMP] PO: Added flag for merged POs, merged PDF can only be used by merged POs
    Signed-off-by: Dorian Kemps <dk@tempo-consulting.fr>

--- bin/addons/purchase_override/report/merged_order.py
+++ bin/addons/purchase_override/report/merged_order.py
@@ -20,6 +20,8 @@
 ##############################################################################
 
 import time
+from osv import osv
+from tools.translate import _
 from report import report_sxw
 
 class merged_order(report_sxw.rml_parse):
@@ -32,6 +34,16 @@ class merged_order(report_sxw.rml_parse):
             'getOrigin': self._get_origin,
             'get_merged_lines': self.get_merged_lines,
         })
+
+    def set_context(self, objects, data, ids, report_type=None):
+        '''
+        opening check
+        '''
+        for obj in objects:
+            if not obj.merged_po:
+                raise osv.except_osv(_('Warning !'), _('This PDF is only available for merged POs'))
+
+        return super(merged_order, self).set_context(objects, data, ids, report_type=report_type)
 
     def _get_origin(self, origin, number=5):
         res = []
