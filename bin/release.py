27c011940b114855febbe3c78eda7308d5e4a519
jftempo <jfb@tempo-consulting.fr>
2026-01-29 12:11:25 +0100

    Release UF39.1rc1

--- bin/release.py
+++ bin/release.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 name = 'openerp-server'
-version = 'UF39.0'
+version = 'UF39.1rc1'
 major_version = '9.0'
 description = 'OpenERP Server'
 long_desc = '''OpenERP is a complete ERP and CRM. The main features are accounting (analytic
