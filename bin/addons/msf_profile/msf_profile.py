4dfabef456c18ab1285168815c2f581b39721ad6
Dorian Kemps <dk@tempo-consulting.fr>
2026-04-02 10:10:34 +0200

    US-14587-15645 [FIX] SDE: Do not add the groups to the SDE user on sync server
    Signed-off-by: Dorian Kemps <dk@tempo-consulting.fr>

--- bin/addons/msf_profile/msf_profile.py
+++ bin/addons/msf_profile/msf_profile.py
@@ -80,16 +80,17 @@ class patch_scripts(osv.osv):
         # SQL request to activate the menus, then restart the server
         # UPDATE ir_ui_menu SET active = 't' WHERE id IN (SELECT res_id FROM ir_model_data WHERE name IN ('sde_import_menu', 'sde_import_main_menu', 'sde_paginated_import_menu', 'sde_paginated_export_menu'));
 
-        # Set the needed Groups to "sde_tool" user
-        user_obj = self.pool.get('res.users')
-        sde_tool_user_id = user_obj._get_sde_tool_user_id(cr)
-        if sde_tool_user_id:
-            group_obj = self.pool.get('res.groups')
-            sync_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sync / User')])
-            sup_wh_man_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sup_Warehouse_Manager')])
-            sup_tr_man_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sup_Transport_Manager')])
+        # Set the needed Groups to "sde_tool" user, exclude sync server
+        if not cr.table_exists('sync_server_user_rights'):
+            user_obj = self.pool.get('res.users')
+            sde_tool_user_id = user_obj._get_sde_tool_user_id(cr)
+            if sde_tool_user_id:
+                group_obj = self.pool.get('res.groups')
+                sync_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sync / User')])
+                sup_wh_man_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sup_Warehouse_Manager')])
+                sup_tr_man_group_ids = group_obj.search(cr, uid, [('name', '=', 'Sup_Transport_Manager')])
 
-            user_obj.write(cr, uid, sde_tool_user_id, {'groups_id': [(6, 0, [sync_group_ids[0], sup_wh_man_group_ids[0], sup_tr_man_group_ids[0]])]}, context={})
+                user_obj.write(cr, uid, sde_tool_user_id, {'groups_id': [(6, 0, [sync_group_ids[0], sup_wh_man_group_ids[0], sup_tr_man_group_ids[0]])]}, context={})
 
         return True
 
