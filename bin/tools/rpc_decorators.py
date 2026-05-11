2b37b2368a6e55df9a8492afbf4cbd501b569296
jftempo <jfb@tempo-consulting.fr>
2026-05-11 10:14:11 +0200

    US-15419 [FIX] json-rpc api remove debug

--- bin/tools/rpc_decorators.py
+++ bin/tools/rpc_decorators.py
@@ -13,6 +13,5 @@ def jsonrpc_orm_exposed(model, method=None):
     def decorator(func):
         m = method or func.__name__
         JSONRPC_ORM_ALLOWED.add((model, m))
-        print(f"Exposed via JSON-RPC: {(model, m)}")
         return func
     return decorator
