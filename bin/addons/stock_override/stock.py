d4f2f95a37c3c79fa9babd02a2d0c0af379dd8bb
Dorian Kemps <dk@tempo-consulting.fr>
2026-09-24 12:52:27 +0200

    US-16158 [FIX] IN/INT/OUT from scratch: Reverted the changes from stock_picking's draft_force_assign and search for draft moves for the moves' action_confirm
    Signed-off-by: Dorian Kemps <dk@tempo-consulting.fr>

--- bin/addons/stock_override/stock.py
+++ bin/addons/stock_override/stock.py
@@ -926,7 +926,13 @@ class stock_picking(osv.osv):
         if context.get('from_button') and context.get('picking_type') == 'internal_move':
             move_obj.check_moves_loc_reason_type(cr, uid, move_ids, context=context)
 
-        return super(stock_picking, self).draft_force_assign(cr, uid, ids)
+        res = super(stock_picking, self).draft_force_assign(cr, uid, ids)
+
+        # Search for moves that are still Draft to confirm them
+        move_ids = move_obj.search(cr, uid, [('state', '=', 'draft'), ('picking_id', 'in', ids)], context=context)
+        move_obj.action_confirm(cr, uid, move_ids, context=context)
+
+        return res
 
     def is_invoice_needed(self, cr, uid, sp=None, invoice_type=None):
         """
